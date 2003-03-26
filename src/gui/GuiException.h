f6a5d001b4957ca1b3fafdb715b007275a0e7f37
James Boyden <>
2003-03-26 12:07:12 +0000

    Initial commit.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@54 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

---
+++ src/gui/GuiException.h
@@ -0,0 +1,44 @@
+/* $Id$ */
+
+/**
+ * \file 
+ * File specific comments.
+ *
+ * Most recent change:
+ *   $Author$
+ *   $Date$
+ * 
+ * Copyright (C) 2003 The GPlates Consortium
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Authors:
+ *   James Boyden <jboyden@es.usyd.edu.au>
+ */
+
+#ifndef _GPLATES_GUI_GUIEXCEPTION_H_
+#define _GPLATES_GUI_GUIEXCEPTION_H_
+
+#include "global/Exception.h"
+
+namespace GPlatesGui
+{
+	/**
+	 * The (pure virtual) base class of all GUI exceptions.
+	 */
+	class GuiException : public GPlatesGlobal::Exception
+	{
+		public:
+			virtual
+			~GuiException() {  }
+	};
+}
+
+#endif  // _GPLATES_GUI_GUIEXCEPTION_H_
