580b0eed434e6038f09e62d3a4a36f59c6fa1c2a
James Boyden <>
2003-06-15 19:50:14 +0000

    Initial commit of the class 'FPData'.  This class is intended to represent floating-point data which is static (ie. unchanging through arithmetic operations or other mathematical means).  In effect, this class will be used as a 'storage unit' for floating-point data.  Its primary use is intended to lie in the geo/ development, as a replacement for the class 'Real', which is going to become more mathematical.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@66 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

---
+++ src/global/FPData.h
@@ -0,0 +1,103 @@
+/* $Id$ */
+
+/**
+ * \file 
+ * File specific comments.
+ *
+ * Most recent change:
+ *   $Author$
+ *   $Date$
+ * 
+ * Copyright (C) 2003 The GPlates Consortium
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Authors:
+ *   James Boyden <jboyden@geosci.usyd.edu.au>
+ */
+
+#ifndef _GPLATES_GLOBAL_FPDATA_H_
+#define _GPLATES_GLOBAL_FPDATA_H_
+
+namespace GPlatesGlobal
+{
+	/**
+	 * This class is used to represent any static floating-point data
+	 * used in the project.  By "static" is meant "will not change",
+	 * ie. no arithmetic or other mathematical operations will be
+	 * performed upon it.  For this reason, no arithmetic operators
+	 * will be provided for it, and no mathematical functions (sin,
+	 * cos, tan, sqrt, etc) will be implemented for it.
+	 *
+	 * It <i>will</i> have mathematical comparison operations and
+	 * I/O operators, but this should be all it needs.
+	 */
+	class FPData
+	{
+			double _dval;
+
+		public:
+			FPData(double d) : _dval(d) {  }
+
+			double dval() const { return _dval; }
+	};
+
+
+	inline bool
+	operator==(FPData fpd1, FPData fpd2) {
+
+		return (fpd1.dval() == fpd2.dval());
+	}
+
+
+	inline bool
+	operator!=(FPData fpd1, FPData fpd2) {
+
+		return (fpd1.dval() != fpd2.dval());
+	}
+
+
+	inline bool
+	operator<=(FPData fpd1, FPData fpd2) {
+
+		return (fpd1.dval() <= fpd2.dval());
+	}
+
+
+	inline bool
+	operator>=(FPData fpd1, FPData fpd2) {
+
+		return (fpd1.dval() >= fpd2.dval());
+	}
+
+
+	inline bool
+	operator>(FPData fpd1, FPData fpd2) {
+
+		return (fpd1.dval() > fpd2.dval());
+	}
+
+
+	inline bool
+	operator<(FPData fpd1, FPData fpd2) {
+
+		return (fpd1.dval() < fpd2.dval());
+	}
+
+
+	inline std::ostream &
+	operator<<(std::ostream &os, FPData fpd) {
+
+		os << fpd.dval();
+		return os;
+	}
+}
+
+#endif  // _GPLATES_GLOBAL_FPDATA_H_
