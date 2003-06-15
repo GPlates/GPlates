36bbdcb56fcf04f69d3db062e3ec9c164457dd52
James Boyden <>
2003-06-15 20:55:23 +0000

    These two files (which contain the definition of the 'Real' class and associated operators and functions) have been moved here from ../global/ as their focus has become more mathematical than general.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@73 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

---
+++ src/maths/Real.cc
@@ -0,0 +1,36 @@
+/* $Id$ */
+
+/**
+ * \file 
+ * File specific comments.
+ *
+ * Most recent change:
+ *   $Author$
+ *   $Date$
+ * 
+ * Copyright (C) 2003 The GPlates Consortium
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Authors:
+ *   James Boyden <jboyden@geosci.usyd.edu.au>
+ */
+
+#include "Real.h"
+
+/*
+ * FIXME: this value was just a guess. Discover what this value should be.
+ */
+#define REAL_EPSILON (1.0e-15)
+
+using namespace GPlatesMaths;
+
+const double Real::Epsilon = REAL_EPSILON;
+const double Real::NegativeEpsilon = -REAL_EPSILON;
