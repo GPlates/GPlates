ee92829274ac5030224c835ffad411c64266ef34
James Boyden <>
2003-06-17 21:59:39 +0000

    Added definitions for the functions - sqrt - asin both of which rely upon the new exception 'FunctionDomainException'.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@97 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- src/maths/Real.cc
+++ src/maths/Real.cc
@@ -23,7 +23,9 @@
  *   James Boyden <jboyden@geosci.usyd.edu.au>
  */
 
+#include <sstream>
 #include "Real.h"
+#include "FunctionDomainException.h"
 
 /*
  * FIXME: this value was just a guess. Discover what this value should be.
@@ -34,3 +36,75 @@ using namespace GPlatesMaths;
 
 const double Real::Epsilon = REAL_EPSILON;
 const double Real::NegativeEpsilon = -REAL_EPSILON;
+
+
+Real
+sqrt(Real r) {
+
+	// First, perform "almost exact" comparison.
+	if (r < 0.0) {
+
+		/*
+		 * Even allowing some flexibility of comparison,
+		 * the argument is negative, which falls outside
+		 * the domain of sqrt.
+		 */
+		std::ostringstream
+		 oss("function 'sqrt' invoked with invalid argument ");
+		oss << r;
+		throw FunctionDomainException(oss.str().c_str());
+	}
+
+	/*
+	 * Now, clean up after any errors which are caused by "almost valid"
+	 * arguments.
+	 */
+	if (isNegative(r)) {
+
+		// it was just slightly less than zero -- return sqrt of zero
+		return Real(0.0);
+
+	}
+	return Real(std::sqrt(r.dval()));
+}
+
+
+/*
+ * FIXME: This is often defined in the C Standard Library -- but is it
+ * *always* defined *everywhere*, and to the *same precision*?
+ */
+#define MATHVALUE_PI_ON_2 (1.57079632679489661923)
+
+Real
+asin(Real r) {
+
+	// First, perform "almost exact" comparisons for bounds of domain.
+	if (r < -1.0 || r > 1.0) {
+
+		/*
+		 * Even allowing some flexibility of comparison, 
+		 * the argument which falls outside the domain of asin.
+		 */
+		std::ostringstream 
+		 oss("function 'asin' invoked with invalid argument ");
+		oss << r;
+		throw FunctionDomainException(oss.str().c_str());
+	}
+
+	/*
+	 * Now, clean up after any errors which are caused by "almost valid"
+	 * arguments.
+	 */
+	if (isLessThanMinusOne(r)) {
+
+		// it was just slightly less than minus one
+		// -- return asin of minus one
+		return Real(-MATHVALUE_PI_ON_2);
+	}
+	if (isGreaterThanOne(r)) {
+
+		// it was just slightly greater than one -- return asin of one
+		return Real(MATHVALUE_PI_ON_2);
+	}
+	return Real(std::asin(r.dval()));
+}
