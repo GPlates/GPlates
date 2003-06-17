49daf8d33a70d90ed705411111cda3c2e912cfa5
James Boyden <>
2003-06-17 21:56:15 +0000

    This class is required for the mathematical functions operating on the Real class.  The mathematical functions ensure that their arguments are within the expected domain.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@95 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

---
+++ src/maths/FunctionDomainException.h
@@ -0,0 +1,68 @@
+/* $Id$ */
+
+/**
+ * \file 
+ * File specific comments.
+ *
+ * Most recent change:
+ *   $Author$
+ *   $Date$
+ * 
+ * Copyright (C) 2003 The GPlates Consortium
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Authors:
+ *   James Boyden <jboyden@geosci.usyd.edu.au>
+ */
+
+#ifndef _GPLATES_MATHS_FUNCTIONDOMAINEXCEPTION_H_
+#define _GPLATES_MATHS_FUNCTIONDOMAINEXCEPTION_H_
+
+#include "MathematicalException.h"
+
+namespace GPlatesMaths
+{
+	/**
+	 * The Exception thrown when the argument to a mathematical function
+	 * lies outside the valid domain of the function.
+	 *
+	 * Analogous to the 'EDOM' error in the C Standard Library.
+	 */
+	class FunctionDomainException
+		: public MathematicalException
+	{
+		public:
+			/**
+			 * @param msg is a description of the conditions
+			 * which cause the invariant to be violated.
+			 */
+			FunctionDomainException(const char *msg)
+				: _msg(msg) {  }
+
+			virtual
+			~FunctionDomainException() {  }
+
+		protected:
+			virtual const char *
+			ExceptionName() const {
+
+				return "FunctionDomainException";
+			}
+
+			virtual std::string
+			Message() const { return _msg; }
+
+		private:
+			std::string _msg;
+	};
+}
+
+#endif  // _GPLATES_MATHS_FUNCTIONDOMAINEXCEPTION_H_
