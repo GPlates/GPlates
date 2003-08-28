7ca2ea22a1dbf0d95bf7cd6f24cba0d65dd1802f
James Boyden <>
2003-08-28 21:02:26 +0000

    Improved the comment for the FPDATA_EPSILON variable, based upon some other links I found.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@137 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- src/global/FPData.cc
+++ src/global/FPData.cc
@@ -26,9 +26,22 @@
 #include "FPData.h"
 
 /*
- * FIXME: this value was just a guess. Discover what this value should be.
- * [The guess was based upon several pages found on the net; first and
- * foremost http://www.ma.utexas.edu/documentation/lapack/node73.html ]
+ * FIXME: the value below was just a guess. Discover what this value should be.
+ *
+ * According to:
+ *  http://www.cs.berkeley.edu/~demmel/cs267/lecture21/lecture21.html
+ * and
+ *  http://www.ma.utexas.edu/documentation/lapack/node73.html
+ * the machine epsilon for an IEEE 754-compliant machine is about 1.2e-16.
+ *
+ * According to these documents, the machine epsilon (aka "macheps") is
+ * half the distance between 1 and the next largest fp value.
+ *
+ * I wish to allow for rounding errors due to the limits of floating-point
+ * precision, but _unlike_ the "Real" type (found in ../maths/Real.{h,cc}),
+ * I don't want to allow for accumulation of rounding errors.  The "FPData"
+ * type is only supposed to be used for arithmetic comparisons, not other
+ * arithmetic operations (such as addition, subtraction, multiplcation, etc.).
  */
 #define FPDATA_EPSILON (1.2e-16)
 
