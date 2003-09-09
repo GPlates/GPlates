7e444a29b9ccb7ea25e4cdc5acf427b082286079
Mike Dowman <>
2003-09-09 01:51:41 +0000

    First version of exceptions for FileFormat errors.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@146 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

---
+++ src/fileio/FileFormatException.h
@@ -0,0 +1,43 @@
+/*
+ *
+ * Copyright (C) 2003 The GPlates Consortium
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Author:
+ *   Mike Dowman <mdowman@geosci.usyd.edu.au>
+ */
+
+#ifndef _GPLATES_FILEIO_FILEFORMATEXCEPTION_H_
+#define _GPLATES_FILEIO_FILEFORMATEXCEPTION_H_
+
+#include "FileIOException.h"
+
+namespace GPlatesFileIO {
+  
+  // This exception is thrown when there is  a problem with the Format of a file being read in by GPlates.
+  class FileFormatException : public FileIOException {
+  public:
+    FileFormatException(const char* message) : _message(message) { }
+    
+    virtual ~FileFormatException() { }
+    
+  protected:
+    virtual const char * ExceptionName() const { return "FileFormatException"; }
+    
+    virtual std::string Message() const { return _message; }
+    
+   private:
+    std::string _message;
+  };
+  
+ } // End namespace
+
+#endif  // _GPLATES_FILEIO_FILEFORMATEXCEPTION_H_
