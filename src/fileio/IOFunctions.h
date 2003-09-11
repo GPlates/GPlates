479078f8bfa54dbe4f28d719a72417fb73aeb335
Mike Dowman <>
2003-09-11 01:23:43 +0000

    Added CVS keywords and doxygen comments and copyright notice
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@161 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- src/fileio/IOFunctions.h
+++ src/fileio/IOFunctions.h
@@ -1,3 +1,28 @@
+/*$Id$*/
+/// Functions used by RotationDataFileReader.cc and IO.cc
+/**
+ * @file
+ *
+ * Most recent change:
+ * $Author$
+ * $Date$
+ *
+ *
+ * Copyright (C) 2003 The GPlates Consortium
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Author:
+ *   Mike Dowman <mdowman@geosci.usyd.edu.au>
+ */
+
 #ifndef _GPLATES_FILEIO_IOFUNCTIONS_H
 #define _GPLATES_FILEIO_IOFUNCTIONS_H
 
@@ -5,9 +30,11 @@
 
 namespace GPlatesFileIO {
 
-  bool empty(const char *buf); // Returns true iff the buffer is empty of anything except spaces, tabs or newlines.
+  /// Returns true iff the buffer is empty of anything except spaces, tabs or newlines.
+  bool empty(const char *buf); 
 
-  std::string substring(std::string wholestring, int start, int end); // Returns the substrng starting at start and ending at end. (N.B. end is actually the last character included, not one after it.)
+  /// Returns the substrng starting at start and ending at end. (N.B. end is actually the last character included, not one after it.)
+  std::string substring(std::string wholestring, int start, int end); 
 
 }
 
