312cdf9c7cba91db6057476e4f4e983308b9c240
Mike Dowman <>
2003-09-11 01:36:11 +0000

    Added CVS keywords and doxygen comments. Removed the test fuction temp's prototype.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@162 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- src/fileio/IO.h
+++ src/fileio/IO.h
@@ -1,4 +1,13 @@
-/* Copyright (C) 2003 The GPlates Consortium
+/*$Id$*/
+/// Reads in Plates format isochron files.
+/**
+ * @file
+ *
+ * Most recent change:
+ * $Author$
+ * $Date$
+ *
+ * Copyright (C) 2003 The GPlates Consortium
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License,
@@ -11,68 +20,68 @@
  *
  * Authors:
  *   Mike Dowman <mdowman@geosci.usyd.edu.au> & James Boyden <jboyden@geosci.usyd.edu.au>
-
-This file reads in files in the plates format which is as follows:-
-
-The Plates String Format
-
-Header
-
-1st Line
-
-2 digit integer    region number       (These two togeth are also called
-2digit integer     reference number  (a reference number.)
-space
-4 digit integer    string number
-space
-ascii string       geographic description of the data
-
-2nd Line
-
-space
-3 digit integer    plate identification number
-space
-6 character, 1dp float   time of appearance
-space
-6 character, 1dp float   time of disappearance
-space
-2 ascii chararacters   code for the type of data
-4digit integer      data type number
-1 ascii character   code letter for further description of the data
-3 digit integer     plate identification number
-space
-3 digit integer     colour code
-space
-5 digit integer     how many points are in the string.
-
-Each latitude-longitude line (there are any number of these - 
-each on a separate line)
-
-space
-3 digit integer
-space 
-9 character, 4dp float
-space
-9 character, 4dp float
-space
-1 digit integer
-
-Final Line
-
-space
-99.0000
-3 spaces
-99.0000
-space
-3
-
-N.B. Everything is right alligned, and any gaps on the left are filled up with spaces. (For example if a three digit integer is between 0 and 9, two spaces will be added on the left, so that it takes up three characters.)
-
-N.B. Some items are NOT delimited by spaces, and sometimes they can be missing! (e.g. the single ascii character in line two can be a space.)
-
-This code requires that each polyline contains at least two points, and that the plotter code is 3 on the first code, and 2 thereafter, except for the terminating line when it becomes 3 again. This might not be the case with all Plates files - if that is the case then this is a bug.
-
-*/
+ * 
+ * This file reads in files in the plates format which is as follows:-
+ * 
+ * The Plates String Format
+ * 
+ * Header
+ * 
+ * 1st Line
+ * 
+ * 2 digit integer    region number       (These two togeth are also called
+ * 2digit integer     reference number  (a reference number.)
+ * space
+ * 4 digit integer    string number
+ * space
+ * ascii string       geographic description of the data
+ * 
+ * 2nd Line
+ * 
+ * space
+ * 3 digit integer    plate identification number
+ * space
+ * 6 character, 1dp float   time of appearance
+ * space
+ * 6 character, 1dp float   time of disappearance
+ * space
+ * 2 ascii chararacters   code for the type of data
+ * 4digit integer      data type number
+ * 1 ascii character   code letter for further description of the data
+ * 3 digit integer     plate identification number
+ * space
+ * 3 digit integer     colour code
+ * space
+ * 5 digit integer     how many points are in the string.
+ * 
+ * Each latitude-longitude line (there are any number of these - 
+ * each on a separate line)
+ * 
+ * space
+ * 3 digit integer
+ * space 
+ * 9 character, 4dp float
+ * space
+ * 9 character, 4dp float
+ * space
+ * 1 digit integer
+ * 
+ * Final Line
+ * 
+ * space
+ * 99.0000
+ * 3 spaces
+ * 99.0000
+ * space
+ * 3
+ * 
+ * N.B. Everything is right alligned, and any gaps on the left are filled up with spaces. (For example if a three digit integer is between 0 and 9, two spaces will be added on the left, so that it takes up three characters.)
+ * 
+ * N.B. Some items are NOT delimited by spaces, and sometimes they can be missing! (e.g. the single ascii character in line two can be a space.)
+ * 
+ * This code requires that each polyline contains at least two points, and that the plotter code is 3 on the first code, and 2 thereafter, except for the terminating line when it becomes 3 again. This might not be the case with all Plates files - if that is the case then this is a bug.
+ * 
+ */
 
 #ifndef _GPLATES_FILEIO_IO_H_
 #define _GPLATES_FILEIO_IO_H_
@@ -92,42 +101,41 @@ namespace GPlatesFileIO {
 
   using GPlatesGlobal::rgid_t;
 
-  void temp(int num);
+  /** Read in a Plates format isochron file and put its contents in the map.
+   * The inputstream must point to the beginning of an already open file containing plates boundary data.  
+   */
+  void readinplateboundarydata(const char *filename, std::istream &inputstream, std::map< rgid_t, PlatesPlate> &platesdata); 
 
-  void readinplateboundarydata(const char *filename, std::istream &inputstream, std::map< rgid_t, PlatesPlate> &platesdata); // The inputstream must point to the beginning of an already open file containing plates boundary data.  
-
-  bool readIsochron(LineBuffer &lb, std::map< rgid_t, PlatesPlate> &platesdata); // given a LineBuffer, reads in the file into two strings and a list of LatLonPoints
+  bool readIsochron(LineBuffer &lb, std::map< rgid_t, PlatesPlate> &platesdata); /// given a LineBuffer, reads in the file into two strings and a list of LatLonPoints
 
   fpdata_t extractbegintime(const std::string &secondline);
 
   fpdata_t extractendtime(const std::string &secondline);
 
-  rgid_t extractplateid(const std::string &firstline); // The plateid is the first four characters of the file, which should be followed by a space.
+  rgid_t extractplateid(const std::string &firstline); /// The plateid is the first four characters of the file, which should be followed by a space.
 
-  std::string readFirstLineOfIsochronHeader(LineBuffer &lb); // takes a reference to a line buffer and returns the first line read in a string.
+    std::string readFirstLineOfIsochronHeader(LineBuffer &lb); /// takes a reference to a line buffer and returns the first line read in a string.
 
-  std::string readSecondLineOfIsochronHeader(LineBuffer &lb); // takes a reference to a line buffer and returns the first line read in a string.
+  std::string readSecondLineOfIsochronHeader(LineBuffer &lb); /// takes a reference to a line buffer and returns the first line read in a string.
 
-  void readIsochronPoints(LineBuffer &lb, std::list< LatLonPoint > &points, size_t num_points_to_expect); // Just read all the LatLon points from the file into the list points.
+    void readIsochronPoints(LineBuffer &lb, std::list< LatLonPoint > &points, size_t num_points_to_expect); /// Just read all the LatLon points from the file into the list points.
 
-  std::string readIsochronPoint(LineBuffer &lb); // Just read a line of the file from lb and return it as a string.
+  std::string readIsochronPoint(LineBuffer &lb); /// Just read a line of the file from lb and return it as a string.
 
   // *** Warning - there are two different functions both called parseLine ***
-  LatLonPoint parseLine(const std::string &line, const LineBuffer &lb, int expected_plotter_code);
+    LatLonPoint parseLine(const std::string &line, const LineBuffer &lb, int expected_plotter_code); /// Read parse the string to get a Lat and a Lon point and return them in a LatLonPoint. Will cause an exception if the expected plotter code isn't found.
   
   fpdata_t attemptToReadCoord(std::istringstream &iss, const LineBuffer &lb, const char *desc);
   
-  int attemptToReadPlotterCode(std::istringstream &iss, const LineBuffer &lb);
+  int attemptToReadPlotterCode(std::istringstream &iss, const LineBuffer &lb); /// Try to read a particular Lat or Lon coordinate from the istringstream.
   
-  void parseTerminatingLine(const std::string &line, const LineBuffer &lb);
+  void parseTerminatingLine(const std::string &line, const LineBuffer &lb); /// Check that the final line conforms to the expected format.
     
-  void parseTerminatingLine(const std::string &line, const LineBuffer &lb);
-  
-  size_t parseLine(const std::string &line, const LineBuffer &lb); // This just returns the last item in the second line of the file (which is an integer).
+    size_t parseLine(const std::string &line, const LineBuffer &lb); /// This just returns the last item in the second line of the file (which is an integer).
 
 }
 
-#endif
+#endif // _GPLATES_FILEIO_IO_H_
 
 
 
