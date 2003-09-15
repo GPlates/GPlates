394478aa93c254580077214335aaa74ebc6fdb14
James Boyden <>
2003-09-15 22:04:49 +0000

    Some pretty major changes, due to the following changes in design: - the types which were defined in "PrimitiveDataTypes.h" have now been moved    into this file.  For now, at least, they will all be PLATES-specific.    Thus, the types now defined in this file are: LatLonPoint, EulerRotation,    FiniteRotation, PolyLineHeader, PolyLine and Plate. - creation of a 'PlatesParser' namespace, into which all the PLATES-format-    specific code will go. - this header file now declares some functions to parse lines of input    from a PLATES file and instantiate some of the types.  The corresponding    source file will define these functions.  This is intended to reduce the    possibility of these types being instantiated with bogus values in their    members.  Hence: none of these types possess default constructors.  Also,    some of these types now have no public constructor;  instead, they have    public static member functions (or friend global functions) to parse a    line of input and invoke a protected constructor. - LatLonPoint now also has the static member functions 'isValidLat' and    'isValidLon' to check whether a given fp value is a valid latitude/    longitude respectively.  These functions are used throughout the code,    where previously the values were all hard-coded... Bad Idea.
    Listing the changes in more detail:
    - addition of 'ParseRotationLine' to parse a line in the rotation file
       and return a FiniteRotation.  Friend of LatLonPoint, EulerRotation and
       FiniteRotation.
    - addition of 'ParseBoundaryLine' and 'ParseTermBoundaryLine' into
       LatLonPoint, to parse lines of the boundary file.
    - addition of 'ParseLines' and 'ParseSecondLine' into PolyLineHeader
       to parse the lines of a polyline header in the boundary file.
    - the following classes now have protected constructors: LatLonPoint,
       EulerRotation, FiniteRotation and PolyLineHeader.
    - various members were added to some of the classes, to store "all the
       relevant information" in one place (at the cost of some duplication
       and redundancy of data).
    
    
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@169 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- src/fileio/PlatesDataTypes.h
+++ src/fileio/PlatesDataTypes.h
@@ -2,9 +2,14 @@
 
 /**
  * \file 
- * This file contains a collection of data types which will be used by
- * the PLATES-format parser, and will probably _not_ be used by any other
- * parser.
+ * This file contains a collection of data types which will be used
+ * by the PLATES-format parser.  They will probably _not_ be used
+ * by any other parser.  These types are really only intended to be
+ * temporary place-holders, providing data-types for the parsing
+ * before the geometry engine takes over.  These types are currently
+ * all structs due to their primitive and temporary nature;
+ * if necessary, they may become classes (with a correspondingly more
+ * complicated interface of accessors and modifiers).
  *
  * Most recent change:
  *   $Author$
@@ -30,59 +35,176 @@
 
 #include <list>
 #include <string>
-#include "../global/types.h"
-#include "PrimitiveDataTypes.h"
-#include "../geo/TimeWindow.h"
+
+#include "LineBuffer.h"
+#include "global/types.h"
+#include "geo/TimeWindow.h"
 
 namespace GPlatesFileIO
 {
-	using namespace GPlatesGlobal;
-
-	struct PlatesPolyLineHeader {
-
-		std::string _first_line, _second_line;  // stores the whole line
-		rgid_t      _plate_id;
-
-		GPlatesGeo::TimeWindow _lifetime;
-
-		// no default constructor
-
-		PlatesPolyLineHeader(const std::string &first_line,
-		 const std::string &second_line,
-		 const rgid_t &plate_id,
-		 GPlatesGeo::TimeWindow &lifetime)
-
-		 : _first_line(first_line),
-		   _second_line(second_line),
-		   _plate_id(plate_id),
-		   _lifetime(lifetime) {  }
-	};
+	namespace PlatesParser
+	{
+		using namespace GPlatesGlobal;
 
 
-	struct PlatesPolyLine {
+		struct FiniteRotation;
 
-		PlatesPolyLineHeader     _header;
-		std::list< LatLonPoint > _points;
-
-		// no default constructor
-
-		PlatesPolyLine(const PlatesPolyLineHeader &header)
-		 : _header(header) {  }
-	};
+		FiniteRotation ParseRotationLine(const LineBuffer &lb,
+		 const std::string &line);
 
 
-	struct PlatesPlate {
+		struct LatLonPoint {
 
-		rgid_t                      _plate_id;
-		std::list< PlatesPolyLine > _polylines;
+				friend FiniteRotation
+				ParseRotationLine(const LineBuffer &lb,
+				 const std::string &line);
 
-	  // N.B. we must have a default constructor so that PlatesPlates can be stored in a map.
+			public:
+				static LatLonPoint
+				ParseBoundaryLine(const LineBuffer &lb,
+				 const std::string &line,
+				 int expected_plotter_code);
 
-	  PlatesPlate() {}
+				static void
+				ParseTermBoundaryLine(const LineBuffer &lb,
+				 const std::string &line,
+				 int expected_plotter_code);
 
-		PlatesPlate(const rgid_t &plate_id)
-		 : _plate_id(plate_id) {  }
-	};
+				static bool isValidLat(const fpdata_t &val);
+				static bool isValidLon(const fpdata_t &val);
+
+				fpdata_t _lat, _lon;
+
+				// no default constructor
+
+			protected:
+
+				LatLonPoint(const fpdata_t &lat,
+				 const fpdata_t &lon)
+				 : _lat(lat), _lon(lon) {  }
+		};
+
+
+		struct EulerRotation {
+
+				friend FiniteRotation
+				ParseRotationLine(const LineBuffer &lb,
+				 const std::string &line);
+
+			public:
+				LatLonPoint _pole;
+				fpdata_t    _angle;  // degrees
+
+				// no default constructor
+
+			protected:
+				EulerRotation(const LatLonPoint &pole,
+				 const fpdata_t &angle)
+				 : _pole(pole), _angle(angle) {  }
+		};
+
+
+		struct FiniteRotation {
+
+				friend FiniteRotation
+				ParseRotationLine(const LineBuffer &lb,
+				 const std::string &line);
+
+			public:
+				fpdata_t      _time;  // Millions of years ago
+				rgid_t        _moving_plate, _fixed_plate;
+				EulerRotation _rot;
+				std::string   _comment;
+
+				// no default constructor
+
+			protected:
+				FiniteRotation(const fpdata_t &time,
+				 const rgid_t &moving_plate,
+				 const rgid_t &fixed_plate,
+				 const EulerRotation &rot,
+				 const std::string &comment)
+				 : _time(time),
+				   _moving_plate(moving_plate),
+				   _fixed_plate(fixed_plate),
+				   _rot(rot),
+				   _comment(comment) {  }
+		};
+
+
+		struct PolyLineHeader {
+
+			public:
+				static PolyLineHeader
+				ParseLines(const LineBuffer &lb,
+				 const std::string &first_line,
+				 const std::string &second_line);
+
+				static void
+				ParseSecondLine(const LineBuffer &lb,
+				 const std::string &line, rgid_t &plate_id,
+				 fpdata_t &age_appear, fpdata_t &age_disappear,
+				 size_t &num_points);
+
+				// store both lines in their original format
+				std::string _first_line, _second_line;
+
+				/*
+				 * the plate id of the plate
+				 * to which this polyline belongs.
+				 */
+				rgid_t _plate_id;
+
+				/*
+				 * the lifetime of this polyline, from
+				 * age of appearance to age of disappearance.
+				 */
+				GPlatesGeo::TimeWindow _lifetime;
+
+				// the number of points in this polyline
+				size_t _num_points;
+
+				// no default constructor
+
+			protected:
+
+				PolyLineHeader(const std::string &first_line,
+				 const std::string &second_line,
+				 const rgid_t &plate_id,
+				 const GPlatesGeo::TimeWindow &lifetime,
+				 size_t num_points)
+
+				 : _first_line(first_line),
+				   _second_line(second_line),
+				   _plate_id(plate_id),
+				   _lifetime(lifetime),
+				   _num_points(num_points) {  }
+		};
+
+
+		struct PolyLine {
+
+			PolyLineHeader           _header;
+			std::list< LatLonPoint > _points;
+
+			// no default constructor
+
+			PolyLine(const PolyLineHeader &header)
+			 : _header(header) {  }
+		};
+
+
+		struct Plate {
+
+			rgid_t                _plate_id;
+			std::list< PolyLine > _polylines;
+
+			// no default constructor
+
+			Plate(const rgid_t &plate_id)
+			 : _plate_id(plate_id) {  }
+		};
+	}
 }
 
 #endif  // _GPLATES_FILEIO_PLATESDATATYPES_H_
