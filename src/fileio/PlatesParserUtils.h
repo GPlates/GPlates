72d020f97ebb707a757f47c1c56ab0bb39a697f0
James Boyden <>
2003-09-16 01:17:14 +0000

    Header file for new source file "PlatesParserUtils.cc".
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@175 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

---
+++ src/fileio/PlatesParserUtils.h
@@ -0,0 +1,61 @@
+/* $Id$ */
+
+/**
+ * \file 
+ * This file contains a collection of data types which will be used by
+ * the PLATES-format parser, and will probably _not_ be used by any other
+ * parser.
+ *
+ * Most recent change:
+ *   $Author$
+ *   $Date$
+ * 
+ * Copyright (C) 2003 The GPlates Consortium
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Authors:
+ *   James Boyden <jboyden@geosci.usyd.edu.au>
+ */
+
+#ifndef _GPLATES_FILEIO_PLATESPARSERUTILS_H_
+#define _GPLATES_FILEIO_PLATESPARSERUTILS_H_
+
+#include <sstream>
+#include <string>
+
+#include "global/types.h"
+#include "LineBuffer.h"
+
+
+namespace GPlatesFileIO
+{
+	namespace PlatesParser
+	{
+		using namespace GPlatesGlobal;
+
+		int attemptToReadInt(const LineBuffer &lb,
+			std::istringstream &iss, const char *desc);
+
+		fpdata_t attemptToReadFloat(const LineBuffer &lb,
+			std::istringstream &iss, const char *desc);
+
+		std::string attemptToReadString(const LineBuffer &lb,
+			std::istringstream &iss, const char *desc);
+
+		rgid_t attemptToReadRGID(const LineBuffer &lb,
+			std::istringstream &iss, const char *desc);
+
+		int attemptToReadPlotterCode(const LineBuffer &lb,
+			std::istringstream &iss);
+	}
+}
+
+#endif  // _GPLATES_FILEIO_PLATESPARSERUTILS_H_
