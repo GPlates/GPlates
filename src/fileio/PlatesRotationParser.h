cce8dfa72c7f228b0b6fd6fed9d176752a5968f3
James Boyden <>
2003-09-16 00:44:35 +0000

    This is a rewrite of what used to be in "RotationDataFileReader.h".  Changes: - change filename, of course.  "RotationDataFileReader.h" was too general and    vague.  In a directory full of different readers and writers for different    file formats, the name of the file should suggest the format. - fix indentation and line-length to match style guide. - fix doxygenated comments to match style guide. - place functions and other definitions in 'PlatesParser' namespace. - alter function prototypes to match new code in "PlatesRotationParser.cc".
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@172 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

---
+++ src/fileio/PlatesRotationParser.h
@@ -0,0 +1,61 @@
+/*$Id$*/
+
+/**
+ * @file
+ *
+ * Most recent change:
+ * $Author$
+ * $Date$
+ *
+ * Copyright (C) 2003 The GPlates Consortium
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Authors:
+ *   Mike Dowman <mdowman@geosci.usyd.edu.au>
+ *   James Boyden <jboyden@geosci.usyd.edu.au>
+ */
+
+#ifndef _GPLATES_FILEIO_PLATESROTATIONPARSER_H_
+#define _GPLATES_FILEIO_PLATESROTATIONPARSER_H_
+
+#include <istream>
+#include <string>
+#include <map>
+
+#include "global/types.h"
+#include "PlatesDataTypes.h"
+#include "LineBuffer.h"
+
+
+namespace GPlatesFileIO
+{
+	namespace PlatesParser
+	{
+		using GPlatesGlobal::fpdata_t;
+		using GPlatesGlobal::rgid_t;
+
+		typedef std::multimap< fpdata_t, FiniteRotation >
+		 FiniteRotationsOfPlateMap;
+
+		typedef std::map< rgid_t, FiniteRotationsOfPlateMap >
+		 RotationDataMap;
+
+		void ReadInPlateRotationData(const char *filename,
+		 std::istream &input_stream, RotationDataMap &rotation_data);
+
+		void ReadRotation(LineBuffer &lb,
+		 RotationDataMap &rotation_data);
+
+		void ReadRotationLine(LineBuffer &lb, std::string &str);
+	}
+}
+
+#endif // _GPLATES_FILEIO_PLATESROTATIONPARSER_H_
