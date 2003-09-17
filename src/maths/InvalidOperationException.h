a4445b1e8886af9eeeec44c822ec3a14570cac99
James Boyden <>
2003-09-17 19:39:49 +0000

    This exception used by the FiniteRotation class.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@217 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

---
+++ src/maths/InvalidOperationException.h
@@ -0,0 +1,66 @@
+/* $Id$ */
+
+/**
+ * \file 
+ * File specific comments.
+ *
+ * Most recent change:
+ *   $Author$
+ *   $Date$
+ * 
+ * Copyright (C) 2003 The GPlates Consortium
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Authors:
+ *   James Boyden <jboyden@geosci.usyd.edu.au>
+ */
+
+#ifndef _GPLATES_MATHS_INVALIDOPERATIONEXCEPTION_H_
+#define _GPLATES_MATHS_INVALIDOPERATIONEXCEPTION_H_
+
+#include "MathematicalException.h"
+
+namespace GPlatesMaths
+{
+	/**
+	 * The Exception thrown when an invalid mathematical operation is
+	 * attempted.
+	 */
+	class InvalidOperationException
+		: public MathematicalException
+	{
+		public:
+			/**
+			 * @param msg is a description of the conditions
+			 * which cause the invariant to be violated.
+			 */
+			InvalidOperationException(const char *msg)
+				: _msg(msg) {  }
+
+			virtual
+			~InvalidOperationException() {  }
+
+		protected:
+			virtual const char *
+			ExceptionName() const {
+
+				return "InvalidOperationException";
+			}
+
+			virtual std::string
+			Message() const { return _msg; }
+
+		private:
+			std::string _msg;
+	};
+}
+
+#endif  // _GPLATES_MATHS_INVALIDOPERATIONEXCEPTION_H_
