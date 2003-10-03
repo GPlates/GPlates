28713cb45e82d5e4ce4d31384a9dd7db625a9ab1
Hamish Ivey-Law <>
2003-10-03 11:51:21 +0000

    Skeleton of the  implementation of the controls system.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@307 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

---
+++ src/controls/Reconstruct.cc
@@ -0,0 +1,39 @@
+/* $Id$ */
+
+/**
+ * @file 
+ * File specific comments.
+ *
+ * Most recent change:
+ *   $Author$
+ *   $Date$
+ * 
+ * Copyright (C) 2003 The GPlates Consortium
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Authors:
+ *   Hamish Ivey-Law <hlaw@geosci.usyd.edu.au>
+ */
+
+#include "Reconstruct.h"
+
+using namespace GPlatesControls;
+
+void
+Reconstruct::Time(const GPlatesMaths::real_t&)
+{
+}
+
+void
+Reconstruct::Animation(const GPlatesMaths::real_t&, 
+					   const GPlatesMaths::real_t&)
+{
+}
---
+++ src/controls/View.cc
@@ -0,0 +1,34 @@
+/* $Id$ */
+
+/**
+ * @file 
+ * File specific comments.
+ *
+ * Most recent change:
+ *   $Author$
+ *   $Date$
+ * 
+ * Copyright (C) 2003 The GPlates Consortium
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Authors:
+ *   Hamish Ivey-Law <hlaw@geosci.usyd.edu.au>
+ */
+
+#include "View.h"
+
+using namespace GPlatesControls;
+
+std::string
+View::DocumentMetadata()
+{
+	return "";
+}
