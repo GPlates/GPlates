bfe039bc4e7ed15de2bc0c559bc80dab4cad3a3f
James Boyden <>
2003-10-04 09:00:35 +0000

    Initial commit.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@317 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

---
+++ src/global/ControlFlowException.h
@@ -0,0 +1,64 @@
+/* $Id$ */
+
+/**
+ * \file 
+ * File specific comments.
+ *
+ * Most recent change:
+ *   $Author$
+ *   $Date$
+ * 
+ * Copyright (C) 2003 The GPlates Consortium
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Authors:
+ *   James Boyden <jboyden@geosci.usyd.edu.au>
+ */
+
+#ifndef _GPLATES_GLOBAL_CONTROLFLOWEXCEPTION_H_
+#define _GPLATES_GLOBAL_CONTROLFLOWEXCEPTION_H_
+
+#include "Exception.h"
+
+namespace GPlatesGlobal
+{
+	/**
+	 * Should be thrown when a section of code is reached
+	 * which should not be logically reachable.
+	 */
+	class ControlFlowException : public Exception
+	{
+		public:
+			/**
+			 * @param msg is a message describing the situation.
+			 */
+			ControlFlowException(const char *msg)
+				: _msg(msg) {  }
+
+			virtual
+			~ControlFlowException() {  }
+
+		protected:
+			virtual const char *
+			ExceptionName() const {
+
+				return "ControlFlowException";
+			}
+
+			virtual std::string
+			Message() const { return _msg; }
+
+		private:
+			std::string _msg;
+	};
+}
+
+#endif  // _GPLATES_GLOBAL_CONTROLFLOWEXCEPTION_H_
