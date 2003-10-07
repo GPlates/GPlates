ecaf7d1af410f17fb31a92e09878be54aee8a824
James Boyden <>
2003-10-07 19:04:14 +0000

    * Changed parameters of the functions 'Time' and 'Animation' from 'GPlatesMaths::real_t' to 'GPlatesGlobal::fpdata_t'. * Improved some comments. * Added using-namespace directives for GPlatesGlobal, GPlatesState and GPlatesMaths. * Added the functions 'WarpToTime', 'PopulateRotatableData', 'CheckRotation' and the inline template function 'ListContainsElem'. * In the interests of getting this code done fast, added the static const variable 'RIDOfGlobe', which really should be moved somewhere more out-of- the-way.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@383 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- src/controls/Reconstruct.cc
+++ src/controls/Reconstruct.cc
@@ -24,44 +24,155 @@
  *   James Boyden <jboyden@geosci.usyd.edu.au>
  */
 
+#include <list>
+#include <map>
+#include <utility>  /* std::pair */
+#include <algorithm>  /* std::find */
+
 #include "Reconstruct.h"
 #include "Dialogs.h"
+#include "global/types.h"
 #include "state/Data.h"
 #include "state/Layout.h"
 #include "controls/View.h"
+#include "maths/FiniteRotation.h"
 
 
+using namespace GPlatesGlobal;
+using namespace GPlatesState;
 using namespace GPlatesControls;
+using namespace GPlatesMaths;
 
 
-void
-Reconstruct::Time(const GPlatesMaths::real_t &time)
-{
-	if (GPlatesState::Data::GetDataGroup() == NULL) {
+typedef std::map< rid_t, FiniteRotation > RotationsByPlate;
 
-		Dialogs::ErrorMessage("No data to reconstruct",
-		 "Cannot perform a reconstruction, since there is no"
-		 " data loaded.",
-		 "Cannot perform reconstruction.");
 
-		return;
+enum status { SUCCESSFUL, CANNOT_BE_ROTATED };
+
+static const rid_t RIDOfGlobe = 0;
+
+
+template< typename T >
+inline bool
+ListContainsElem(const std::list< T > &l, const T &e) {
+
+	return (std::find(l.begin(), l.end(), e) != l.end());
+}
+
+
+enum status
+CheckRotation(rid_t plate_id, real_t t,
+	RotationsByPlate &rot_cache, std::list< rid_t > &cannot_be_rotated,
+	const Data::RotationMap_type &histories) {
+
+	// Firstly, check whether this plate can be rotated
+	Data::RotationMap_type::const_iterator i = histories.find(plate_id);
+	if (i == histories.end()) {
+
+		// this plate has no rotation history, so cannot rotate it
+		cannot_be_rotated.push_back(plate_id);
+		return CANNOT_BE_ROTATED;
 	}
-	if (GPlatesState::Data::GetRotationHistories() == NULL) {
+	if ( ! i->second.isDefinedAtTime(t)) {
 
-		Dialogs::ErrorMessage("No rotation data",
-		 "Cannot perform a reconstruction, since there is no"
-		 " rotation data loaded.",
-		 "Cannot perform reconstruction.");
+		// this plate's rotation history is not defined at this time
+		cannot_be_rotated.push_back(plate_id);
+		return CANNOT_BE_ROTATED;
+	}
 
-		return;
+	/*
+	 * Otherwise, we know that this plate has a rotation history defined
+	 * at time 't', which means there must exist a finite rotation for
+	 * time 't'.
+	 *
+	 * Now we need to check upwards in the rotation hierarchy, to ensure
+	 * that those plates are also rotatable to time 't'.
+	 */
+	RotationHistory::const_iterator info = i->second.atTime(t);
+	rid_t fixed_plate_id = (*info).fixedPlate();
+
+	if (fixed_plate_id == RIDOfGlobe) {
+
+		/* the fixed plate is the globe, which is always defined,
+		 * so the rotation of our plate is defined */
+		FiniteRotation rot = (*info).finiteRotationAtTime(t);
+		rot_cache.insert(std::make_pair(plate_id, rot));
+		return SUCCESSFUL;
+	}
+
+	/*
+	 * Now, check in caches to see if the fixed plate has already been
+	 * dealt with.
+	 */
+	RotationsByPlate::const_iterator j = rot_cache.find(fixed_plate_id);
+	if (j != rot_cache.end()) {
+
+		/* the fixed plate has already been dealt with, and is known
+		 * to be rotatable, so the rotation of our plate is defined */
+		FiniteRotation rot = (*info).finiteRotationAtTime(t);
+		rot_cache.insert(std::make_pair(plate_id, j->second * rot));
+		return SUCCESSFUL;
+	}
+	if (ListContainsElem(cannot_be_rotated, fixed_plate_id)) {
+
+		/* the fixed plate (or some plate in the hierarchy above it)
+		 * is known not to be rotatable */
+		cannot_be_rotated.push_back(plate_id);
+		return CANNOT_BE_ROTATED;
+	}
+
+	/*
+	 * There is no cached result, so we must query this the hard way
+	 * (recursively).
+	 */
+	enum status query_status = CheckRotation(fixed_plate_id, t, rot_cache,
+	 cannot_be_rotated, histories);
+	if (query_status == CANNOT_BE_ROTATED) {
+
+		/* the fixed plate (or some plate in the hierarchy above it)
+		 * has been determined to be not rotatable */
+		cannot_be_rotated.push_back(plate_id);
+		return CANNOT_BE_ROTATED;
+	}
+	// else, the fixed plate has been determined to be rotatable
+
+	/*
+	 * Since the fixed plate is rotatable, it must be in the rotation cache,
+	 * so this search should not fail.
+	 */
+	RotationsByPlate::const_iterator k = rot_cache.find(fixed_plate_id);
+
+	FiniteRotation rot = (*info).finiteRotationAtTime(t);
+	rot_cache.insert(std::make_pair(plate_id, k->second * rot));
+	return SUCCESSFUL;
+}
+
+
+static void
+PopulateRotatableData(const Data::DrawableMap_type *plates_to_draw,
+	RotationsByPlate &rot_cache, const real_t &t) {
+
+	std::list< rid_t > cannot_be_rotated;
+
+	// assume this is non-NULL
+	const Data::RotationMap_type *histories = Data::GetRotationHistories();
+
+	// for each plate to draw...
+	for (Data::DrawableMap_type::const_iterator i = plates_to_draw->begin();
+	     i != plates_to_draw->end();
+	     i++) {
+
+		// get the plate id
+		rid_t plate_id = i->first;
+
+		CheckRotation(plate_id, t, rot_cache, cannot_be_rotated,
+		 *histories);
 	}
 }
 
 
 static void
-WarpToPresent() {
-
-	using namespace GPlatesState;
+WarpToTime(const fpdata_t &t) {
 
 	/*
 	 * Assume that if Data::GetDataGroup() is not NULL, then
@@ -70,12 +181,87 @@ WarpToPresent() {
 	Data::DrawableMap_type *drawable_data = Data::GetDrawableData();
 	Layout::Clear();
 
-	// for each plate...
+	RotationsByPlate rotatable_data;
+	PopulateRotatableData(drawable_data, rotatable_data, real_t(t));
+
+	// for each drawable plate...
 	for (Data::DrawableMap_type::const_iterator i = drawable_data->begin();
 	     i != drawable_data->end();
 	     i++) {
 
-		// get the drawable data items which move with that plate
+		rid_t plate_id = i->first;
+
+		RotationsByPlate::const_iterator k =
+		 rotatable_data.find(plate_id);
+		if (k == rotatable_data.end()) {
+
+			// the current plate is not rotatable to time 't'
+			continue;
+		}
+		FiniteRotation rot = k->second;  // the rotation for this plate
+
+		// get the drawable data items which move with this plate
+		const Data::DrawableDataSet &items = i->second;
+
+		// then for each item...
+		for (Data::DrawableDataSet::const_iterator j = items.begin();
+		     j != items.end();
+		     j++) {
+
+			// if that item exists at time 't'
+			if ((*j)->ExistsAtTime(t)) {
+
+				// rotate it and draw it
+				(*j)->RotateAndDraw(rot);
+			}
+		}
+	}
+	GPlatesControls::View::Redisplay();
+}
+
+
+void
+Reconstruct::Time(const fpdata_t &time)
+{
+	if (Data::GetDataGroup() == NULL) {
+
+		Dialogs::ErrorMessage("No data to reconstruct",
+		 "Cannot perform a reconstruction, since there is no"
+		 " data loaded.",
+		 "Cannot perform reconstruction.");
+
+		return;
+	}
+	if (Data::GetRotationHistories() == NULL) {
+
+		Dialogs::ErrorMessage("No rotation data",
+		 "Cannot perform a reconstruction, since there is no"
+		 " rotation data loaded.",
+		 "Cannot perform reconstruction.");
+
+		return;
+	}
+
+	WarpToTime(time);
+}
+
+
+static void
+WarpToPresent() {
+
+	/*
+	 * Assume that if Data::GetDataGroup() is not NULL, then
+	 * Data::GetDrawableData() is also not NULL.
+	 */
+	Data::DrawableMap_type *drawable_data = Data::GetDrawableData();
+	Layout::Clear();
+
+	// for each drawable plate...
+	for (Data::DrawableMap_type::const_iterator i = drawable_data->begin();
+	     i != drawable_data->end();
+	     i++) {
+
+		// get the drawable data items which move with this plate
 		const Data::DrawableDataSet &items = i->second;
 
 		// then for each item...
@@ -98,7 +284,7 @@ WarpToPresent() {
 void
 Reconstruct::Present() {
 
-	if (GPlatesState::Data::GetDataGroup() == NULL) {
+	if (Data::GetDataGroup() == NULL) {
 
 		Dialogs::ErrorMessage("No data to reconstruct",
 		 "Cannot perform a reconstruction, since there is no"
@@ -114,11 +300,11 @@ Reconstruct::Present() {
 
 
 void
-Reconstruct::Animation(const GPlatesMaths::real_t &start_time,
-	const GPlatesMaths::real_t &end_time,
-	const GPlatesGlobal::integer_t &nsteps)
+Reconstruct::Animation(const fpdata_t &start_time,
+	const fpdata_t &end_time,
+	const integer_t &nsteps)
 {
-	if (GPlatesState::Data::GetDataGroup() == NULL) {
+	if (Data::GetDataGroup() == NULL) {
 
 		Dialogs::ErrorMessage("No data to reconstruct",
 		 "Cannot perform a reconstruction, since there is no"
@@ -127,7 +313,7 @@ Reconstruct::Animation(const GPlatesMaths::real_t &start_time,
 
 		return;
 	}
-	if (GPlatesState::Data::GetRotationHistories() == NULL) {
+	if (Data::GetRotationHistories() == NULL) {
 
 		Dialogs::ErrorMessage("No rotation data",
 		 "Cannot perform a reconstruction, since there is no"
