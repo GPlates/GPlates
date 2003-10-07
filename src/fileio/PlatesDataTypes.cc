ced6eecc47f51db7202ed6ad78b22b4678f386a5
James Boyden <>
2003-10-07 12:09:41 +0000

    Longitudes in the PLATES formats may now lie in the range [-360.0, 360.0].
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@375 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- src/fileio/PlatesDataTypes.cc
+++ src/fileio/PlatesDataTypes.cc
@@ -279,14 +279,14 @@ LatLonPoint::isValidLat(const fpdata_t &val) {
 
 /**
  * Return whether a given value is a valid longitude.
- * For the PLATES format, the valid range of longitude is [-180.0, 180.0].
- * Note that this is different to the rest of GPlates, which uses the
- * half-open range (-180.0, 180.0].
+ * For the PLATES formats, we must accept longitudes in the range
+ * [-360.0, 360.0].  Note that this is different to the rest of GPlates,
+ * which uses the half-open range (-180.0, 180.0].
  */
 bool
 LatLonPoint::isValidLon(const fpdata_t &val) {
 
-	return (-180.0 <= val && val <= 180.0);
+	return (-360.0 <= val && val <= 360.0);
 }
 
 
