b406cfa9b97e6c58cd5571de137dd8dbdd6bdb89
James Boyden <>
2004-02-03 01:31:00 +0000

    The class 'FPValidator' provides a wxWindows-style validator for floating-point numbers.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@627 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

---
+++ src/gui/FPValidator.cc
@@ -0,0 +1,232 @@
+/* $Id$ */
+
+/**
+ * @file 
+ * File specific comments.
+ *
+ * Most recent change:
+ *   $Author$
+ *   $Date$
+ * 
+ * Copyright (C) 2003 The GPlates Consortium
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Authors:
+ *   James Boyden <jboyden@geosci.usyd.edu.au>
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include <cctype>
+#include <wx/defs.h>  /* wxCHECK_MSG */
+#include <wx/intl.h>  /* _ */
+#include <wx/msgdlg.h>
+#include "FPValidator.h"
+
+
+GPlatesGui::FPValidator::FPValidator(const FPValidator &other) :
+ wxValidator(), _style(other._style), _val(other._val) {
+
+	wxValidator::Copy(other);
+}
+
+
+bool
+GPlatesGui::FPValidator::TransferFromWindow() {
+
+	wxTextCtrl *ctrl = GetTextCtrl();
+	if (ctrl == NULL) return false;
+
+	(*_val) = ctrl->GetValue();
+	return true;
+}
+
+
+bool
+GPlatesGui::FPValidator::TransferToWindow() {
+
+	wxTextCtrl *ctrl = GetTextCtrl();
+	if (ctrl == NULL) return false;
+
+	ctrl->SetValue(*_val);
+	return true;
+}
+
+
+bool
+GPlatesGui::FPValidator::Validate(wxWindow *parent) {
+
+	wxTextCtrl *ctrl = GetTextCtrl();
+	if (ctrl == NULL) return false;
+
+	// If window is disabled, simply return
+	if ( ! ctrl->IsEnabled()) return true;
+
+	wxString val(ctrl->GetValue());
+	wxString error_msg;
+	if ( ! CheckValue(val, error_msg)) {
+
+		// invalid string
+		wxMessageBox(error_msg, _("Invalid field"),
+		             wxOK | wxICON_EXCLAMATION, parent);
+		return false;
+	}
+	return true;
+}
+
+
+namespace {
+
+	inline bool
+	isDecimalPlace(char c) {
+
+		// Allow ',' for Europe.
+		return ((c == '.') || (c == ','));
+	}
+}
+
+
+bool
+GPlatesGui::FPValidator::CheckValue(wxString &val, wxString &error_msg)
+ const {
+
+	size_t len = val.Len();
+	if (len == 0) {
+
+		error_msg =
+		 _("The field is empty.\n"
+		  "A valid number is required.");
+		return false;
+	}
+
+	// The index of the current character
+	size_t i = 0;
+
+	// Allow field to begin with '-' in *some* cases.
+	if (val.GetChar(0) == '-') {
+
+		if (len == 1) {
+
+			// Field is empty except for '-'.
+			error_msg =
+			 _("'-' is not a valid number.\n"
+			  "A valid number is required.");
+			return false;
+		}
+
+		// else, field contains '-' followed by other stuff.
+		if (_style & DISALLOW_NEG) {
+
+			// Negative numbers are not allowed.
+			wxString fmt =
+			 _("'%s' appears to be negative,\n"
+			 "which is invalid for this field.");
+			error_msg.Printf(fmt, val.c_str());
+			return false;
+
+		} else {
+
+			// Negative numbers are allowed.
+			i++;
+		}
+	}
+
+	// Whether a decimal place has yet been encountered.
+	bool have_read_dp = false;
+
+	// Whether a digit has yet been encountered.
+	bool have_read_digit = false;
+
+	// Whether a non-zero digit has yet been encountered.
+	bool have_read_nz_digit = false;
+
+	for ( ; i < len; i++) {
+
+		char c = val.GetChar(i);
+		if (isDecimalPlace(c) && ( ! have_read_dp)) {
+
+			have_read_dp = true;
+			continue;
+		}
+		if (std::isdigit(c)) {
+
+			have_read_digit = true;
+			if (c != '0') have_read_nz_digit = true;
+			continue;
+		}
+
+		// else, contents of field are invalid
+		wxString fmt = 
+		 _("'%s' is not a valid number.\n"
+		  "A valid number is required.");
+		error_msg.Printf(fmt, val.c_str());
+		return false;
+	}
+
+	// Whole string has been processed.
+	if ( ! have_read_digit) {
+
+		// Processed whole string, but found no digits.
+		wxString fmt =
+		 _("'%s' is not a valid number.\n"
+		  "A valid number is required.");
+		error_msg.Printf(fmt, val.c_str());
+		return false;
+	}
+	if ((_style & DISALLOW_ZERO) && ( ! have_read_nz_digit)) {
+
+		// Processed whole string, but found no non-zero digits.
+		// Zero is not allowed.
+		wxString fmt =
+		 _("'%s' appears to be zero,\n"
+		 "which is invalid for this field.");
+		error_msg.Printf(fmt, val.c_str());
+		return false;
+	}
+
+	return true;
+}
+
+
+wxTextCtrl *
+GPlatesGui::FPValidator::GetTextCtrl() const {
+
+	if ( ! CheckValidator()) {
+
+		// Validator unable to operate properly.
+		return NULL;
+	}
+
+	wxTextCtrl *ctrl = dynamic_cast< wxTextCtrl * >(m_validatorWindow);
+	if (ctrl == NULL) {
+
+		// CheckValidator should already have *checked* this...
+		std::cerr << "Internal error in function "
+		 << "'GPlatesGui::FPValidator::TransferFromWindow':\n"
+		 << "dynamic_cast< wxTextCtrl * > failed!" << std::endl;
+		std::exit(1);
+	}
+	return ctrl;
+}
+
+
+bool
+GPlatesGui::FPValidator::CheckValidator() const {
+
+	wxCHECK_MSG( m_validatorWindow, false,
+	             _("No window associated with validator") );
+	wxCHECK_MSG( m_validatorWindow->IsKindOf(CLASSINFO(wxTextCtrl)), false,
+	             _("FPValidator is only for wxTextCtrls") );
+	wxCHECK_MSG( _val, false,
+	             _("No variable storage for validator") );
+
+	return true;
+}
---
+++ src/gui/FPValidator.h
@@ -0,0 +1,116 @@
+/* $Id$ */
+
+/**
+ * @file 
+ * File specific comments.
+ *
+ * Most recent change:
+ *   $Author$
+ *   $Date$
+ * 
+ * Copyright (C) 2003 The GPlates Consortium
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Authors:
+ *   James Boyden <jboyden@geosci.usyd.edu.au>
+ */
+
+#ifndef _GPLATES_GUI_FPVALIDATOR_H_
+#define _GPLATES_GUI_FPVALIDATOR_H_
+
+#include <wx/validate.h>
+#include <wx/textctrl.h>
+#include <wx/string.h>
+
+
+namespace GPlatesGui
+{
+	/**
+	 * A wxWindows-style validator, used to validate floating-point data.
+	 */
+	class FPValidator : public wxValidator {
+
+		public:
+			// The values in this enumeration will be bitwise ORed.
+			enum {
+
+				DISALLOW_NEG = 1,
+				DISALLOW_ZERO = 2
+			};
+
+
+			FPValidator(long style = 0, wxString *val = NULL) :
+			 wxValidator(), _style(style), _val(val) {  }
+
+
+			FPValidator(const FPValidator &other);
+
+
+			~FPValidator() {  }
+
+
+			/**
+			 * Return an identical copy of this validator.
+			 *
+			 * This is necessary because validators are passed
+			 * to control constructors as references, which must
+			 * be cloned internally.
+			 */
+			virtual wxObject *
+			Clone() const {
+
+				return new FPValidator(*this);
+			}
+
+			/**
+			 * Transfer the data from the control.
+			 *
+			 * Return false if there is a problem.
+			 */
+			virtual bool TransferFromWindow();
+
+			/**
+			 * Transfer the data to the control.
+			 *
+			 * Return false if there is a problem.
+			 */
+			virtual bool TransferToWindow();
+
+			/**
+			 * Called when the value in the associated window
+			 * must be validated.
+			 *
+			 * Return true if the data in the control is valid.
+			 * Return false, and pop up an error dialog, if the
+			 * data is not valid.
+			 */
+			virtual bool Validate(wxWindow *parent);
+
+#if 0
+			virtual void OnChar(wxKeyEvent &ev);
+
+			DECLARE_EVENT_TABLE()
+#endif
+
+		private:
+			long _style;
+			wxString *_val;
+
+			wxTextCtrl *GetTextCtrl() const;
+
+			bool CheckValidator() const;
+
+			bool CheckValue(wxString &val,
+			                wxString &error_msg) const;
+	};
+}
+
+#endif  /* _GPLATES_GUI_FPVALIDATOR_H_ */
