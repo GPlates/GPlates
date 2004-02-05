73c8d42a52bc8db282448e7d3c25677ed4826f07
James Boyden <>
2004-02-05 11:57:45 +0000

    Fixed up the old namespace problem in this file. Added the member function 'calcRotationParams'. Fixed up the old exception/ostringstream problems in this file.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@657 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- src/maths/UnitQuaternion3D.cc
+++ src/maths/UnitQuaternion3D.cc
@@ -25,17 +25,51 @@
 
 #include <sstream>
 #include "UnitQuaternion3D.h"
+#include "IndeterminateResultException.h"
 #include "ViolatedUnitQuatInvariantException.h"
 
 
-using namespace GPlatesMaths;
+GPlatesMaths::UnitQuaternion3D::RotationParams
+GPlatesMaths::UnitQuaternion3D::calcRotationParams() const {
+
+	/*
+	 * Ensure that the quaternion does not represent an identity rotation.
+	 *
+	 * In an identity rotation, the angle of rotation is (2 * n * pi),
+	 * for some integer 'n':  this would later result in an evaluation of
+	 * the sine of some (n * pi), which is always zero.  This, in turn,
+	 * would result in a division by zero when attempting to calculate
+	 * the rotation axis, which is geometrically equivalent to the fact
+	 * that, in an identity rotation, the axis is indeterminate.
+	 */
+	if (isIdentity()) {
+
+		std::ostringstream oss;
+		oss << "Attempted to calculate the rotation parameters\n"
+		 << "of a quaternion which represents the identity rotation:\n"
+		 << (*this);
+		throw IndeterminateResultException(oss.str().c_str());
+	}
+
+	/*
+	 * We can now be sure that the angle of rotation ('theta') is not a
+	 * multiple of two pi, and the axis of rotation is clearly determined.
+	 */
+	real_t theta_on_2 = acos(s());  // theta_on_2 is not a multiple of pi
+	real_t sin_of_theta_on_2 = sin(theta_on_2);  // not zero
+
+	Vector3D axis_v = (1 / sin_of_theta_on_2) * v();
+	UnitVector3D axis_uv = axis_v.normalise();
+
+	return RotationParams(axis_uv, theta_on_2 * 2);
+}
 
 
-UnitQuaternion3D
-UnitQuaternion3D::CreateEulerRotation(const UnitVector3D &axis,
-	const real_t &rotation_angle) {
+GPlatesMaths::UnitQuaternion3D
+GPlatesMaths::UnitQuaternion3D::CreateEulerRotation(const UnitVector3D &axis,
+	const real_t &angle) {
 
-	real_t theta_on_two = rotation_angle / 2.0;
+	real_t theta_on_two = angle / 2.0;
 
 	real_t   scalar_part = cos(theta_on_two);
 	Vector3D vector_part = sin(theta_on_two) * Vector3D(axis);
@@ -45,7 +79,7 @@ UnitQuaternion3D::CreateEulerRotation(const UnitVector3D &axis,
 
 
 void
-UnitQuaternion3D::AssertInvariantHolds() const {
+GPlatesMaths::UnitQuaternion3D::AssertInvariantHolds() const {
 
 	/*
 	 * Calculate magnitude of quaternion to ensure that it actually _is_ 1.
@@ -55,14 +89,15 @@ UnitQuaternion3D::AssertInvariantHolds() const {
 	if (mag_sqrd != 1.0) {
 
 		// invariant has been violated
-		std::ostringstream oss("UnitQuaternion3D has magnitude ");
-		oss << sqrt(mag_sqrd);
+		std::ostringstream oss;
+		oss << "UnitQuaternion3D has magnitude "
+		 << sqrt(mag_sqrd) << ".";
 		throw ViolatedUnitQuatInvariantException(oss.str().c_str());
 	}
 }
 
 
-UnitQuaternion3D
+GPlatesMaths::UnitQuaternion3D
 GPlatesMaths::operator*(UnitQuaternion3D q1, UnitQuaternion3D q2) {
 
 	real_t   s1 = q1.s();
