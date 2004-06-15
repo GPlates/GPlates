b1e3773b0be0be10735eb8c2d8509404d1b827d0
Hamish Ivey-Law <>
2004-06-15 11:05:50 +0000

    Initial checkin of the PlatesWriter class.  PlatesWriter will provide the functionality required to export a data file to the Plates4 data format.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@768 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

---
+++ src/fileio/PlatesWriter.h
@@ -0,0 +1,32 @@
+/**
+ * @file 
+ * $Revision$
+ * $Date$
+ *
+ * Copyright (C) 2003 The GPlates Consortium
+ *
+ * This file is part of GPlates.
+ *
+ * GPlates is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * GPlates is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _GPLATES_FILEIO_PLATESWRITER_H_
+#define _GPLATES_FILEIO_PLATESWRITER_H_
+
+#include "WriterVisitor.h"
+
+namespace GPlatesFileIO
+{
+	class PlatesWriter
+	{
+	};
+};
+
+#endif  /*_GPLATES_FILEIO_PLATESWRITER_H_ */ 
