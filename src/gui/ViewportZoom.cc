32b4f21f19eb23dc50f47d65cd5afebfbde41395
James Boyden <>
2004-11-02 10:10:10 +0000

    Initial commit.  This class represents functionality which was extracted from the 'GLCanvas' class ("GLCanvas.{h,cc}").
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@801 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

---
+++ src/gui/ViewportZoom.cc
@@ -0,0 +1,92 @@
+/* $Id$ */
+
+/**
+ * @file 
+ * File specific comments.
+ *
+ * Most recent change:
+ *   $Date$
+ * 
+ * Copyright (C) 2003 The GPlates Consortium
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#include <cmath>
+#include "ViewportZoom.h"
+
+#define MIN_ZOOM_POWER 1.0
+#define MAX_ZOOM_POWER 2.0
+
+#define NUM_POWER_INCRS 20
+
+
+const double
+GPlatesGui::ViewportZoom::m_min_zoom_power = MIN_ZOOM_POWER;
+
+
+const double
+GPlatesGui::ViewportZoom::m_max_zoom_power = MAX_ZOOM_POWER;
+
+
+const double
+GPlatesGui::ViewportZoom::m_zoom_power_delta =
+ ((MAX_ZOOM_POWER - MIN_ZOOM_POWER) / NUM_POWER_INCRS);
+
+
+const double
+GPlatesGui::ViewportZoom::m_initial_zoom_power = MIN_ZOOM_POWER;
+
+
+void
+GPlatesGui::ViewportZoom::zoom_in() {
+
+	double incred_zoom_power = m_zoom_power + m_zoom_power_delta;
+
+	// Allow for a bit of accumulated floating-point error...
+	if (incred_zoom_power < (m_max_zoom_power + (m_zoom_power_delta / 2))) {
+
+		m_zoom_power = incred_zoom_power;
+		update_zoom_params();
+	}
+}
+
+
+void
+GPlatesGui::ViewportZoom::zoom_out() {
+
+	double decred_zoom_power = m_zoom_power - m_zoom_power_delta;
+
+	// Allow for a bit of accumulated floating-point error...
+	if (decred_zoom_power > (m_min_zoom_power - (m_zoom_power_delta / 2))) {
+
+		m_zoom_power = decred_zoom_power;
+		update_zoom_params();
+	}
+}
+
+
+void
+GPlatesGui::ViewportZoom::reset_zoom() {
+
+	m_zoom_power = m_initial_zoom_power;
+	update_zoom_params();
+}
+
+
+void
+GPlatesGui::ViewportZoom::update_zoom_params() {
+
+	double f_zoom_percent = std::pow(10.0, m_zoom_power + 1.0);
+
+	m_zoom_percent = static_cast< unsigned >(f_zoom_percent + 0.5);
+	m_zoom_factor = f_zoom_percent / 100.0;
+}
---
+++ src/gui/ViewportZoom.h
@@ -0,0 +1,97 @@
+/* $Id$ */
+
+/**
+ * @file 
+ * File specific comments.
+ *
+ * Most recent change:
+ *   $Date$
+ * 
+ * Copyright (C) 2003 The GPlates Consortium
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef GPLATES_GUI_VIEWPORTZOOM_H
+#define GPLATES_GUI_VIEWPORTZOOM_H
+
+namespace GPlatesGui {
+
+	/**
+	 * This class encapsulates the behaviour of the zooming-in and -out of
+	 * the Viewport.
+	 */
+	class ViewportZoom {
+
+		static const double m_min_zoom_power;
+
+		static const double m_max_zoom_power;
+
+		static const double m_zoom_power_delta;
+
+		static const double m_initial_zoom_power;
+
+	 public:
+
+		ViewportZoom() :
+		 m_zoom_power(m_initial_zoom_power) {
+
+			update_zoom_params();
+		}
+
+		~ViewportZoom() {  }
+
+		unsigned
+		zoom_percent() const {
+
+			return m_zoom_percent;
+		}
+
+		double
+		zoom_factor() const {
+
+			return m_zoom_factor;
+		}
+
+		void
+		zoom_in();
+
+		void
+		zoom_out();
+
+		void
+		reset_zoom();
+
+	 private:
+
+		void
+		update_zoom_params();
+
+		/**
+		 * The zoom power affects the zoom thusly:
+		 *  zoom_percent = pow(10.0, (zoom_power + 1))
+		 *
+		 * The zoom power must lie within the range [1.0, 2.0].
+		 * Thus (in particular):
+		 * - zoom power of 1.0 => zoom percent of 100.0 => no "zoom".
+		 * - zoom power of 2.0 => zoom percent of 1000.0 => 10x "zoom".
+		 */
+		double   m_zoom_power;
+
+		unsigned m_zoom_percent;
+
+		double   m_zoom_factor;
+
+	};
+
+}
+
+#endif  /* GPLATES_GUI_VIEWPORTZOOM_H */
