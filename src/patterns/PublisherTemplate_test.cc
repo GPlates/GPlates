0ec7f1aa11ee158f30b219f27e7ede440ccfa4b4
James Boyden <>
2005-04-08 19:02:17 +0000

    * Cleaned up the tests a little (a little refactoring here and there). * Vastly expanded the test suite.  I'm pretty sure that all public    operations of PublisherTemplate< T > and PublisherTemplate< T >::Subscriber    are now tested.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@840 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- src/patterns/PublisherTemplate_test.cc
+++ src/patterns/PublisherTemplate_test.cc
@@ -9,7 +9,7 @@
  * instantiated until it's used.  (The same goes for its member functions.)
  * And since you can't check the correctness of code which hasn't been
  * instantiated, the only way to check the correctness of this code is to use
- * it.  Hence: this class.
+ * it.  Hence: this file.
  *
  * It also tests that all the operations behave as they are meant to, and as an
  * added bonus, it acts as demo code for @a GPlatesPatterns::PublisherTemplate.
@@ -17,8 +17,9 @@
  * The contents of this file will never be compiled into the GPlates
  * executable, and as such, it will not be compiled automatically.  That's
  * right, kids, you have to compile it @b yourselves!  I recommend something
- * like <code>g++ -Wall -ansi -pedantic PublisherTemplate_test.cc</code>, but
- * as always, YMMV.  (Don't forget to execute it after all that.)
+ * like <code>g++ -Wall -ansi -pedantic PublisherTemplate_test.cc -I ..
+ * ../global/InternalInconsistencyException.cc ../global/Exception.cc</code>,
+ * but as always, YMMV.  (Don't forget to execute it after all that.)
  *
  * Most recent change:
  *   $Date$
@@ -33,7 +34,6 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
  */
 
 #include <iostream>
@@ -56,8 +56,12 @@ class TestSubscriber: public
 		std::cout
 		 << "- Subscriber "
 		 << this
-		 << " (subscribed to "
-		 << publisher()
+		 << " (subscribed to ";
+
+		if (publisher() == NULL) std::cout << "no publisher";
+		else std::cout << publisher();
+
+		std::cout
 		 << ") received notification."
 		 << std::endl;
 	}
@@ -65,119 +69,286 @@ class TestSubscriber: public
 };
 
 
+void
+describe_subscriber(
+ const TestSubscriber &ts);
+
+void
+describe_publisher(
+ const TestPublisher &tp);
+
+
 int
 main() {
 
+	std::cout
+	 << "\n* Default-ctoring 1 subscriber."
+	 << std::endl;
 	TestSubscriber ts3;
 
-	std::cout
-	 << "- Subscriber "
-	 << (&ts3)
-	 << " is currently subscribed to: "
-	 << ts3.publisher()
-	 << std::endl;
+	describe_subscriber(ts3);
 
 	{
-		std::cout << "* Beginning nested block..." << std::endl;
-
+		std::cout
+		 << "\n* Beginning nested block."
+		 << "\n* Default-ctoring 1 publisher and 2 subscribers."
+		 << std::endl;
 		TestPublisher tp1;
 		TestSubscriber ts1, ts2;
 
-		std::cout
-		 << "- Number of subscribers: "
-		 << tp1.num_subscribers()
-		 << std::endl;
-		std::cout
-		 << "- Subscriber "
-		 << (&ts3)
-		 << " is currently subscribed to: "
-		 << ts3.publisher()
-		 << std::endl;
-		std::cout
-		 << "- Subscriber "
-		 << (&ts1)
-		 << " is currently subscribed to: "
-		 << ts1.publisher()
-		 << std::endl;
-		std::cout
-		 << "- Subscriber "
-		 << (&ts2)
-		 << " is currently subscribed to: "
-		 << ts2.publisher()
-		 << std::endl;
+		describe_publisher(tp1);
+		describe_subscriber(ts3);
+		describe_subscriber(ts1);
+		describe_subscriber(ts2);
 
-		std::cout << "* Subscribing three subscribers..." << std::endl;
-
-		ts3.subscribe(tp1);
+		std::cout
+		 << "\n* Subscribing 3 subscribers to publisher "
+		 << (&tp1)
+		 << "."
+		 << std::endl;
+		ts3.subscribe_to(tp1);
 		tp1.append_subscriber(ts1);
-		ts2.subscribe(tp1);
+		ts2.subscribe_to(tp1);
+
+		describe_publisher(tp1);
+		describe_subscriber(ts3);
+		describe_subscriber(ts1);
+		describe_subscriber(ts2);
 
 		std::cout
-		 << "- Number of subscribers: "
-		 << tp1.num_subscribers()
+		 << "\n* Notifying subscribers of publisher "
+		 << (&tp1)
+		 << "."
 		 << std::endl;
-		std::cout
-		 << "- Subscriber "
-		 << (&ts3)
-		 << " is currently subscribed to: "
-		 << ts3.publisher()
-		 << std::endl;
-		std::cout
-		 << "- Subscriber "
-		 << (&ts1)
-		 << " is currently subscribed to: "
-		 << ts1.publisher()
-		 << std::endl;
-		std::cout
-		 << "- Subscriber "
-		 << (&ts2)
-		 << " is currently subscribed to: "
-		 << ts2.publisher()
-		 << std::endl;
-
-		std::cout << "* Notifying subscribers." << std::endl;
 		tp1.notify_subscribers();
 
-		std::cout << "* Unsubscribing two subscribers..." << std::endl;
+		std::cout
+		 << "\n* Copy-ctoring a new subscriber from subscriber "
+		 << (&ts2)
+		 << "."
+		 << std::endl;
+		TestSubscriber ts4(ts2);
+
+		describe_publisher(tp1);
+		describe_subscriber(ts3);
+		describe_subscriber(ts1);
+		describe_subscriber(ts2);
+		describe_subscriber(ts4);
+
+		std::cout
+		 << "\n* Notifying subscribers of publisher "
+		 << (&tp1)
+		 << "."
+		 << std::endl;
+		tp1.notify_subscribers();
+
+		std::cout
+		 << "\n* Default-ctoring 1 subscriber."
+		 << std::endl;
+		TestSubscriber ts5;
+
+		describe_publisher(tp1);
+		describe_subscriber(ts3);
+		describe_subscriber(ts1);
+		describe_subscriber(ts2);
+		describe_subscriber(ts4);
+		describe_subscriber(ts5);
+
+		std::cout
+		 << "\n* Assigning subscriber "
+		 << (&ts5)
+		 << " to subscriber "
+		 << (&ts2)
+		 << "."
+		 << std::endl;
+		ts2 = ts5;
+
+		describe_publisher(tp1);
+		describe_subscriber(ts3);
+		describe_subscriber(ts1);
+		describe_subscriber(ts2);
+		describe_subscriber(ts4);
+		describe_subscriber(ts5);
+
+		std::cout
+		 << "\n* Unsubscribing two subscribers ("
+		 << (&ts1)
+		 << " and "
+		 << (&ts2)
+		 << ") from publisher\n "
+		 << (&tp1)
+		 << "."
+		 << std::endl;
 
 		ts1.unsubscribe();
 		tp1.remove_subscriber(ts2);
 
-		std::cout
-		 << "- Number of subscribers: "
-		 << tp1.num_subscribers()
-		 << std::endl;
-		std::cout
-		 << "- Subscriber "
-		 << (&ts3)
-		 << " is currently subscribed to: "
-		 << ts3.publisher()
-		 << std::endl;
-		std::cout
-		 << "- Subscriber "
-		 << (&ts1)
-		 << " is currently subscribed to: "
-		 << ts1.publisher()
-		 << std::endl;
-		std::cout
-		 << "- Subscriber "
-		 << (&ts2)
-		 << " is currently subscribed to: "
-		 << ts2.publisher()
-		 << std::endl;
+		describe_publisher(tp1);
+		describe_subscriber(ts3);
+		describe_subscriber(ts1);
+		describe_subscriber(ts2);
+		describe_subscriber(ts4);
+		describe_subscriber(ts5);
 
 		std::cout
-		 << "* Ending nested block.  "
-		 << "(End of lifetime of publisher.)"
+		 << "\n* Subscribing subscriber "
+		 << (&ts3)
+		 << " to publisher "
+		 << (&tp1)
+		 << "."
+		 << std::endl;
+		ts3.subscribe_to(tp1);
+
+		describe_publisher(tp1);
+		describe_subscriber(ts3);
+		describe_subscriber(ts1);
+		describe_subscriber(ts2);
+		describe_subscriber(ts4);
+		describe_subscriber(ts5);
+
+		std::cout
+		 << "\n* Notifying subscribers of publisher "
+		 << (&tp1)
+		 << "."
+		 << std::endl;
+		tp1.notify_subscribers();
+
+		std::cout << "\n* Default-ctoring 1 publisher." << std::endl;
+		TestPublisher tp2;
+
+		describe_publisher(tp1);
+		describe_publisher(tp2);
+		describe_subscriber(ts3);
+		describe_subscriber(ts1);
+		describe_subscriber(ts2);
+		describe_subscriber(ts4);
+		describe_subscriber(ts5);
+
+		std::cout
+		 << "\n* Notifying subscribers of publisher "
+		 << (&tp1)
+		 << "."
+		 << std::endl;
+		tp1.notify_subscribers();
+
+		std::cout
+		 << "\n* Notifying subscribers of publisher "
+		 << (&tp2)
+		 << "."
+		 << std::endl;
+		tp2.notify_subscribers();
+
+		std::cout
+		 << "\n* Subscribing 1 subscriber to publisher "
+		 << (&tp2)
+		 << "."
+		 << std::endl;
+		ts3.subscribe_to(tp2);
+
+		describe_publisher(tp1);
+		describe_publisher(tp2);
+		describe_subscriber(ts3);
+		describe_subscriber(ts1);
+		describe_subscriber(ts2);
+		describe_subscriber(ts4);
+		describe_subscriber(ts5);
+
+		std::cout
+		 << "\n* Subscribing 1 subscriber to publisher "
+		 << (&tp2)
+		 << "."
+		 << std::endl;
+		ts2.subscribe_to(tp2);
+
+		describe_publisher(tp1);
+		describe_publisher(tp2);
+		describe_subscriber(ts3);
+		describe_subscriber(ts1);
+		describe_subscriber(ts2);
+		describe_subscriber(ts4);
+		describe_subscriber(ts5);
+
+		std::cout
+		 << "\n* Subscribing 1 subscriber to publisher "
+		 << (&tp1)
+		 << "."
+		 << std::endl;
+		ts5.subscribe_to(tp1);
+
+		describe_publisher(tp1);
+		describe_publisher(tp2);
+		describe_subscriber(ts3);
+		describe_subscriber(ts1);
+		describe_subscriber(ts2);
+		describe_subscriber(ts4);
+		describe_subscriber(ts5);
+
+		std::cout
+		 << "\n* Removing all subscribers of publisher "
+		 << (&tp1)
+		 << "."
+		 << std::endl;
+		tp1.remove_all_subscribers();
+
+		describe_publisher(tp1);
+		describe_publisher(tp2);
+		describe_subscriber(ts3);
+		describe_subscriber(ts1);
+		describe_subscriber(ts2);
+		describe_subscriber(ts4);
+		describe_subscriber(ts5);
+
+		std::cout
+		 << "\n* Notifying subscribers of publisher "
+		 << (&tp1)
+		 << "."
+		 << std::endl;
+		tp1.notify_subscribers();
+
+		std::cout
+		 << "\n* Notifying subscribers of publisher "
+		 << (&tp2)
+		 << "."
+		 << std::endl;
+		tp2.notify_subscribers();
+
+		std::cout
+		 << "\n* Ending nested block.  "
+		 << "(End of lifetime of 2 publishers and 4 subscribers.)"
 		 << std::endl;
 	}
 
-	std::cout
-	 << "- Subscriber "
-	 << (&ts3)
-	 << " is currently subscribed to: "
-	 << ts3.publisher()
-	 << std::endl;
+	describe_subscriber(ts3);
 
 	return 0;
+}
+
+
+void
+describe_subscriber(
+ const TestSubscriber &ts) {
+
+	std::cout
+	 << "- Subscriber "
+	 << (&ts)
+	 << " is currently subscribed to ";
+
+	if (ts.publisher() == NULL) std::cout << "no publisher";
+	else std::cout << "publisher " << ts.publisher();
+
+	std::cout << std::endl;
+}
+
+
+void
+describe_publisher(
+ const TestPublisher &tp) {
+
+	std::cout
+	 << "- Publisher "
+	 << (&tp)
+	 << " currently has "
+	 << tp.num_subscribers()
+	 << " subscribers"
+	 << std::endl;
 }
