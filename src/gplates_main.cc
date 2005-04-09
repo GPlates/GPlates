d9d63a30557f34fd432dd41285bde694004d8c7b
James Boyden <>
2005-04-09 07:12:41 +0000

    Expanded and updated the big comment. In particular, the citations in the bibliography are now properly done.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@843 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- src/gplates_main.cc
+++ src/gplates_main.cc
@@ -15,54 +15,79 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
  */
 
 /** 
- * @mainpage GPlates - Interactive Plate Tectonic Reconstructions
+ * @mainpage GPlates Developers' Manual
  * 
- * @section intro Introduction
+ * @section introduction Introduction
+ *
  * Welcome to the GPlates Developers' Manual.
  *
- * @section patterns Design Patterns
- * All the references to design patterns in the GPlates documentation 
- * refer to the book <i>Design Patterns</i> by Gamma, Helm, Johnson 
- * and Vlissides.
- * See the References section below for more details.
- *
  * @section contact Contact
+ *
  * <b>Email</b>:
- * - Dr. R. Dietmar M&uuml;ller <dietmar (at) geosci.usyd.edu.au>
- * - James Boyden <jboyden (at) geosci.usyd.edu.au>
- * - Hamish Law <hlaw (at) geosci.usyd.edu.au>
+ *  - James Boyden <jboyden (at) geosci.usyd.edu.au>
+ *  - Mark Turner <mturner (at) gps.caltech.edu>
  * 
  * <b>Snail Mail</b>:
- * - University of Sydney Institute of Marine Science<br>
+ * @par
+ *   James Boyden<br>
+ *   University of Sydney Institute of Marine Science<br>
  *   Edgeworth David Building F05<br>
  *   School of Geosciences<br>
  *   The University of Sydney, NSW 2006<br>
  *   AUSTRALIA<br>
  *
- * @section refs References
- * The following books and articles are either cited in the documentation 
- * or played a significant role in the project:
- * - Foley, J., van Dam, A., Feiner, S., and Hughes, J. (1996)
- *   <i>Computer Graphics: Principles and Practice (2nd Ed.)</i>,
- *   Addison-Wesley.
- * - Gahagan, L. (1999) <i>plates4.0: A User's Manual for the Plates
- *   Project's interactive reconstruction software</i>, The
- *   University of Texas Institute for Geophysics.
- * - Gamma, E., Helm, R., Johnson, R., and Vlissides, J. (1995)
- *   <i>Design Patterns: Elements of Reusable Object-Oriented Software</i>,
- *   Addison-Wesley.
- * - Greiner, B., "Euler Rotations in Plate-Tectonic Reconstructions"
- *   in <i>Computers and Geosciences</i> (1999) No. 25, pp209-216.
- * - Josuttis, N. (1999)
- *   <i>The C++ Standard Library: A Tutorial and Reference</i>,
- *   Addison-Wesley.
- * - Stoustrup, B. (2000)
- *   <i>The C++ Programming Language (3rd Ed.)</i>,
- *   Addison-Wesley.
+ * @section wxwidgets WxWidgets
+ *
+ * <a href="http://www.wxwidgets.org/">WxWidgets</a> (previously known as
+ * WxWindows) is an open-source, cross-platform C++ GUI framework.  GPlates
+ * currently requires version 2.4, release
+ * <a href="http://www.wxwidgets.org/manuals/2.4.2/wx.htm">2.4.2</a> or above.
+ *
+ * @section patterns Design Patterns
+ *
+ * All the references to design patterns in the GPlates documentation refer to
+ * the book <i>Design Patterns</i> (Gamma95).  See the Bibliography below for
+ * more details.
+ *
+ * @section bibliography Bibliography
+ *
+ * The following references have played a significant role in the design or
+ * implementation of the project:
+ *
+ *  - Benson04:  Calum Benson, Adam Elman, Seth Nickell, and Colin Z Robertson,
+ *     <i>GNOME Human Interface Guidelines 2.0</i> [online].
+ *     http://developer.gnome.org/projects/gup/hig/2.0/  [Accessed 9 April
+ *     2005]
+ *  - Cox03:  Allan Cox and Robert Brian Hart, <i>Plate Tectonics:  How It
+ *     Works</i>.  Blackwell Scientific Publications, 2003 reprint.
+ *  - Foley96:  James D. Foley, Andries van Dam, Steven K. Feiner, and John F.
+ *     Hughes, <i>Computer Graphics:  Principles and Practice (2nd
+ *     Edition)</i>.  Addison-Wesley, 1996.
+ *  - Gahagan99:  L. M. Gahagan, <i>plates4.0:  A User's Manual for the Plates
+ *     Project's interactive reconstruction software</i>.  The University of
+ *     Texas Institute for Geophysics, 1999.
+ *  - Gamma95:  Erich Gamma, Richard Helm, Ralph Johnson, and John
+ *     Vlissides, <i>Design Patterns:  Elements of Reusable
+ *     Object-Oriented Software</i>.  Addison-Wesley, 1995.
+ *  - Greiner99:  B. Greiner, "Euler Rotations in Plate-Tectonic
+ *     Reconstructions".  <i>Computers and Geosciences</i> No. 25, Pergamon
+ *     Press, 1999 (pp.209-216).
+ *  - Josuttis99:  Nicolai M. Josuttis, <i>The C++ Standard Library:  A
+ *     Tutorial and Reference</i>.  Addison-Wesley, 1999.
+ *  - Kuipers02:  Jack B. Kuipers, <i>Quaternions and Rotation Sequences</i>,
+ *     Princeton University Press, 2002.
+ *  - Meyers98:  Scott Meyers, <i>Effective C++:  50 Specific Ways to Improve
+ *     Your Programs and Designs (2nd Edition)</i>.  Addison-Wesley, 1998.
+ *  - Schettino99:  Antonio Schettino, "Polygon intersections in spherical
+ *     topology: Application to Plate Tectonics".  <i>Computers and
+ *     Geosciences</i> No. 25, Pergamon Press, 1999 (pp.61-69).
+ *  - Stroustrup97:  Bjarne Stroustrup, <i>The C++ Programming Language (3rd
+ *     Edition)</i>.  Addison-Wesley, 1997.
+ *  - Sutter00:  Herb Sutter, <i>Exceptional C++:  47 Engineering Puzzles,
+ *     Programming Problems, and Solutions</i>.  Addison Wesley Longman, 2000.
  */
 
 #include "gui/GPlatesApp.h"
