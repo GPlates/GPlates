c099c9aca5cad7089281e7efe229f31d3e1e4628
James Boyden <>
2006-10-13 08:38:34 +0000

    Added class XsString.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@905 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

---
+++ src/model/XsString.h
@@ -0,0 +1,83 @@
+/* $Id$ */
+
+/**
+ * \file 
+ * File specific comments.
+ *
+ * Most recent change:
+ *   $Date$
+ * 
+ * Copyright (C) 2006 The GPlates Consortium
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef GPLATES_MODEL_XSSTRING_H
+#define GPLATES_MODEL_XSSTRING_H
+
+#include <boost/intrusive_ptr.hpp>
+#include <unicode/icustr.h>
+#include "PropertyValue.h"
+
+
+namespace GPlatesModel {
+
+	class XsString :
+			public PropertyValue {
+
+	public:
+
+		virtual
+		~XsString() {  }
+
+		static
+		boost::intrusive_ptr<XsString>
+		create(
+				const UnicodeString &s) {
+			boost::intrusive_ptr<XsString> ptr(new XsString(s));
+			return ptr;
+		}
+
+		virtual
+		boost::intrusive_ptr<PropertyValue>
+		clone() const {
+			boost::intrusive_ptr<PropertyValue> dup(new XsString(*this));
+			return dup;
+		}
+
+		// FIXME: visitor accept method
+
+	protected:
+
+		// This operator should not be public, because we don't want to allow instantiation
+		// of this type on the stack.
+		explicit
+		XsString(
+				const UnicodeString &s) :
+			PropertyValue(),
+			d_s(s)
+		{  }
+
+	private:
+
+		UnicodeString d_s;
+
+		// This operator should never be defined, because we don't want/need to allow
+		// copy-assignment:  All copying should use the virtual copy-constructor 'clone'
+		// (which will in turn use the copy-constructor); all "assignment" should really
+		// only be assignment of one intrusive_ptr to another.
+		XsString &
+		operator=(const XsString &);
+
+	};
+
+}
+
+#endif  // GPLATES_MODEL_XSSTRING_H
