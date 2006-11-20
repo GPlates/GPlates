b9e583ca1d30be4dbec7dddd5f98c36cf727a773
James Boyden <>
2006-11-20 03:28:31 +0000

    IMPORTANT UPDATE: Updated and corrected the copyright and license information in the comment at the top of each source file.
    This was performed mostly-automatically by a program written by Glen, which
    used some of the cherry-picking code to parse the SVN dumpfile and determine
    when each file was first committed to CVS.  A small amount of manual activity
    was required afterwards to account for files which had been renamed (which is
    not tracked in CVS, and thus could not be handled by the cherry-picking code).
    
    
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@968 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- src/gui/SphericalGrid.h
+++ src/gui/SphericalGrid.h
@@ -7,17 +7,22 @@
  * Most recent change:
  *   $Date$
  * 
- * Copyright (C) 2003 The GPlates Consortium
+ * Copyright (C) 2003, 2004, 2005, 2006 The University of Sydney, Australia
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
+ * This file is part of GPlates.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GPlates is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, version 2, as published by
+ * the Free Software Foundation.
  *
+ * GPlates is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 #ifndef _GPLATES_GUI_SPHERICALGRID_H_
