ad85c40dd2de04a7ed7963ee15ab75fcee93e3c5
James Boyden <>
2006-11-21 04:04:06 +0000

    With the new Automake build system in place (augmenting the existing Autoconf build system), the new script "bootstrap" handles the invocation of all the "Auto*" programs, which means that "build-everything.sh" and "generate-docs.sh" no longer need to invoke the "Auto*" programs themselves.
    Also, the dependency deduction is now handled implicitly in the makefiles, so
    an explicit "make dep" is no longer necessary.
    
    
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@974 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- build-everything.sh
+++ build-everything.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
 set -x
-autoconf && \
 ./configure --enable-dev && \
-make dep && \
 make
--- generate-docs.sh
+++ generate-docs.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
 set -x
-autoconf && \
 ./configure --enable-dev && \
 make doc
