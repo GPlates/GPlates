aa1e102c23134b775230e6a3ec63b27abec621c9
James Boyden <>
2007-03-16 00:43:59 +0000

    Fixed a "#include" error in "FeatureCollectionRevision.cc" (which was present since "FeatureCollectionRevision.cc" was added to SVN in changeset:1146, but hadn't been detected since "FeatureCollectionRevision.cc" wasn't being compiled).
    Added "FeatureCollectionRevision.cc" to the list of libmodel_a_SOURCES in
    "Makefile.am" (which I should have done when I added "FeatureCollectionRevision.cc"
    in changeset:1146), so that it would be compiled and linked into "libmodel.a".
    
    
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@1154 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- src/model/FeatureCollectionRevision.cc
+++ src/model/FeatureCollectionRevision.cc
@@ -25,7 +25,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#include "FeatureRevisionCollection.h
+#include "FeatureCollectionRevision.h"
 #include "DummyTransactionHandle.h"
 
 
