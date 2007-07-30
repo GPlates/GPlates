5f0b6663eb994f35bec9fcbe574f5fe9ddce446a
James Boyden <>
2007-07-30 23:53:07 +0000

    Initial commit of a one-step bootstrap-and-build script.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@1298 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

---
+++ one-step-bootstrap-and-build.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+# This script is not intended for use by users of official GPlates releases.
+#
+# The purpose of this script is to enable one-step bootstrap-and-building of
+# GPlates, after a fresh checkout from the GPlates source-code repositiory,
+# until the build problem described by ticket:1 ("Qt Ui-file build problem")
+# is fixed.
+#
+# After executing this script, GPlates should be completely built; it should
+# not be necessary to execute any other compilation scripts or commands.
+
+set -x
+sh bootstrap.sh
+sh build-everything.sh
+( cd src/gplates-qt-demo && make DocumentUi.h InformationDialogUi.h )
+make
