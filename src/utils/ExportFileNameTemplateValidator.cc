0e203f2f453c62649f8d1d7db84e5f437b79ec3b
Michael Chin <michael.chin@sydney.edu.au>
2010-06-09 02:31:18 +0000

    (truck) Fix a bug in ExportFileNameTemplateValidator code
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@8660 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- src/utils/ExportFileNameTemplateValidator.cc
+++ src/utils/ExportFileNameTemplateValidator.cc
@@ -212,7 +212,7 @@ GPlatesUtils::ExportFileNameTemplateValidator::file_sequence_validate(
 	catch (...)
 	{
 		set_result_message("Unexpected exception happened in the validation of file name template.");
-		throw false;
+		throw;
 	}
 }
 
