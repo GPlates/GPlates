c80026edf1e09508ee27227d1ffbc5840c040cb5
John Cannon <john.cannon@sydney.edu.au>
2011-01-19 01:32:09 +0000

    (trunk) Updated pre-compiled header script. It was including some external include headers that were not top-level (eg, a header that a boost header itself included). Also updated for new source directories.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@10778 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- cmake/list_external_includes.py
+++ cmake/list_external_includes.py
@@ -27,8 +27,13 @@ class ParserMSVC(IncludeParser):
     
         self.locations = [
             ('src', 'gplates'),
-            ('src', 'gplates-no-gui'),
+            ('src', 'gplates-cli'),
+            ('src', 'gplates-unit-test'),
+			('src/api', 'api'),
+			('src/app-logic', 'app-logic'),
             ('src/canvas-tools', 'canvas-tools'),
+			('src/cli', 'cli'),
+			('src/data-mining', 'data-mining'),
             ('src/feature-visitors', 'feature-visitors'),
             ('src/file-io', 'file-io'),
             ('src/global', 'global'),
@@ -36,10 +41,13 @@ class ParserMSVC(IncludeParser):
             ('src/maths', 'maths'),
             ('src/model', 'model'),
             ('src/opengl', 'opengl'),
+            ('src/presentation', 'presentation'),
             ('src/property-values', 'property-values'),
             ('src/qt-resources', 'qt-resources'),
             ('src/qt-widgets', 'qt-widgets'),
+            ('src/unit-test', 'unit-test'),
             ('src/utils', 'utils'),
+            ('src/view-operations', 'view-operations'),
         ]
         
         # Include headers that look different across platforms or
@@ -136,13 +144,29 @@ class ParserMSVC(IncludeParser):
         
         return lines
 
-    def convert_msvc_include(self, msvc_include):
+    def convert_path_to_forward_slashes(self, path):
+        path_regexp  = re.compile('\\\\')
+        return path_regexp.sub('/', path)
+
+    def convert_msvc_include_to_relative_include(self, msvc_include):
         #  Because MSVC uses '\' in its paths and any '#include' lines in our sources files use '/' in their paths
         # we can easily strip off the include paths
+		# An example of an include path being stripped is:
+		# "C:\SDK\boost\boost_1_36_0\boost/cstdint.hpp" converted to "boost/cstdint.hpp".
         msvc_include_regexp  = re.compile('\\\\([^\\\\]+)$')
         msvc_include_match = msvc_include_regexp.search(msvc_include)
         if not msvc_include_match:
-            raise SyntaxError, "Line cannot be parsed '%s'" % msvc_include
+			# Some includes are not in MSVC format (ie, they only use '/' in their path).
+            # Currently these are some boost headers (see bottom of 'cmake/modules/Config_h.cmake') that
+            # are in 'src/system-fixes' that indirectly include the real boost installation header files
+            # (if installed boost version greater than 1.34).
+			# NOTE: Currently we'll need to go through the generated pch files and strip off the include paths by hand.
+			# For example, "C:/SDK/boost/boost_1_36_0/boost/cstdint.hpp" needs to be converted to "boost/cstdint.hpp".
+            #
+            # TODO: Test if 'src/global/config.h' file exists and if so look for those boost include paths and use that
+            # as a prefix here that can be removed from the path to make it relative.
+            # For example, to remove "C:/SDK/boost/boost_1_36_0/".
+			return msvc_include
         
         return msvc_include_match.group(1)
 
@@ -166,7 +190,7 @@ class ParserMSVC(IncludeParser):
 
         print >>output_file, '#include <' + include + '>'
 
-    def parse_include(self, input_filename, output_filename, root_source_dir_lower, root_build_tree_dir_lower):
+    def parse_include(self, input_filename, output_filename, root_source_dir_lower_and_forward_slashes, root_build_tree_dir_lower_and_forward_slashes):
         
         lines = self.read_build_log(input_filename)
         
@@ -174,7 +198,8 @@ class ParserMSVC(IncludeParser):
         
         msvc_include_regexp  = re.compile('^Note\: including file\:(\s+)(.*)')
         
-        last_indent_first_external_include = 1000
+        # Use a large number so that the next indent will be less than it.
+        indent_of_last_toplevel_external_include = 1000
         
         msvc_include_set = set()
         
@@ -184,36 +209,75 @@ class ParserMSVC(IncludeParser):
             msvc_include_match = msvc_include_regexp.search(line)
             if msvc_include_match:
                 indent = len(msvc_include_match.group(1))-1
-                msvc_include = string.strip(msvc_include_match.group(2))
+
+                # Some definitions:
+                #
+                # Internal header - an include header file that is part of the GPlates source code.
+                #
+                # External header - an include header file that is *not* part of the GPlates source code.
+                # These are the system headers and headers of dependency libraries.
+                #
+                # Internal headers can include both internal and external headers.
+                # External headers can only include external headers (external headers don't know about GPlates).
+                #
+                # A toplevel external include header is an external header file that is included by an internal header
+                # or internal ".cc" file. These are the headers we want to put in the PCH file.
                 
+                # If the indent of the current line is greater than the indent of the last toplevel external include
+                # then we are at an external include that is not a toplevel and hence should be ignored.
+                # We ignore these non-toplevel external includes because if our PCH file includes the toplevel include file
+                # then that will automatically include all its dependency includes anyway. Also we don't want to include
+                # header files that are included by the toplevel external include because they are implementation headers
+                # that could be dependent on the version of the library or operating system installed on.
+                # For example <boost/bimap/bimap.hpp> includes <boost/bimap/detail/user_interface_config.hpp> -
+                # the former is a stable interface header filename whereas the latter might not be.
+                if indent > indent_of_last_toplevel_external_include:
+                    continue
+
                 # If we found an internal path then start looking for external paths at any indent level.
-                # Any include path that matches the root source dir or root build directory is considered an internal header and won't be included in the PCH.
-                msvc_include_lower = msvc_include.lower()
-                if string.find(msvc_include_lower, root_source_dir_lower) != -1 or \
-                    string.find(msvc_include_lower, root_build_tree_dir_lower) != -1:
-                    last_indent_first_external_include = 1000
-                
-                # If we're an external include and we've been included by an internal include then print to output.
-                elif indent <= last_indent_first_external_include:
-                    last_indent_first_external_include = indent
-                    if msvc_include not in msvc_include_set:
-                        msvc_include_set.add(msvc_include)
-                        include = self.convert_msvc_include(msvc_include)
-                        self.output_platform_independent_include(include, output_file)
+                # Any include path that matches the root source dir or root build directory is considered an
+                # internal header and won't be included in the PCH.
+                else:
+                    msvc_include = string.strip(msvc_include_match.group(2))
+                    msvc_include_lower_and_forward_slashes = self.convert_path_to_forward_slashes(msvc_include.lower())
+                    
+                    # Compare all paths using forward slashes in the pathname.
+                    if string.find(msvc_include_lower_and_forward_slashes, root_source_dir_lower_and_forward_slashes) != -1 or \
+                        string.find(msvc_include_lower_and_forward_slashes, root_build_tree_dir_lower_and_forward_slashes) != -1:
+                        # The current include is an internal include so any external include after this will
+                        # be the next toplevel external include.
+                        # Use a large number so that the next indent will be less than it.
+                        indent_of_last_toplevel_external_include = 1000
+                    
+                    # If we get here then we're an external include.
+                    # See if we've been included by an internal include.
+                    # If so then add to the list of include files for our PCH.
+                    # We can simply compare the current indent with the indent of the last toplevel external include.
+                    # If it's not larger then it means the current external include is a toplevel external include.
+                    elif indent <= indent_of_last_toplevel_external_include:
+                        indent_of_last_toplevel_external_include = indent
+                        # Only need to add each header filename once.
+                        if msvc_include not in msvc_include_set:
+                            msvc_include_set.add(msvc_include)
+                            include = self.convert_msvc_include_to_relative_include(msvc_include)
+                            self.output_platform_independent_include(include, output_file)
+
+            # There was no match so we must have reached a new ".cc" file so start anew.
             else:
-                last_indent_first_external_include = 1000
+                # Use a large number so that the next indent will be less than it.
+                indent_of_last_toplevel_external_include = 1000
                 
         output_file.close()
 
     def parse_includes(self, build_type, root_source_dir, root_build_tree_dir):
         # Any include path that matches the root source dir or root build directory is considered an internal header and won't be included in the PCH.
-        root_source_dir_lower = os.path.abspath(root_source_dir).lower()
-        root_build_tree_dir_lower = os.path.abspath(root_build_tree_dir).lower()
+        root_source_dir_lower_and_forward_slashes = self.convert_path_to_forward_slashes(os.path.abspath(root_source_dir).lower())
+        root_build_tree_dir_lower_and_forward_slashes = self.convert_path_to_forward_slashes(os.path.abspath(root_build_tree_dir).lower())
         
         for base_dir, proj in self.locations:
             input_filename = os.path.join(root_build_tree_dir, base_dir, proj + '.dir', build_type, 'BuildLog.htm')
             output_filename = os.path.join(root_source_dir, base_dir, proj + '_pch.h')
-            self.parse_include(input_filename, output_filename, root_source_dir_lower, root_build_tree_dir_lower)
+            self.parse_include(input_filename, output_filename, root_source_dir_lower_and_forward_slashes, root_build_tree_dir_lower_and_forward_slashes)
 
 def main(argv):
     # Parse the command-line options. 
