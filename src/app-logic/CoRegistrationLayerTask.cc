183235d43c25f690e8b85b1e53d2d7283fc703c4
Michael Chin <michael.chin@sydney.edu.au>
2011-05-18 02:25:57 +0000

    (trunk) Bug fix: co-registration crash.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@11580 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- src/app-logic/CoRegistrationLayerTask.cc
+++ src/app-logic/CoRegistrationLayerTask.cc
@@ -93,7 +93,13 @@ GPlatesAppLogic::CoRegistrationLayerTask::process(
 		const ReconstructionTree::non_null_ptr_to_const_type &default_reconstruction_tree)
 {
 	using namespace GPlatesDataMining;
+	if(!d_layer_params.d_call_back || !d_layer_params.d_cfg_table)
+	{
+		//This case means layer parameters haven't be initialized. We should not proceed further from here.
+		return boost::none;
+	}
 	refresh_data(DataTable());
+
 	// Get the reconstruction tree input.
 	boost::optional<ReconstructionTree::non_null_ptr_to_const_type> reconstruction_tree =
 			extract_reconstruction_tree(
@@ -105,13 +111,7 @@ GPlatesAppLogic::CoRegistrationLayerTask::process(
 		qWarning() << "No reconstruction tree found.";
 		return boost::none;
 	}
-
-	if(!d_layer_params.d_call_back || !d_layer_params.d_cfg_table)
-	{
-		//This case means layer parameters haven't be initialized. We should not proceed further from here.
-		return boost::none;
-	}
-
+	
 	// Get seeds.
 	std::vector<ReconstructionGeometryCollection::non_null_ptr_to_const_type> seeds_collection;
 	extract_input_channel_data(
