7219f1e8c09b9a867ebad4770b0de1d85fbfd5ed
Michael Chin <michael.chin@sydney.edu.au>
2011-06-29 00:56:17 +0000

    (trunk) Hardy compile error.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@11896 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- src/qt-widgets/DrawStyleDialog.cc
+++ src/qt-widgets/DrawStyleDialog.cc
@@ -22,6 +22,7 @@
  * with this program; if not, write to Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+#include <QHeaderView>
 #include "DrawStyleDialog.h"
 #include "gui/DrawStyleManager.h"
 #include "gui/HTMLColourNames.h"
