421b7c44441652d264dadc800f04753911317924
Michael Chin <michael.chin@sydney.edu.au>
2011-07-07 03:38:43 +0000

    (truck) It seems "lag behind" problem of exporting raster has been fixed. Merge fix into truck.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@11960 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- src/gui/ExportRasterAnimationStrategy.cc
+++ src/gui/ExportRasterAnimationStrategy.cc
@@ -37,7 +37,6 @@
 
 #include "maths/MathsUtils.h"
 
-#include "qt-widgets/GlobeAndMapWidget.h"
 #include "qt-widgets/GlobeCanvas.h"
 #include "qt-widgets/ReconstructionViewWidget.h"
 #include "qt-widgets/ViewportWindow.h"
@@ -49,8 +48,15 @@ GPlatesGui::ExportRasterAnimationStrategy::ExportRasterAnimationStrategy(
 		GPlatesGui::ExportAnimationContext &export_animation_context,
 		const const_configuration_ptr &configuration):
 	ExportAnimationStrategy(export_animation_context),
-	d_configuration(configuration)
+	d_configuration(configuration),
+	d_main_widget(d_export_animation_context_ptr->viewport_window().reconstruction_view_widget().globe_and_map_widget())
 {
+	QObject::connect(
+			&d_main_widget,
+			SIGNAL(repainted(bool)),
+			this,
+			SLOT(handle_repaint(bool)));
+
 	set_template_filename(d_configuration->get_filename_template());
 }
 
@@ -66,36 +72,12 @@ GPlatesGui::ExportRasterAnimationStrategy::do_export_iteration(
 	// Add the target dir to that to figure out the absolute path + name.
 	QString full_filename = d_export_animation_context_ptr->target_dir().absoluteFilePath(basename);
 
-	// FIXME: The method by which we cause the globe/map to repaint in order to
-	// export a new frame to file is fragile and would be best replaced once we
-	// have the ability to render to off-screen targets.
-
-	// The following causes an event to be enqueued onto the Qt event loop that
-	// will cause the globe/map to be repainted. Because of the use of the Qt event
-	// loop to cause the repaint, this must be followed by @a processEvents().
-	d_export_animation_context_ptr->viewport_window().reconstruction_view_widget().
-			globe_and_map_widget().update_canvas();
-	QCoreApplication::processEvents();
-
-	// It appears that the previous two lines, on some computers, result in the
-	// current frame to be in the front buffer, and the previous frame to be in the
-	// back buffer. QGLWidget::grabFrameBuffer() reads from the back buffer when
-	// double-buffering is enabled (most computers); this could therefore pick up
-	// the previous frame for export to file. On other computers, the previous two
-	// lines result in both the front and back buffer to contain the current frame,
-	// and thus on these computers, the following code works.
-	//
-	// Instead of causing grabFrameBuffer() to read from the front buffer by using
-	// glReadBuffer(GL_FRONT), we considered that it is more fail-safe to cause
-	// both front and back buffers to contain the current frame. This can be
-	// achieved by running the previous two lines again.
-	d_export_animation_context_ptr->viewport_window().reconstruction_view_widget().
-			globe_and_map_widget().update_canvas();
-	QCoreApplication::processEvents();
-
-	QImage img = d_export_animation_context_ptr->viewport_window().
-		reconstruction_view_widget().globe_and_map_widget().grab_frame_buffer();
-	img.save(full_filename);
+	d_repaint_flag = false;
+	d_main_widget.update_canvas();
+	while(!d_repaint_flag)
+		QApplication::processEvents();
+	
+	d_image.save(full_filename);
 
 	// Normal exit, all good, ask the Context process the next iteration please.
 	return true;
