607dbff2d00d71fc38423082292e58c1d3a30095
John Cannon <john.cannon@sydney.edu.au>
2012-01-28 05:01:46 +0000

    (1.2) Updated version number to 12840 for GPlates 1.2 public release.
    git-svn-id: http://svn.gplates.org/projects/gplates/branches/1.2@12841 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- cmake/ConfigDefault.cmake
+++ cmake/ConfigDefault.cmake
@@ -69,7 +69,7 @@ set(GPLATES_HTML_COPYRIGHT_STRING "${GPLATES_HTML_COPYRIGHT_STRING}</body></html
 # This is manually set when making GPlates *public* releases.
 # However, when making internal releases or just an ordinary developer build, leave it
 # empty; if it is empty, the revision number is automatically populated for you on build.
-set(GPLATES_SOURCE_CODE_CONTROL_VERSION_STRING "12825")
+set(GPLATES_SOURCE_CODE_CONTROL_VERSION_STRING "12840")
 
 # List the Qt plugins used by GPlates.
 # This is needed for packaging standalone versions of GPlates for a binary installer.
