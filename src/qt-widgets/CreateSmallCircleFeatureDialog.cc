300ebb1a215828a2476fdaa99c58c1cab11a51df
John Cannon <john.cannon@sydney.edu.au>
2012-10-18 09:38:11 +0000

    (trunk) Fixed small circle creation taking a very long time for large number of circles.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@13456 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- src/qt-widgets/CreateSmallCircleFeatureDialog.cc
+++ src/qt-widgets/CreateSmallCircleFeatureDialog.cc
@@ -49,6 +49,7 @@
 #include "model/FeatureCollectionHandle.h"
 #include "model/ModelInterface.h"
 #include "model/ModelUtils.h"
+#include "model/NotificationGuard.h"
 
 #include "presentation/ViewState.h"
 
@@ -166,6 +167,11 @@ GPlatesQtWidgets::CreateSmallCircleFeatureDialog::handle_create()
 
 	try
 	{
+		// We want to merge model events across this scope so that only one model event
+		// is generated instead of many as we incrementally modify the feature below.
+		GPlatesModel::NotificationGuard model_notification_guard(
+				d_application_state_ptr->get_model_interface().access_model());
+
 		// Get the FeatureCollection the user has selected.
 		std::pair<GPlatesAppLogic::FeatureCollectionFileState::file_reference, bool> collection_file_iter =
 			d_choose_feature_collection_widget->get_file_reference();
@@ -237,6 +243,13 @@ GPlatesQtWidgets::CreateSmallCircleFeatureDialog::handle_create()
 		}
 
 
+		
+		// Release the model notification guard now that we've finished modifying the feature.
+		// Provided there are no nested guards this should notify model observers.
+		// We want any observers to see the changes before we emit signals because we don't
+		// know whose listening on those signals and they may be expecting model observers to
+		// be up-to-date with the modified model.
+		model_notification_guard.release_guard();
 
 		// To trigger a reconstruction.
 		emit feature_created();
