7d9be69dd741344971e1d03b39f6d51c6124d3a2
John Cannon <john.cannon@sydney.edu.au>
2013-05-27 09:03:48 +0000

    (trunk) Added copyright to OpenGL shader source files.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@14197 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- src/qt-resources/opengl/scalar_field_3d/volume_fill_spherical_cap_geometry_shader.glsl
+++ src/qt-resources/opengl/scalar_field_3d/volume_fill_spherical_cap_geometry_shader.glsl
@@ -1,3 +1,28 @@
+/* $Id$ */
+
+/**
+ * \file 
+ * $Revision$
+ * $Date$
+ * 
+ * Copyright (C) 2013 The University of Sydney, Australia
+ *
+ * This file is part of GPlates.
+ *
+ * GPlates is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, version 2, as published by
+ * the Free Software Foundation.
+ *
+ * GPlates is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 //
 // Geometry shader source code to render volume fill spherical caps.
 //
