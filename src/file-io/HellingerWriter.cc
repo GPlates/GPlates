9878bed8f39401cfd78b3da057d5e3af2d0b1b91
Robin Watson <>
2013-09-06 09:12:15 +0000

    (hellinger) Temporary fixes to export behaviour.
    git-svn-id: http://svn.gplates.org/projects/gplates/branches/hellinger-2013-feb-21@14616 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- src/file-io/HellingerWriter.cc
+++ src/file-io/HellingerWriter.cc
@@ -24,6 +24,7 @@
  */
 
 #include <QFile>
+#include <QFileInfo>
 #include <QStringList>
 
 #include "qt-widgets/HellingerModel.h"
@@ -100,13 +101,27 @@ void GPlatesFileIO::HellingerWriter::write_com_file(
 	boost::optional<GPlatesQtWidgets::HellingerComFileStructure> com_struct = hellinger_model.get_com_file();
 	if (com_struct)
 	{
+		//TODO: check for .com extension, add if missing.
 		QFile file(filename);
 		QTextStream out(&file);
+
+		//TODO: if we haven't specified a pick file name, export a pick file with the same root name as the provided .com filename.
+
+		// Strip the pick file name down from the full path to the filename only. The .com file format expects the filename relative
+		// to the location of the .com file.  The procedures here won't guarantee that (the user can choose a .com location anywhere).
+		// TODO: To do this properly I think we need a dedicated export dialog to handle the various possibilities i.e.
+		//   - export pick file alone, let user select any output path/filename.
+		//   - export .com and pick file together: user selects names for both, but path of pick file is forced to be the same as the .com file.
+		//   - export .com only (say the user wants to use an existing pick file but just wants to change the initial guess or something...)
+		//		- but what do we do with the pick filepath in this situation??
+		QFile pick_file(com_struct->d_pick_file);
+		QFileInfo pick_fileinfo(pick_file);
+		QString pick_filename = pick_fileinfo.fileName();
 
 		if (file.open(QIODevice::WriteOnly))
 		{
 			// Pick file name
-			out << com_struct->d_pick_file << '\n';
+			out << pick_filename << '\n';
 
 			// Initial guess: lat, lon, rho
 			out << com_struct->d_lat << " " << com_struct->d_lon << " " << com_struct->d_rho << '\n';
