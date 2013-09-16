53e61ce330b17837ba42ae48836b28f8c98d6c2d
John Cannon <john.cannon@sydney.edu.au>
2013-09-16 07:57:15 +0000

    (python-api) Add some example paths for pygplates for non-linux CMake builds and out-of-place linux CMake builds.
    git-svn-id: http://svn.gplates.org/projects/gplates/branches/python-api-2013-jul-25@14674 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- src/pygplates/test/test.py
+++ src/pygplates/test/test.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 """
 Unit tests for the pygplates native library.
 """
@@ -8,7 +10,18 @@ import types
 import unittest
 
 # Import the built pygplates library (not any other one installed on the system).
-sys.path.insert(1, os.path.join(os.path.dirname(__file__), '..', '..', '..', 'bin'))
+# TODO: Get CMake to specify these paths using its 'configure_file' command.
+
+# Path used for linux/MacOS CMake builds that are 'in-place' (same source and build directories)...
+PYGPLATES_PATH = os.path.join(os.path.dirname(__file__), '..', '..', '..', 'bin')
+
+# An example path used for linux/MacOS CMake builds where the source and builds directories differ...
+#PYGPLATES_PATH = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'python-api-2013-jul-25-build', 'bin')
+
+# An example path used for Windows CMake builds...
+#PYGPLATES_PATH = "C:\\Users\\John\\Development\\Usyd\\gplates\\svn\\build\\python-api-2013-jul-25\\bin\\profilegplates"
+
+sys.path.insert(1, PYGPLATES_PATH)
 import pygplates
 
 import test_app_logic.test
