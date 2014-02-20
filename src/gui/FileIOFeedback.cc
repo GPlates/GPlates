d8d652d4eff231140c0fca02c2615d57549ef40b
John Cannon <john.cannon@sydney.edu.au>
2014-02-20 08:47:32 +0000

    (trunk) No longer show warning dialog when loading files (due to different GPGIM version). It's a bit too irritating. We still show a warning dialog when saving (when GPGIM version differs).
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@14984 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- src/gui/FileIOFeedback.cc
+++ src/gui/FileIOFeedback.cc
@@ -403,6 +403,9 @@ GPlatesGui::FileIOFeedback::FileIOFeedback(
 	d_gpgim_version_warning_dialog_ptr(
 			new GPlatesQtWidgets::GpgimVersionWarningDialog(
 					app_state_.get_gpgim(),
+					// We no longer show the dialog when *loading* files since it's too annoying.
+					// It's still shown when *saving* files though...
+					false/*show_dialog_on_loading_files*/,
 					&viewport_window()))
 {
 	setObjectName("FileIOFeedback");
