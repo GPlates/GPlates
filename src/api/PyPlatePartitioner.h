cad76e77c48a627e354eb78edd7daf6c9d6c683f
John Cannon <john.cannon@sydney.edu.au>
2016-01-08 03:32:12 +0000

    (python-api) Store partitioning features in PlatePartitioner Python wrapper to keep them alive (otherwise returned Python partitioned plates return None when calling 'get_feature()'). Also store rotation model so can later reverse reconstruct parttioned features.
    git-svn-id: http://svn.gplates.org/projects/gplates/branches/python-api-2013-jul-25@16940 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- src/api/PyPlatePartitioner.h
+++ src/api/PyPlatePartitioner.h
@@ -26,6 +26,14 @@
 #ifndef GPLATES_API_PYPLATEPARTITIONER_H
 #define GPLATES_API_PYPLATEPARTITIONER_H
 
+#include <boost/optional.hpp>
+#include <boost/shared_ptr.hpp>
+
+#include "PyFeatureCollection.h"
+#include "PyRotationModel.h"
+
+#include "app-logic/GeometryCookieCutter.h"
+
 #include "global/PreconditionViolationError.h"
 
 
@@ -87,6 +95,82 @@ namespace GPlatesApi
 			BY_PLATE_AREA
 		};
 	};
+
+
+	/**
+	 * A Python wrapper around a @a GeometryCookieCutter that keeps any referenced features alive
+	 * (if plates reconstructed/resolved from features).
+	 *
+	 * Keeping the referenced features alive is important because partitioning results can return
+	 * @a ReconstructionGeometry objects which, in turn, reference features (but only weak references).
+	 *
+	 * This is the wrapper type that gets stored in the python object.
+	 */
+	class PlatePartitionerWrapper
+	{
+	public:
+
+		/**
+		 * The default boost-python 'pointee<HeldType>::type' is defined as 'HeldType::element_type'.
+		 *
+		 * This is needed for wrapped types ('HeldType') that are not already smart pointers.
+		 */
+	    typedef GPlatesAppLogic::GeometryCookieCutter element_type;
+
+		explicit
+		PlatePartitionerWrapper(
+				const boost::shared_ptr<GPlatesAppLogic::GeometryCookieCutter> &geometry_cookie_cutter,
+				boost::optional<RotationModel::non_null_ptr_type> rotation_model = boost::none,
+				boost::optional<const FeatureCollectionSequenceFunctionArgument &> partitioning_features = boost::none) :
+			d_geometry_cookie_cutter(geometry_cookie_cutter),
+			d_rotation_model(rotation_model)
+		{
+			if (partitioning_features)
+			{
+				d_partitioning_features = partitioning_features.get();
+			}
+		}
+
+		/**
+		 * Get the wrapped geometry cookie cutter.
+		 */
+		boost::shared_ptr<GPlatesAppLogic::GeometryCookieCutter>
+		get_geometry_cookie_cutter() const
+		{
+			return d_geometry_cookie_cutter;
+		}
+
+		/**
+		 * Get the rotation model (if any).
+		 */
+		boost::optional<RotationModel::non_null_ptr_type>
+		get_rotation_model() const
+		{
+			return d_rotation_model;
+		}
+
+	private:
+
+		boost::shared_ptr<GPlatesAppLogic::GeometryCookieCutter> d_geometry_cookie_cutter;
+
+		//! A rotation model if needed to reverse reconstruct feature geometries after cookie-cutting.
+		boost::optional<RotationModel::non_null_ptr_type> d_rotation_model;
+
+		//! Keep any partitioning features alive since returned partitioning reconstruction geometries reference them.
+		boost::optional<FeatureCollectionSequenceFunctionArgument> d_partitioning_features;
+	};
+
+	/**
+	 * Boost-python requires 'get_pointer(HeldType)' for wrapped types ('HeldType') that
+	 * are not already smart pointers.
+	 */
+	inline
+	GPlatesAppLogic::GeometryCookieCutter *
+	get_pointer(
+			const PlatePartitionerWrapper &wrapper)
+	{
+		return wrapper.get_geometry_cookie_cutter().get();
+	}
 }
 
 #endif   // GPLATES_NO_PYTHON
