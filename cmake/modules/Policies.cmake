be9a22c40ebe4fa9ca58b7905a0b16d03ad60ff2
John Cannon <john.cannon@sydney.edu.au>
2018-07-26 07:55:25 +0000

    (trunk) Rename CMake policies file.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@18678 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- cmake/modules/Policies.cmake
+++ cmake/modules/Policies.cmake
@@ -1,9 +1,10 @@
 #
-# CMake version 2.6 introduces policies to tell CMake to behave like a certain version ( > 2.4 ).
-# Each 'cmake_policy' command is surrounded by if/endif because, for example, CMake 2.6.2
-# has policies 'CMP0008' and 'CMP0009' which CMake 2.6.0 doesn't.
-# See http://www.cmake.org/cmake/help/cmake2.6docs.html#section_Policies
-# for a description of the policies.
+# CMake policies give the choice of OLD (backwards compatible) or NEW (non-backwards compatible) behaviour
+# when a version of CMake newer than specified in 'cmake_minimum_required()' is used.
+# CMake states that ultimately all policies should specify NEW with OLD being a temporary solution.
+#
+# Each 'cmake_policy' command is surrounded by if/endif since the runtime version of CMake may not support them.
+# For example, when specifying CMake 3.x policies but the runtime CMake is 2.8.8.
 #
 if(COMMAND cmake_policy)
     # NEW = A minimum required CMake version must be specified.
