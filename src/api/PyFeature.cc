03d1b7bc51a52ebb5d82d0e54a0c3369d51eae40
John Cannon <john.cannon@sydney.edu.au>
2019-10-07 11:11:59 +0000

    (symbology) Move property-to-python visitor into python feature.
    git-svn-id: http://svn.gplates.org/projects/gplates/branches/symbology-2019-oct-01@19188 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- src/api/PyFeature.cc
+++ src/api/PyFeature.cc
@@ -22,17 +22,436 @@
  * with this program; if not, write to Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+
+#include <iostream>
+
 #include "PyFeature.h"
+
+#include "PythonUtils.h"
 
 #include "feature-visitors/ShapefileAttributeFinder.h"
 #include "feature-visitors/KeyValueDictionaryFinder.h"
 
-#include "property-values/GpmlKeyValueDictionary.h"
+#include "model/FeatureVisitor.h"
 
-#include "utils/GetPropertyAsPythonObjVisitor.h"
+#include "property-values/Enumeration.h"
+#include "property-values/GeoTimeInstant.h"
+#include "property-values/GmlDataBlock.h"
+#include "property-values/GmlFile.h"
+#include "property-values/GmlGridEnvelope.h"
+#include "property-values/GmlLineString.h"
+#include "property-values/GmlMultiPoint.h"
+#include "property-values/GmlOrientableCurve.h"
+#include "property-values/GmlPoint.h"
+#include "property-values/GmlPolygon.h"
+#include "property-values/GmlRectifiedGrid.h"
+#include "property-values/GmlTimeInstant.h"
+#include "property-values/GmlTimePeriod.h"
+#include "property-values/GpmlArray.h"
+#include "property-values/GpmlTimeSample.h"
+#include "property-values/GpmlTimeWindow.h"
+#include "property-values/GpmlInterpolationFunction.h"
+#include "property-values/GpmlFiniteRotationSlerp.h"
+#include "property-values/GpmlFiniteRotation.h"
+#include "property-values/GpmlConstantValue.h"
+#include "property-values/GpmlFeatureReference.h"
+#include "property-values/GpmlFeatureSnapshotReference.h"
+#include "property-values/GpmlFiniteRotationSlerp.h"
+#include "property-values/GpmlFiniteRotation.h"
+#include "property-values/GpmlHotSpotTrailMark.h"
+#include "property-values/GpmlInterpolationFunction.h"
+#include "property-values/GpmlIrregularSampling.h"
+#include "property-values/GpmlKeyValueDictionaryElement.h"
+#include "property-values/GpmlKeyValueDictionary.h"
+#include "property-values/GpmlMeasure.h"
+#include "property-values/GpmlOldPlatesHeader.h"
+#include "property-values/GpmlPiecewiseAggregation.h"
+#include "property-values/GpmlPlateId.h"
+#include "property-values/GpmlPolarityChronId.h"
+#include "property-values/GpmlPropertyDelegate.h"
+#include "property-values/GpmlRasterBandNames.h"
+#include "property-values/GpmlRevisionId.h"
+#include "property-values/GpmlStringList.h"
+#include "property-values/GpmlTimeSample.h"
+#include "property-values/GpmlTimeWindow.h"
+#include "property-values/GpmlTopologicalLineSection.h"
+#include "property-values/GpmlTopologicalPoint.h"
+#include "property-values/GpmlTopologicalPolygon.h"
+#include "property-values/GpmlTopologicalSection.h"
+#include "property-values/UninterpretedPropertyValue.h"
+#include "property-values/XsBoolean.h"
+#include "property-values/XsDouble.h"
+#include "property-values/XsInteger.h"
+#include "property-values/XsString.h"
+
 
 #if !defined(GPLATES_NO_PYTHON)
 
+namespace GPlatesApi
+{
+	namespace
+	{
+		/*
+		 * Get property value as python object.
+		 * This visitor visits one property value each time. 
+		 * Use outer loop to iterator through all properties in a feature.
+		 */
+		class GetPropertyAsPythonObjVisitor : 
+				public GPlatesModel::ConstFeatureVisitor
+		{
+		public:
+		
+			void 
+			visit_gml_data_block(
+					gml_data_block_type &v)
+			{ 
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+
+
+			void
+			visit_gpml_plate_id(
+					gpml_plate_id_type &v)
+			{
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+
+			void 
+			visit_gml_time_period(
+					gml_time_period_type &v)
+			{
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+
+			void 
+			visit_enumeration(
+					enumeration_type &v)
+			{
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+
+			void 
+			visit_gml_line_string(
+					gml_line_string_type &v)
+			{
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+
+			void 
+			visit_gml_multi_point(
+					gml_multi_point_type &v)
+			{
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+
+			void 
+			visit_gml_orientable_curve(
+						gml_orientable_curve_type &v)
+			{
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+
+			void
+			visit_gml_point(
+						gml_point_type &v)
+			{
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+
+			void 
+			visit_gml_polygon(
+						gml_polygon_type &v)
+			{
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+
+			void 
+			visit_gml_time_instant(
+					gml_time_instant_type &v)
+			{
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+
+
+			void 
+			visit_gpml_feature_reference(
+					gpml_feature_reference_type &v)
+			{ 
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+
+
+			void 
+			visit_gpml_feature_snapshot_reference(
+					gpml_feature_snapshot_reference_type &v)
+			{ 
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+
+
+			void 
+			visit_gpml_finite_rotation(
+					gpml_finite_rotation_type &v)
+			{ 
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+
+
+			void 
+			visit_gpml_finite_rotation_slerp(
+					gpml_finite_rotation_slerp_type &v)
+			{ 
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+
+
+			void 
+			visit_gpml_hot_spot_trail_mark(
+					gpml_hot_spot_trail_mark_type &v)
+			{ 
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+
+
+			void 
+			visit_gpml_irregular_sampling(
+					gpml_irregular_sampling_type &v)
+			{ 
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+
+
+			void 
+			visit_gpml_key_value_dictionary(
+					gpml_key_value_dictionary_type &v)
+			{ 
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+
+
+			void
+			visit_gpml_measure(
+					gpml_measure_type &v)
+			{ 
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+
+
+			void 
+			visit_gpml_old_plates_header(
+					gpml_old_plates_header_type &v) 
+			{ 
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+
+
+			void
+			visit_gpml_piecewise_aggregation(
+					gpml_piecewise_aggregation_type &v)
+			{ 
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+
+
+			void
+			visit_gpml_polarity_chron_id(
+					gpml_polarity_chron_id_type &v)
+			{ 
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+
+
+			void 
+			visit_gpml_property_delegate(
+					gpml_property_delegate_type &v)
+			{ 
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+
+
+			void
+			visit_gpml_revision_id(
+					gpml_revision_id_type &v)
+			{ 
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+
+
+			void
+			visit_gpml_topological_polygon(
+					gpml_topological_polygon_type &v)
+			{ 
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+		
+
+			void
+			visit_gpml_topological_line_section(
+					gpml_topological_line_section_type &v)
+			{ 
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+
+
+			void
+			visit_gpml_topological_point(
+					gpml_topological_point_type &v)
+			{ 
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+
+
+			void 
+			visit_uninterpreted_property_value(
+					uninterpreted_property_value_type &v)
+			{ 
+				//TODO:
+				d_val =	
+					PythonUtils::qstring_to_python_string(
+							property_value_to_qstring(v));
+			}
+
+
+			void 
+			visit_xs_boolean(
+					xs_boolean_type &v)
+			{
+				d_val = bp::object(v.value());
+			}
+
+
+			void
+			visit_xs_double(
+					xs_double_type &v)
+			{
+				d_val = bp::object(v.value());
+			}
+		
+
+			void 
+			visit_xs_integer(
+					xs_integer_type &v)
+			{
+				d_val = bp::object(v.value());
+			}
+
+
+			void 
+			visit_xs_string(
+					xs_string_type &xs_string)
+			{
+				const QByteArray buf = xs_string.value().get().qstring().toUtf8();
+				d_val = bp::object(bp::str(buf.constData()));
+			}
+
+
+			void 
+			visit_gpml_constant_value(
+					gpml_constant_value_type &gpml_constant_value)
+			{
+				gpml_constant_value.value()->accept_visitor(*this);
+			}
+
+
+			bp::object
+			get_data()
+			{
+				return d_val;
+			}
+
+		protected:
+			QString
+			to_qstring(
+					const GPlatesModel::PropertyValue& data)
+			{
+				std::stringstream ss;
+				data.print_to(ss);
+				return QString(ss.str().c_str());
+			}
+		
+		private:
+			bp::object d_val;
+		};
+	}
+}
 
 void
 export_feature()
@@ -111,7 +530,7 @@ GPlatesApi::Feature::get_properties_by_name(
 		{
 			if(!is_shapefile_attr)
 			{
- 				GPlatesUtils::GetPropertyAsPythonObjVisitor visitor;
+ 				GetPropertyAsPythonObjVisitor visitor;
  				(*it)->accept_visitor(visitor);
  				bp::object data = visitor.get_data(); 
 				if(!(data.ptr() == Py_None))
