c90749ab6b7d826389abc5a24f25e93973ef71cb
John Cannon <john.cannon@sydney.edu.au>
2019-10-16 07:05:00 +0000

    (trunk) Move code into ".cc" file.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@19200 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- src/app-logic/DeformationStrainRate.h
+++ src/app-logic/DeformationStrainRate.h
@@ -26,10 +26,6 @@
 #ifndef GPLATES_APP_LOGIC_DEFORMATION_STRAIN_RATE_H
 #define GPLATES_APP_LOGIC_DEFORMATION_STRAIN_RATE_H
 
-#include <cfloat>
-#include <cmath>
-
-
 namespace GPlatesAppLogic
 {
 	/**
@@ -210,27 +206,7 @@ namespace GPlatesAppLogic
 		 * This is sqrt[trace(D^2)]: trace of the square of the rate-of-deformation symmetric tensor D.
 		 */
 		double
-		get_strain_rate_second_invariant() const
-		{
-			// Use the geodesy definition of second invariant in Kreemer et al. 2014.
-			// This amounts to sqrt[trace(D^2)] where D is the rate-of-deformation symmetric tensor.
-			// See chapter 4 of "Introduction to the mechanics of a continuous medium" by Malvern).
-			//
-			// Note that the usual second invariant is 0.5 * [trace(D)^2 - trace(D^2)].
-			// But a sqrt[trace(D^2)] is a function of invariants and hence is also invariant.
-			const RateOfDeformation rate_of_deformation = get_rate_of_deformation();
-			return std::sqrt(
-					//
-					// trace(D^2) = D_11*D_11 + D_12*D_21 + D_21*D_12 + D_22*D_22
-					//            = D_11*D_11 + D_22*D_22 + 2 * D_12*D_21
-					//            = D_11*D_11 + D_22*D_22 + 2 * D_12*D_12
-					// 
-					// where D_21 = D_12 since D is symmetric.
-					//
-					rate_of_deformation.theta_theta * rate_of_deformation.theta_theta +
-					rate_of_deformation.phi_phi * rate_of_deformation.phi_phi +
-					2 * rate_of_deformation.theta_phi * rate_of_deformation.theta_phi);
-		}
+		get_strain_rate_second_invariant() const;
 
 
 		/**
@@ -245,35 +221,7 @@ namespace GPlatesAppLogic
 		 * ...and the result is typically clamped to the range [-1, 1], although that's not done here.
 		 */
 		double
-		get_strain_rate_style() const
-		{
-			//
-			// The principal values of D are (eigenvalues of 2x2 tensor D):
-			// 
-			// D_principal = 0.5 * (D_11 + D_22) +/- sqrt[(D_12 * D21) + ((D_11 - D_22)/2)^2]
-			//             = 0.5 * (D_11 + D_22) +/- sqrt[D_12^2 + ((D_11 - D_22)/2)^2]
-			// 
-			// where D_21 = D_12 since D is symmetric.
-			//
-			const RateOfDeformation rate_of_deformation = get_rate_of_deformation();
-
-			const double half_trace_D = 0.5 * (rate_of_deformation.theta_theta + rate_of_deformation.phi_phi);
-			const double half_principal_D_diff = std::sqrt(
-					rate_of_deformation.theta_phi * rate_of_deformation.theta_phi +
-					0.25 * (rate_of_deformation.theta_theta - rate_of_deformation.phi_phi) *
-							(rate_of_deformation.theta_theta - rate_of_deformation.phi_phi));
-
-			const double principal_D_11 = half_trace_D + half_principal_D_diff;
-			const double principal_D_22 = half_trace_D - half_principal_D_diff;
-
-			const double abs_principal_D_11 = std::fabs(principal_D_11);
-			const double abs_principal_D_22 = std::fabs(principal_D_22);
-			const double max_abs_principal_D = (abs_principal_D_11 > abs_principal_D_22)
-					? abs_principal_D_11
-					: abs_principal_D_22;
-
-			return (principal_D_11 + principal_D_22) / max_abs_principal_D;
-		}
+		get_strain_rate_style() const;
 
 
 		friend
