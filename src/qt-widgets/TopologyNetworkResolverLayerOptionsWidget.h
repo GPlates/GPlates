6503c478594a1dc365529ffd2d1fa9a1d3cee3ec
John Cannon <john.cannon@sydney.edu.au>
2019-10-16 06:06:47 +0000

    (trunk) Clamp QDoubleValidator value to min or max if user tries to enter a value outside the min/max range.
    git-svn-id: http://svn.gplates.org/projects/gplates/trunk@19199 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- src/qt-widgets/TopologyNetworkResolverLayerOptionsWidget.h
+++ src/qt-widgets/TopologyNetworkResolverLayerOptionsWidget.h
@@ -173,6 +173,51 @@ namespace GPlatesQtWidgets
 
 	private:
 
+		/**
+		 * Fixes up any QValidator::Intermediate input (only when user editing has finished) so that we
+		 * always get a valid result (when user editing has finished) and hence always get an
+		 * 'editingFinished()' signal to process/finalise the user input thus avoiding confusing the user.
+		 */
+		class DoubleValidator :
+				public QDoubleValidator
+		{
+		public:
+			DoubleValidator(
+					const double &minimum,
+					const double &maximum,
+					const int decimal_places,
+					QObject *parent_) :
+				QDoubleValidator(minimum, maximum, decimal_places, parent_)
+			{  }
+
+			virtual
+			void
+			fixup(
+					QString &input) const
+			{
+				// Conversion to double assuming the system locale, falling back to C locale.
+				bool ok;
+				double value = locale().toDouble(input, &ok);
+				if (!ok)
+				{
+					// It appears QString::toDouble() only uses C locale despite its documentation.
+					value = input.toDouble(&ok);
+				}
+
+				// Clamp the value to the allowed range.
+				if (value < bottom())
+				{
+					value = bottom();
+				}
+				else if (value > top())
+				{
+					value = top();
+				}
+
+				input = locale().toString(value, 'f', decimals());
+			}
+		};
+
 		TopologyNetworkResolverLayerOptionsWidget(
 				GPlatesAppLogic::ApplicationState &application_state,
 				GPlatesPresentation::ViewState &view_state,
