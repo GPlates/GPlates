af8bc8ec3e7d0360027e4b1d4326b394fa53cf82
John Cannon <john.cannon@sydney.edu.au>
2019-11-07 06:10:44 +0000

    (python-api) Increment revision to 25 due to ReconstructionTree changes from trunk (which result in a faster pygplates.RotationModel, and also the ability to extend total rotation poles infinitely far into the distant past).
    git-svn-id: http://svn.gplates.org/projects/gplates/branches/python-api-2013-jul-25@19257 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- cmake/ConfigDefault.cmake
+++ cmake/ConfigDefault.cmake
@@ -41,7 +41,7 @@ set(GPLATES_PACKAGE_VERSION_PATCH "0")
 # Also it does not need to be incremented at each public release - for example the revision number
 # just prior to the GPlates 1.5.0 release and just after might be the same (if API did not change
 # during that time).
-set(PYGPLATES_REVISION 24)
+set(PYGPLATES_REVISION 25)
 
 # The GPlates package version.
 set(GPLATES_PACKAGE_VERSION "${GPLATES_PACKAGE_VERSION_MAJOR}.${GPLATES_PACKAGE_VERSION_MINOR}.${GPLATES_PACKAGE_VERSION_PATCH}")
