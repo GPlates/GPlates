08a0383dc073aff649f0fc1fa12b526a2f549790
John Cannon <john.cannon@sydney.edu.au>
2020-08-05 14:21:42 +0000

    (opengl3) Remove OpenGL state set keys that are not supported in OpenGL 3.3 core (our new minimum requirement).
    git-svn-id: http://svn.gplates.org/projects/gplates/branches/opengl3-2020-jul-19@19602 e74ed31e-5968-4cd8-ac0b-be5e0f3b170c

--- src/opengl/GLStateSetKeys.cc
+++ src/opengl/GLStateSetKeys.cc
@@ -25,7 +25,7 @@
 
 #include <opengl/OpenGL3.h>  // Should be included at TOP of ".cc" file.
 
-#include <QDebug>
+#include <QtGlobal>
 
 #include "GLStateSetKeys.h"
 
@@ -50,28 +50,16 @@ GPlatesOpenGL::GLStateSetKeys::GLStateSetKeys(
 	// All the remaining keys must be allocated at runtime (here).
 	key_type current_key = NUM_ENUM_KEYS;
 
-	d_generic_vertex_attribute_index_zero_base_key = current_key;
-	current_key +=
-			NUM_GENERIC_VERTEX_ATTRIBUTE_KEY_OFFSETS *
-			d_capabilities.shader.gl_max_vertex_attribs;
-
 	d_texture_image_unit_zero_base_key = current_key;
 	current_key +=
 			NUM_TEXTURE_IMAGE_UNIT_KEY_OFFSETS *
 			d_capabilities.texture.gl_max_texture_image_units;
 
-	d_texture_coord_zero_base_key = current_key;
-	current_key +=
-			NUM_TEXTURE_COORD_KEY_OFFSETS *
-			d_capabilities.texture.gl_max_texture_coords;
-
 	d_num_state_set_keys = current_key;
 
 	//qDebug() << "GLStateSetKeys: Number keys: " << d_num_state_set_keys;
 	//qDebug() << "GLStateSetKeys: gl_max_texture_image_units: " << d_capabilities.texture.gl_max_texture_image_units;
-	//qDebug() << "GLStateSetKeys: gl_max_texture_coords: " << d_capabilities.texture.gl_max_texture_coords;
 	//qDebug() << "GLStateSetKeys: NUM_TEXTURE_IMAGE_UNIT_KEY_OFFSETS: " << NUM_TEXTURE_IMAGE_UNIT_KEY_OFFSETS;
-	//qDebug() << "GLStateSetKeys: NUM_TEXTURE_COORD_KEY_OFFSETS: " << NUM_TEXTURE_COORD_KEY_OFFSETS;
 }
 
 
@@ -86,6 +74,12 @@ GPlatesOpenGL::GLStateSetKeys::get_bind_buffer_key(
 	case GL_ARRAY_BUFFER:
 		key = KEY_BIND_ARRAY_BUFFER;
 		break;
+	case GL_COPY_READ_BUFFER:
+		key = KEY_BIND_COPY_READ_BUFFER;
+		break;
+	case GL_COPY_WRITE_BUFFER:
+		key = KEY_BIND_COPY_WRITE_BUFFER;
+		break;
 	case GL_ELEMENT_ARRAY_BUFFER:
 		key = KEY_BIND_ELEMENT_ARRAY_BUFFER;
 		break;
@@ -97,6 +91,12 @@ GPlatesOpenGL::GLStateSetKeys::get_bind_buffer_key(
 		break;
 	case GL_TEXTURE_BUFFER:
 		key = KEY_BIND_TEXTURE_BUFFER;
+		break;
+	case GL_TRANSFORM_FEEDBACK_BUFFER:
+		key = KEY_BIND_TRANSFORM_FEEDBACK_BUFFER;
+		break;
+	case GL_UNIFORM_BUFFER:
+		key = KEY_BIND_UNIFORM_BUFFER;
 		break;
 
 	default:
@@ -114,20 +114,12 @@ GPlatesOpenGL::GLStateSetKeys::key_type
 GPlatesOpenGL::GLStateSetKeys::get_enable_key(
 		GLenum cap) const
 {
-	// Must be a non-texture capability - texture capabilities provided by 'get_texture_enable_key'.
-	GPlatesGlobal::Assert<GPlatesGlobal::PreconditionViolationError>(
-			cap != GL_TEXTURE_1D && cap != GL_TEXTURE_2D && cap != GL_TEXTURE_3D &&
-				cap != GL_TEXTURE_CUBE_MAP && cap != GL_TEXTURE_RECTANGLE_ARB,
-			GPLATES_ASSERTION_SOURCE);
-
 	key_type key = 0;
 
 	// We're currently only accepting a subset of all capabilities.
 	// Add more as needed.
 	switch (cap)
 	{
-	case GL_ALPHA_TEST:
-		key = KEY_ENABLE_ALPHA_TEST;
 		break;
 	case GL_BLEND:
 		key = KEY_ENABLE_BLEND;
@@ -141,9 +133,6 @@ GPlatesOpenGL::GLStateSetKeys::get_enable_key(
 	case GL_LINE_SMOOTH:
 		key = KEY_ENABLE_LINE_SMOOTH;
 		break;
-	case GL_POINT_SMOOTH:
-		key = KEY_ENABLE_POINT_SMOOTH;
-		break;
 	case GL_POLYGON_OFFSET_FILL:
 		key = KEY_ENABLE_POLYGON_OFFSET_FILL;
 		break;
@@ -156,6 +145,9 @@ GPlatesOpenGL::GLStateSetKeys::get_enable_key(
 	case GL_POLYGON_SMOOTH:
 		key = KEY_ENABLE_POLYGON_SMOOTH;
 		break;
+	case GL_RASTERIZER_DISCARD:
+		key = KEY_ENABLE_RASTERIZER_DISCARD;
+		break;
 	case GL_SCISSOR_TEST:
 		key = KEY_ENABLE_SCISSOR_TEST;
 		break;
@@ -164,37 +156,7 @@ GPlatesOpenGL::GLStateSetKeys::get_enable_key(
 		break;
 	default:
 		// Unsupported capability.
-		qWarning() << "glEnable capability not currently supported - should be easy to add though.";
-		GPlatesGlobal::Abort(GPLATES_EXCEPTION_SOURCE);
-		break;
-	}
-
-	return key;
-}
-
-
-GPlatesOpenGL::GLStateSetKeys::key_type
-GPlatesOpenGL::GLStateSetKeys::get_polygon_mode_key(
-		GLenum face) const
-{
-	key_type key = 0;
-
-	switch (face)
-	{
-	case GL_FRONT:
-		key = KEY_POLYGON_MODE_FRONT;
-		break;
-	case GL_BACK:
-		key = KEY_POLYGON_MODE_BACK;
-		break;
-	case GL_FRONT_AND_BACK:
-		// We shouldn't get here since higher level code should split into separate front and back keys.
-		qWarning() << "Interal Error: glPolygonMode 'GL_FRONT_AND_BACK' was not handled at a higher level.";
-		GPlatesGlobal::Abort(GPLATES_EXCEPTION_SOURCE);
-		break;
-	default:
-		// Unsupported capability.
-		qWarning() << "glPolygonMode: unrecognised parameter 'face'.";
+		qWarning() << "glEnable/glDisable capability not currently supported - should be easy to add though.";
 		GPlatesGlobal::Abort(GPLATES_EXCEPTION_SOURCE);
 		break;
 	}
@@ -216,11 +178,14 @@ GPlatesOpenGL::GLStateSetKeys::get_hint_key(
 	case GL_LINE_SMOOTH_HINT:
 		key = KEY_HINT_LINE_SMOOTH;
 		break;
-	case GL_POINT_SMOOTH_HINT:
-		key = KEY_HINT_POINT_SMOOTH;
-		break;
 	case GL_POLYGON_SMOOTH_HINT:
 		key = KEY_HINT_POLYGON_SMOOTH;
+		break;
+	case GL_TEXTURE_COMPRESSION_HINT:
+		key = KEY_HINT_TEXTURE_COMPRESSION_HINT;
+		break;
+	case GL_FRAGMENT_SHADER_DERIVATIVE_HINT:
+		key = KEY_HINT_FRAGMENT_SHADER_DERIVATIVE_HINT;
 		break;
 	default:
 		// Unsupported capability.
@@ -232,44 +197,6 @@ GPlatesOpenGL::GLStateSetKeys::get_hint_key(
 	return key;
 }
 
-
-GPlatesOpenGL::GLStateSetKeys::key_type
-GPlatesOpenGL::GLStateSetKeys::get_texture_enable_key(
-		GLenum texture_unit,
-		GLenum texture_target) const
-{
-	// Note that other texture targets (like GL_TEXTURE_2D_ARRAY) are not supported by 'glEnable' and
-	// 'glDisable' since they are used by shaders which don't require 'glEnable' and 'glDisable'.
-	switch (texture_target)
-	{
-	case GL_TEXTURE_1D:
-		return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_ENABLE_TEXTURE_1D);
-	case GL_TEXTURE_2D:
-		return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_ENABLE_TEXTURE_2D);
-	case GL_TEXTURE_3D:
-		return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_ENABLE_TEXTURE_3D);
-	case GL_TEXTURE_CUBE_MAP:
-		return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_ENABLE_TEXTURE_CUBE_MAP);
-	case GL_TEXTURE_RECTANGLE_ARB:
-		return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_ENABLE_TEXTURE_RECTANGLE);
-
-	case GL_TEXTURE_GEN_S:
-		return get_texture_coord_key_from_key_offset(texture_unit, TEXTURE_COORD_KEY_ENABLE_TEXTURE_GEN_S);
-	case GL_TEXTURE_GEN_T:
-		return get_texture_coord_key_from_key_offset(texture_unit, TEXTURE_COORD_KEY_ENABLE_TEXTURE_GEN_T);
-	case GL_TEXTURE_GEN_R:
-		return get_texture_coord_key_from_key_offset(texture_unit, TEXTURE_COORD_KEY_ENABLE_TEXTURE_GEN_R);
-	case GL_TEXTURE_GEN_Q:
-		return get_texture_coord_key_from_key_offset(texture_unit, TEXTURE_COORD_KEY_ENABLE_TEXTURE_GEN_Q);
-
-	default:
-		// Unsupported texture target type.
-		GPlatesGlobal::Abort(GPLATES_EXCEPTION_SOURCE);
-	}
-
-	// Shouldn't get there - keep the compiler happy.
-	throw GPlatesGlobal::AbortException(GPLATES_EXCEPTION_SOURCE);
-}
 
 GPlatesOpenGL::GLStateSetKeys::key_type
 GPlatesOpenGL::GLStateSetKeys::get_bind_texture_key(
@@ -288,24 +215,18 @@ GPlatesOpenGL::GLStateSetKeys::get_bind_texture_key(
 		return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_BIND_TEXTURE_3D);
 	case GL_TEXTURE_CUBE_MAP:
 		return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_BIND_TEXTURE_CUBE_MAP);
-	case GL_TEXTURE_RECTANGLE_ARB:
+	case GL_TEXTURE_RECTANGLE:
 		return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_BIND_TEXTURE_RECTANGLE);
-#ifdef GL_EXT_texture_array // In case old 'glew.h' header
-	case GL_TEXTURE_1D_ARRAY_EXT:
+	case GL_TEXTURE_1D_ARRAY:
 		return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_BIND_TEXTURE_1D_ARRAY);
-	case GL_TEXTURE_2D_ARRAY_EXT:
+	case GL_TEXTURE_2D_ARRAY:
 		return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_BIND_TEXTURE_2D_ARRAY);
-#endif
-#ifdef GL_ARB_texture_multisample // In case old 'glew.h' header
 	case GL_TEXTURE_2D_MULTISAMPLE:
 		return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_BIND_TEXTURE_2D_MULTISAMPLE);
 	case GL_TEXTURE_2D_MULTISAMPLE_ARRAY:
 		return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_BIND_TEXTURE_2D_MULTISAMPLE_ARRAY);
-#endif
-#ifdef GL_EXT_texture_buffer_object // In case old 'glew.h' header
-	case GL_TEXTURE_BUFFER_EXT:
+	case GL_TEXTURE_BUFFER:
 		return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_BIND_TEXTURE_BUFFER);
-#endif
 
 	default:
 		// Unsupported texture target type.
@@ -315,214 +236,6 @@ GPlatesOpenGL::GLStateSetKeys::get_bind_texture_key(
 
 	// Shouldn't get there - keep the compiler happy.
 	throw GPlatesGlobal::AbortException(GPLATES_EXCEPTION_SOURCE);
-}
-
-
-GPlatesOpenGL::GLStateSetKeys::key_type
-GPlatesOpenGL::GLStateSetKeys::get_tex_env_key(
-		GLenum texture_unit,
-		GLenum target,
-		GLenum pname) const
-{
-	// We're only supporting what we currently use - more can be added though.
-	// Also add to GLTexEnvStateSet::apply_default_state.
-	switch (target)
-	{
-	case GL_TEXTURE_ENV:
-		switch (pname)
-		{
-		case GL_TEXTURE_ENV_MODE:
-			return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_TEX_ENV_MODE);
-		case GL_TEXTURE_ENV_COLOR:
-			return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_TEX_ENV_COLOR);
-		case GL_COMBINE_RGB_ARB:
-			return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_COMBINE_RGB);
-		case GL_COMBINE_ALPHA_ARB:
-			return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_COMBINE_ALPHA);
-		case GL_SOURCE0_RGB_ARB:
-			return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_SOURCE0_RGB);
-		case GL_SOURCE0_ALPHA_ARB:
-			return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_SOURCE0_ALPHA);
-		case GL_SOURCE1_RGB_ARB:
-			return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_SOURCE1_RGB);
-		case GL_SOURCE1_ALPHA_ARB:
-			return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_SOURCE1_ALPHA);
-		case GL_SOURCE2_RGB_ARB:
-			return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_SOURCE2_RGB);
-		case GL_SOURCE2_ALPHA_ARB:
-			return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_SOURCE2_ALPHA);
-		case GL_OPERAND0_RGB_ARB:
-			return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_OPERAND0_RGB);
-		case GL_OPERAND0_ALPHA_ARB:
-			return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_OPERAND0_ALPHA);
-		case GL_OPERAND1_RGB_ARB:
-			return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_OPERAND1_RGB);
-		case GL_OPERAND1_ALPHA_ARB:
-			return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_OPERAND1_ALPHA);
-		case GL_OPERAND2_RGB_ARB:
-			return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_OPERAND2_RGB);
-		case GL_OPERAND2_ALPHA_ARB:
-			return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_OPERAND2_ALPHA);
-		case GL_RGB_SCALE_ARB:
-			return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_RGB_SCALE);
-		case GL_ALPHA_SCALE:
-			return get_texture_image_unit_key_from_key_offset(texture_unit, TEXTURE_IMAGE_UNIT_KEY_ALPHA_SCALE);
-		default:
-			// Unsupported pname.
-			GPlatesGlobal::Abort(GPLATES_EXCEPTION_SOURCE);
-			break;
-		}
-		break;
-
-	default:
-		// Unsupported target.
-		GPlatesGlobal::Abort(GPLATES_EXCEPTION_SOURCE);
-		break;
-	}
-
-	// Shouldn't get there - keep the compiler happy.
-	throw GPlatesGlobal::AbortException(GPLATES_EXCEPTION_SOURCE);
-}
-
-
-GPlatesOpenGL::GLStateSetKeys::key_type
-GPlatesOpenGL::GLStateSetKeys::get_tex_gen_key(
-		GLenum texture_unit,
-		GLenum coord,
-		GLenum pname) const
-{
-	GPlatesGlobal::Assert<GPlatesGlobal::PreconditionViolationError>(
-			coord >= GL_S && coord <= GL_Q,
-			GPLATES_ASSERTION_SOURCE);
-
-	// We're only supporting what we currently use - more can be added though.
-	// Also add to GLTexGenStateSet::apply_default_state.
-	switch (pname)
-	{
-	case GL_TEXTURE_GEN_MODE:
-		return get_texture_coord_key_from_key_offset(
-				texture_unit,
-				TextureCoordKeyOffsetType(TEXTURE_COORD_KEY_TEXTURE_GEN_MODE_S + (coord - GL_S)));
-
-	case GL_OBJECT_PLANE:
-		return get_texture_coord_key_from_key_offset(
-				texture_unit,
-				TextureCoordKeyOffsetType(TEXTURE_COORD_KEY_TEXTURE_OBJECT_PLANE_S + (coord - GL_S)));
-
-	case GL_EYE_PLANE:
-		return get_texture_coord_key_from_key_offset(
-				texture_unit,
-				TextureCoordKeyOffsetType(TEXTURE_COORD_KEY_TEXTURE_EYE_PLANE_S + (coord - GL_S)));
-
-	default:
-		// Unsupported pname.
-		GPlatesGlobal::Abort(GPLATES_EXCEPTION_SOURCE);
-		break;
-	}
-
-	// Shouldn't get there - keep the compiler happy.
-	throw GPlatesGlobal::AbortException(GPLATES_EXCEPTION_SOURCE);
-}
-
-
-GPlatesOpenGL::GLStateSetKeys::key_type
-GPlatesOpenGL::GLStateSetKeys::get_enable_client_texture_state_key(
-		GLenum texture_unit) const
-{
-	return get_texture_coord_key_from_key_offset(texture_unit, TEXTURE_COORD_KEY_ENABLE_CLIENT_STATE_TEXTURE_COORD_ARRAY);
-}
-
-
-GPlatesOpenGL::GLStateSetKeys::key_type
-GPlatesOpenGL::GLStateSetKeys::get_tex_coord_pointer_state_key(
-		GLenum texture_unit) const
-{
-	return get_texture_coord_key_from_key_offset(texture_unit, TEXTURE_COORD_KEY_VERTEX_ARRAY_TEX_COORD_POINTER);
-}
-
-
-GPlatesOpenGL::GLStateSetKeys::key_type
-GPlatesOpenGL::GLStateSetKeys::get_enable_client_state_key(
-		GLenum array) const
-{
-	key_type key = 0;
-
-	// We're not including GL_INDEX_ARRAY since using RGBA mode (and not colour index mode)
-	// and not including GL_EDGE_FLAG_ARRAY for now.
-	switch (array)
-	{
-	case GL_VERTEX_ARRAY:
-		key = KEY_ENABLE_CLIENT_STATE_VERTEX_ARRAY;
-		break;
-	case GL_COLOR_ARRAY:
-		key = KEY_ENABLE_CLIENT_STATE_COLOR_ARRAY;
-		break;
-	case GL_NORMAL_ARRAY:
-		key = KEY_ENABLE_CLIENT_STATE_NORMAL_ARRAY;
-		break;
-
-	default:
-		// Unsupported capability.
-		qWarning() << "glEnableClientState capability not currently supported - should be easy to add though.";
-		GPlatesGlobal::Abort(GPLATES_EXCEPTION_SOURCE);
-		break;
-	}
-
-	return key;
-}
-
-
-GPlatesOpenGL::GLStateSetKeys::key_type
-GPlatesOpenGL::GLStateSetKeys::get_enable_vertex_attrib_array_key(
-		GLuint attribute_index) const
-{
-	GPlatesGlobal::Assert<GPlatesGlobal::PreconditionViolationError>(
-			attribute_index < d_capabilities.shader.gl_max_vertex_attribs,
-			GPLATES_ASSERTION_SOURCE);
-
-	return d_generic_vertex_attribute_index_zero_base_key +
-		attribute_index * NUM_GENERIC_VERTEX_ATTRIBUTE_KEY_OFFSETS +
-		GENERIC_VERTEX_ATTRIBUTE_KEY_ENABLE_VERTEX_ATTRIB_POINTER;
-}
-
-
-GPlatesOpenGL::GLStateSetKeys::key_type
-GPlatesOpenGL::GLStateSetKeys::get_vertex_attrib_array_key(
-		GLuint attribute_index) const
-{
-	GPlatesGlobal::Assert<GPlatesGlobal::PreconditionViolationError>(
-			attribute_index < d_capabilities.shader.gl_max_vertex_attribs,
-			GPLATES_ASSERTION_SOURCE);
-
-	return d_generic_vertex_attribute_index_zero_base_key +
-		attribute_index * NUM_GENERIC_VERTEX_ATTRIBUTE_KEY_OFFSETS +
-		GENERIC_VERTEX_ATTRIBUTE_KEY_VERTEX_ATTRIB_POINTER;
-}
-
-
-GPlatesOpenGL::GLStateSetKeys::key_type
-GPlatesOpenGL::GLStateSetKeys::get_load_matrix_key(
-		GLenum mode) const
-{
-	key_type key = 0;
-
-	switch (mode)
-	{
-	case GL_MODELVIEW:
-		key = KEY_LOAD_MATRIX_MODELVIEW;
-		break;
-	case GL_PROJECTION:
-		key = KEY_LOAD_MATRIX_PROJECTION;
-		break;
-
-	default:
-		// Unsupported capability.
-		qWarning() << "glLoadMatrix capability not currently supported - should be easy to add though.";
-		GPlatesGlobal::Abort(GPLATES_EXCEPTION_SOURCE);
-		break;
-	}
-
-	return key;
 }
 
 
@@ -538,21 +251,5 @@ GPlatesOpenGL::GLStateSetKeys::get_texture_image_unit_key_from_key_offset(
 
 	return d_texture_image_unit_zero_base_key +
 		(texture_unit - GLCapabilities::Texture::gl_TEXTURE0) * NUM_TEXTURE_IMAGE_UNIT_KEY_OFFSETS +
-		key_offset;
-}
-
-
-GPlatesOpenGL::GLStateSetKeys::key_type
-GPlatesOpenGL::GLStateSetKeys::get_texture_coord_key_from_key_offset(
-		GLenum texture_unit,
-		TextureCoordKeyOffsetType key_offset) const
-{
-	GPlatesGlobal::Assert<GPlatesGlobal::PreconditionViolationError>(
-			texture_unit >= GL_TEXTURE0 &&
-					texture_unit < GL_TEXTURE0 + d_capabilities.texture.gl_max_texture_coords,
-			GPLATES_ASSERTION_SOURCE);
-
-	return d_texture_coord_zero_base_key +
-		(texture_unit - GLCapabilities::Texture::gl_TEXTURE0) * NUM_TEXTURE_COORD_KEY_OFFSETS +
 		key_offset;
 }
--- src/opengl/GLStateSetKeys.h
+++ src/opengl/GLStateSetKeys.h
@@ -73,66 +73,58 @@ namespace GPlatesOpenGL
 
 		/**
 		 * These keys can be used directly.
-		 *
-		 * NOTE: There is no 'get' method for these keys - use the enum value directly.
 		 */
 		enum
 		{
 			KEY_ACTIVE_TEXTURE,
 			KEY_ALPHA_FUNC,
 			KEY_BIND_ARRAY_BUFFER,
+			KEY_BIND_COPY_READ_BUFFER,
+			KEY_BIND_COPY_WRITE_BUFFER,
 			KEY_BIND_ELEMENT_ARRAY_BUFFER,
 			KEY_BIND_FRAME_BUFFER,
 			KEY_BIND_PIXEL_PACK_BUFFER,
 			KEY_BIND_PIXEL_UNPACK_BUFFER,
 			KEY_BIND_PROGRAM_OBJECT,
 			KEY_BIND_TEXTURE_BUFFER,
+			KEY_BIND_TRANSFORM_FEEDBACK_BUFFER,
+			KEY_BIND_UNIFORM_BUFFER,
 			KEY_BIND_VERTEX_ARRAY,
 			KEY_BLEND_EQUATION,
 			KEY_BLEND_FUNC,
 			KEY_CLEAR_COLOR,
 			KEY_CLEAR_DEPTH,
 			KEY_CLEAR_STENCIL,
-			KEY_CLIENT_ACTIVE_TEXTURE,
 			KEY_COLOR_MASK,
 			KEY_CULL_FACE,
 			KEY_DEPTH_FUNC,
 			KEY_DEPTH_MASK,
 			KEY_DEPTH_RANGE,
-			KEY_ENABLE_ALPHA_TEST,
 			KEY_ENABLE_BLEND,
 			KEY_ENABLE_DEPTH_TEST,
-			KEY_ENABLE_CLIENT_STATE_COLOR_ARRAY,
-			KEY_ENABLE_CLIENT_STATE_NORMAL_ARRAY,
-			KEY_ENABLE_CLIENT_STATE_VERTEX_ARRAY,
 			KEY_ENABLE_CULL_FACE,
 			KEY_ENABLE_LINE_SMOOTH,
-			KEY_ENABLE_POINT_SMOOTH,
 			KEY_ENABLE_POLYGON_OFFSET_FILL,
 			KEY_ENABLE_POLYGON_OFFSET_LINE,
 			KEY_ENABLE_POLYGON_OFFSET_POINT,
 			KEY_ENABLE_POLYGON_SMOOTH,
+			KEY_ENABLE_RASTERIZER_DISCARD,
 			KEY_ENABLE_SCISSOR_TEST,
 			KEY_ENABLE_STENCIL_TEST,
 			KEY_FRONT_FACE,
 			KEY_HINT_LINE_SMOOTH,
-			KEY_HINT_POINT_SMOOTH,
 			KEY_HINT_POLYGON_SMOOTH,
+			KEY_HINT_TEXTURE_COMPRESSION_HINT,
+			KEY_HINT_FRAGMENT_SHADER_DERIVATIVE_HINT,
 			KEY_LINE_WIDTH,
-			KEY_LOAD_MATRIX_MODELVIEW,
-			KEY_LOAD_MATRIX_PROJECTION,
-			KEY_MATRIX_MODE,
 			KEY_POINT_SIZE,
-			KEY_POLYGON_MODE_BACK,
-			KEY_POLYGON_MODE_FRONT,
+			// OpenGL 3.3 core requires 'face' (parameter of glPolygonMode) to be 'GL_FRONT_AND_BACK'...
+			KEY_POLYGON_MODE_FRONT_AND_BACK,
 			KEY_POLYGON_OFFSET,
 			KEY_SCISSOR,
 			KEY_STENCIL_FUNC,
 			KEY_STENCIL_MASK,
 			KEY_STENCIL_OP,
-			KEY_VERTEX_ARRAY_COLOR_POINTER,
-			KEY_VERTEX_ARRAY_NORMAL_POINTER,
-			KEY_VERTEX_ARRAY_VERTEX_POINTER,
 			KEY_VIEWPORT,
 
 			NUM_ENUM_KEYS // Must be last.
@@ -144,93 +136,22 @@ namespace GPlatesOpenGL
 		get_bind_buffer_key(
 				GLenum target) const;
 
-		//! For glEnable with non-texture targets.
+		//! For glEnable/glDisable.
 		key_type
 		get_enable_key(
 				GLenum cap) const;
-
-		/**
-		 * For glPolygonMode.
-		 *
-		 * NOTE: Caller must detect 'GL_FRONT_AND_BACK' and request separate keys
-		 * for 'GL_FRONT' and 'GL_BACK'.
-		 */
-		key_type
-		get_polygon_mode_key(
-				GLenum face) const;
 
 		//! For glHint.
 		key_type
 		get_hint_key(
 				GLenum target) const;
 
-		/**
-		 * For glEnable with texture targets.
-		 *
-		 * Note that this also includes texgen state (eg, GL_TEXTURE_GEN_S, etc).
-		 */
-		key_type
-		get_texture_enable_key(
-				GLenum texture_unit,
-				GLenum texture_target) const;
-
 		key_type
 		get_bind_texture_key(
 				GLenum texture_target,
 				GLenum texture_unit) const;
 
-		key_type
-		get_tex_env_key(
-				GLenum texture_unit,
-				GLenum target,
-				GLenum pname) const;
-
-		key_type
-		get_tex_gen_key(
-				GLenum texture_unit,
-				GLenum coord,
-				GLenum pname) const;
-
-		key_type
-		get_enable_client_texture_state_key(
-				GLenum texture_unit) const;
-
-		key_type
-		get_tex_coord_pointer_state_key(
-				GLenum texture_unit) const;
-
-		key_type
-		get_enable_client_state_key(
-				GLenum array) const;
-
-		key_type
-		get_enable_vertex_attrib_array_key(
-				GLuint attribute_index) const;
-
-		key_type
-		get_vertex_attrib_array_key(
-				GLuint attribute_index) const;
-
-		key_type
-		get_load_matrix_key(
-				GLenum mode) const;
-
-		key_type
-		get_load_texture_matrix_key(
-				GLenum texture_unit) const
-		{
-			return get_texture_coord_key_from_key_offset(texture_unit, TEXTURE_COORD_KEY_LOAD_MATRIX_TEXTURE);
-		}
-
 	private:
-		//! Key offsets within a particular generic vertex attribute index - offsets repeat for each subsequent index.
-		enum GenericVertexAttributeKeyOffsetType
-		{
-			GENERIC_VERTEX_ATTRIBUTE_KEY_ENABLE_VERTEX_ATTRIB_POINTER,
-			GENERIC_VERTEX_ATTRIBUTE_KEY_VERTEX_ATTRIB_POINTER,
-
-			NUM_GENERIC_VERTEX_ATTRIBUTE_KEY_OFFSETS // Must be last.
-		};
 
 		//! Key offsets within a particular texture *image* unit - offsets repeat for each subsequent texture unit.
 		enum TextureImageUnitKeyOffsetType
@@ -245,67 +166,16 @@ namespace GPlatesOpenGL
 			TEXTURE_IMAGE_UNIT_KEY_BIND_TEXTURE_BUFFER,
 			TEXTURE_IMAGE_UNIT_KEY_BIND_TEXTURE_CUBE_MAP,
 			TEXTURE_IMAGE_UNIT_KEY_BIND_TEXTURE_RECTANGLE,
-			TEXTURE_IMAGE_UNIT_KEY_ENABLE_TEXTURE_1D,
-			TEXTURE_IMAGE_UNIT_KEY_ENABLE_TEXTURE_2D,
-			TEXTURE_IMAGE_UNIT_KEY_ENABLE_TEXTURE_3D,
-			TEXTURE_IMAGE_UNIT_KEY_ENABLE_TEXTURE_CUBE_MAP,
-			TEXTURE_IMAGE_UNIT_KEY_ENABLE_TEXTURE_RECTANGLE,
-			TEXTURE_IMAGE_UNIT_KEY_TEX_ENV_MODE,
-			TEXTURE_IMAGE_UNIT_KEY_TEX_ENV_COLOR,
-			TEXTURE_IMAGE_UNIT_KEY_COMBINE_RGB,
-			TEXTURE_IMAGE_UNIT_KEY_COMBINE_ALPHA,
-			TEXTURE_IMAGE_UNIT_KEY_SOURCE0_RGB,
-			TEXTURE_IMAGE_UNIT_KEY_SOURCE1_RGB,
-			TEXTURE_IMAGE_UNIT_KEY_SOURCE2_RGB,
-			TEXTURE_IMAGE_UNIT_KEY_SOURCE0_ALPHA,
-			TEXTURE_IMAGE_UNIT_KEY_SOURCE1_ALPHA,
-			TEXTURE_IMAGE_UNIT_KEY_SOURCE2_ALPHA,
-			TEXTURE_IMAGE_UNIT_KEY_OPERAND0_RGB,
-			TEXTURE_IMAGE_UNIT_KEY_OPERAND1_RGB,
-			TEXTURE_IMAGE_UNIT_KEY_OPERAND2_RGB,
-			TEXTURE_IMAGE_UNIT_KEY_OPERAND0_ALPHA,
-			TEXTURE_IMAGE_UNIT_KEY_OPERAND1_ALPHA,
-			TEXTURE_IMAGE_UNIT_KEY_OPERAND2_ALPHA,
-			TEXTURE_IMAGE_UNIT_KEY_RGB_SCALE,
-			TEXTURE_IMAGE_UNIT_KEY_ALPHA_SCALE,
 
 			NUM_TEXTURE_IMAGE_UNIT_KEY_OFFSETS // Must be last.
 		};
 
-		//! Key offsets within a particular texture *coordinate* set - offsets repeat for each subsequent texture unit.
-		enum TextureCoordKeyOffsetType
-		{
-			TEXTURE_COORD_KEY_ENABLE_CLIENT_STATE_TEXTURE_COORD_ARRAY,
-			TEXTURE_COORD_KEY_ENABLE_TEXTURE_GEN_S,
-			TEXTURE_COORD_KEY_ENABLE_TEXTURE_GEN_T,
-			TEXTURE_COORD_KEY_ENABLE_TEXTURE_GEN_R,
-			TEXTURE_COORD_KEY_ENABLE_TEXTURE_GEN_Q,
-			TEXTURE_COORD_KEY_LOAD_MATRIX_TEXTURE,
-			TEXTURE_COORD_KEY_TEXTURE_EYE_PLANE_S,
-			TEXTURE_COORD_KEY_TEXTURE_EYE_PLANE_T,
-			TEXTURE_COORD_KEY_TEXTURE_EYE_PLANE_R,
-			TEXTURE_COORD_KEY_TEXTURE_EYE_PLANE_Q,
-			TEXTURE_COORD_KEY_TEXTURE_GEN_MODE_S,
-			TEXTURE_COORD_KEY_TEXTURE_GEN_MODE_T,
-			TEXTURE_COORD_KEY_TEXTURE_GEN_MODE_R,
-			TEXTURE_COORD_KEY_TEXTURE_GEN_MODE_Q,
-			TEXTURE_COORD_KEY_TEXTURE_OBJECT_PLANE_S,
-			TEXTURE_COORD_KEY_TEXTURE_OBJECT_PLANE_T,
-			TEXTURE_COORD_KEY_TEXTURE_OBJECT_PLANE_R,
-			TEXTURE_COORD_KEY_TEXTURE_OBJECT_PLANE_Q,
-			TEXTURE_COORD_KEY_VERTEX_ARRAY_TEX_COORD_POINTER,
-
-			NUM_TEXTURE_COORD_KEY_OFFSETS // Must be last.
-		};
-
 		const GLCapabilities &d_capabilities;
 
-		key_type d_generic_vertex_attribute_index_zero_base_key;
-
 		key_type d_texture_image_unit_zero_base_key;
-		key_type d_texture_coord_zero_base_key;
 
 		unsigned int d_num_state_set_keys;
+
 
 		//! Default constructor can only be called by @a create.
 		GLStateSetKeys(
@@ -316,12 +186,6 @@ namespace GPlatesOpenGL
 		get_texture_image_unit_key_from_key_offset(
 				GLenum texture_unit,
 				TextureImageUnitKeyOffsetType key_offset) const;
-
-		//! Calculate a key for a texture parameter for the specified texture *coordinate* set.
-		key_type
-		get_texture_coord_key_from_key_offset(
-				GLenum texture_unit,
-				TextureCoordKeyOffsetType key_offset) const;
 	};
 }
 
