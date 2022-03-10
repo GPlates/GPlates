GPlates/GPlates

commit 75e0d2c0c808d8ecb63be73ff957142260624be5

Update LayerPainter to OpenGL 3.

src/qt-resources/opengl/layer_painter/render_axially_symmetric_mesh_fragment_shader.glsl
@@ -31,49 +31,56 @@ uniform bool lighting_enabled;
 uniform float light_ambient_contribution;
 uniform vec3 world_space_light_direction;
 
-varying vec3 world_space_sphere_normal;
-varying vec3 world_space_x_axis;
-varying vec3 world_space_y_axis;
-varying vec3 world_space_z_axis;
-// The model-space coordinates are interpolated across the geometry.
-varying vec2 model_space_radial_position;
-varying vec2 radial_and_axial_normal_weights;
+in VertexData
+{
+	vec3 world_space_sphere_normal;
+	vec4 colour;
+	vec3 world_space_x_axis;
+	vec3 world_space_y_axis;
+	vec3 world_space_z_axis;
+	// The model-space coordinates are interpolated across the geometry.
+	vec2 model_space_radial_position;
+	vec2 radial_and_axial_normal_weights;
+} fs_in;
+
+layout(location = 0) out vec4 colour;
 
 void main (void)
 {
-	// If lighting is disabled simply return the vertex colour.
-	if (!lighting_enabled)
+	if (lighting_enabled)
 	{
-		gl_FragColor = gl_Color;
-		return;
+		// Calculate the model-space normal of the axially symmetric mesh at the current fragment location.
+		// The mesh, in model-space, is axially symmetric about its z-axis so we blend the radial (x,y) normal with the z-axis.
+		vec3 model_space_mesh_normal = vec3(
+				radial_and_axial_normal_weights.x * normalize(model_space_radial_position),
+				radial_and_axial_normal_weights.y);
+		
+		// Convert the model-space normal to world-space (the same space the light direction is in).
+		vec3 world_space_mesh_normal = mat3(world_space_x_axis, world_space_y_axis, world_space_z_axis) * model_space_mesh_normal;
+		
+		// Apply the Lambert diffuse lighting using the world-space normal.
+		// Note that neither the light direction nor the surface normal need be normalised.
+		float lambert = lambert_diffuse_lighting(world_space_light_direction, world_space_mesh_normal);
+		
+		// Need to clamp the lambert term to zero when the (unperturbed) sphere normal
+		// faces away from the light direction otherwise the mesh surface will appear
+		// to be lit *through* the globe (ie, not shadowed by the globe).
+		// NOTE: This is not necessary for the 2D map views (but currently we only render axially symmetric meshes in 3D globe views.
+		// The factor of 8 gives a linear falloff from 1.0 to a lower bound when dot product is below 1/8.
+		// We use a non-zero lower bound so that the mesh retains some diffuse lighting in shadow to help visualise its shape better.
+		// NOTE: Using float instead of integer parameters to 'clamp' otherwise driver compiler
+		// crashes on some systems complaining cannot find (integer overload of) function in 'stdlib'.
+		lambert *= clamp(8 * dot(world_space_light_direction, world_space_sphere_normal), 0.3, 1.0);
+
+		// Blend between ambient and diffuse lighting.
+		float ambient_and_diffuse_lighting = mix_ambient_with_diffuse_lighting(lambert, light_ambient_contribution);
+		
+		// The final fragment colour.
+		colour = vec4(fs_in.colour.rgb * ambient_and_diffuse_lighting, fs_in.colour.a);
 	}
-
-	// Calculate the model-space normal of the axially symmetric mesh at the current fragment location.
-	// The mesh, in model-space, is axially symmetric about its z-axis so we blend the radial (x,y) normal with the z-axis.
-	vec3 model_space_mesh_normal = vec3(
-			radial_and_axial_normal_weights.x * normalize(model_space_radial_position),
-			radial_and_axial_normal_weights.y);
-	
-	// Convert the model-space normal to world-space (the same space the light direction is in).
-	vec3 world_space_mesh_normal = mat3(world_space_x_axis, world_space_y_axis, world_space_z_axis) * model_space_mesh_normal;
-	
-	// Apply the Lambert diffuse lighting using the world-space normal.
-	// Note that neither the light direction nor the surface normal need be normalised.
-	float lambert = lambert_diffuse_lighting(world_space_light_direction, world_space_mesh_normal);
-	
-	// Need to clamp the lambert term to zero when the (unperturbed) sphere normal
-	// faces away from the light direction otherwise the mesh surface will appear
-	// to be lit *through* the globe (ie, not shadowed by the globe).
-	// NOTE: This is not necessary for the 2D map views (but currently we only render axially symmetric meshes in 3D globe views.
-	// The factor of 8 gives a linear falloff from 1.0 to a lower bound when dot product is below 1/8.
-	// We use a non-zero lower bound so that the mesh retains some diffuse lighting in shadow to help visualise its shape better.
-	// NOTE: Using float instead of integer parameters to 'clamp' otherwise driver compiler
-	// crashes on some systems complaining cannot find (integer overload of) function in 'stdlib'.
-	lambert *= clamp(8 * dot(world_space_light_direction, world_space_sphere_normal), 0.3, 1.0);
-
-	// Blend between ambient and diffuse lighting.
-	float ambient_and_diffuse_lighting = mix_ambient_with_diffuse_lighting(lambert, light_ambient_contribution);
-	
-	// The final fragment colour.
-	gl_FragColor = vec4(gl_Color.rgb * ambient_and_diffuse_lighting, gl_Color.a);
+	else
+	{
+		// Simply return the vertex colour.
+		colour = fs_in.colour;
+	}
 }

src/gui/LayerPainter.h
@@ -56,7 +56,8 @@
 
 namespace GPlatesOpenGL
 {
-	class GLRenderer;
+	class GL;
+	class GLViewProjection;
 }
 
 namespace GPlatesGui
@@ -168,15 +169,16 @@ namespace GPlatesGui
 			 */
 			void
 			end_painting(
-					GPlatesOpenGL::GLRenderer &renderer,
-					GPlatesOpenGL::GLBuffer &vertex_element_buffer_data,
-					GPlatesOpenGL::GLBuffer &vertex_buffer_data,
-					GPlatesOpenGL::GLVertexArray &vertex_array,
-					GPlatesOpenGL::GLVertexArray &axially_symmetric_mesh_vertex_array,
+					GPlatesOpenGL::GL &gl,
+					GPlatesOpenGL::GLBuffer::shared_ptr_type vertex_element_buffer_data,
+					GPlatesOpenGL::GLBuffer::shared_ptr_type vertex_buffer_data,
+					GPlatesOpenGL::GLVertexArray::shared_ptr_type vertex_array,
+					GPlatesOpenGL::GLVertexArray::shared_ptr_type axially_symmetric_mesh_vertex_array,
+					GPlatesOpenGL::GLProgram::shared_ptr_type render_point_line_polygon_program,
+					GPlatesOpenGL::GLProgram::shared_ptr_type render_axially_symmetric_mesh_program,
 					GPlatesOpenGL::GLVisualLayers &gl_visual_layers,
-					boost::optional<MapProjection::non_null_ptr_to_const_type> map_projection,
-					boost::optional<GPlatesOpenGL::GLProgram::shared_ptr_type> render_point_line_polygon_program,
-					boost::optional<GPlatesOpenGL::GLProgram::shared_ptr_type> render_axially_symmetric_mesh_program);
+					const GPlatesOpenGL::GLViewProjection &view_projection,
+					boost::optional<MapProjection::non_null_ptr_to_const_type> map_projection);
 
 			/**
 			 * Returns the stream for points of size @a point_size.
@@ -260,10 +262,7 @@ namespace GPlatesGui
 
 				void
 				end_painting(
-						GPlatesOpenGL::GLRenderer &renderer,
-						GPlatesOpenGL::GLBuffer &vertex_element_buffer_data,
-						GPlatesOpenGL::GLBuffer &vertex_buffer_data,
-						GPlatesOpenGL::GLVertexArray &vertex_array,
+						GPlatesOpenGL::GL &gl,
 						GLenum mode);
 
 				// Can only be called between @a begin_painting and @a end_painting.
@@ -294,18 +293,12 @@ namespace GPlatesGui
 
 				void
 				draw_primitives(
-						GPlatesOpenGL::GLRenderer &renderer,
-						GPlatesOpenGL::GLBuffer &vertex_element_buffer_data,
-						GPlatesOpenGL::GLBuffer &vertex_buffer_data,
-						GPlatesOpenGL::GLVertexArray &vertex_array,
+						GPlatesOpenGL::GL &gl,
 						GLenum mode);
 
 				void
 				draw_feedback_primitives_to_qpainter(
-						GPlatesOpenGL::GLRenderer &renderer,
-						GPlatesOpenGL::GLBuffer &vertex_element_buffer_data,
-						GPlatesOpenGL::GLBuffer &vertex_buffer_data,
-						GPlatesOpenGL::GLVertexArray &vertex_array,
+						GPlatesOpenGL::GL &gl,
 						GLenum mode);
 			};
 
@@ -351,32 +344,31 @@ namespace GPlatesGui
 
 			void
 			paint_filled_polygons(
-					GPlatesOpenGL::GLRenderer &renderer,
+					GPlatesOpenGL::GL &gl,
 					GPlatesOpenGL::GLVisualLayers &gl_visual_layers,
+					const GPlatesOpenGL::GLViewProjection &view_projection,
 					boost::optional<MapProjection::non_null_ptr_to_const_type> map_projection);
 
 			/**
 			 * Sets state in shader program for point/line/polygon primitives.
-			 *
-			 * Returns false if shader program not supported by runtime system.
 			 */
-			bool
+			void
 			set_point_line_polygon_program_state(
-					GPlatesOpenGL::GLRenderer &renderer,
+					GPlatesOpenGL::GL &gl,
+					GPlatesOpenGL::GLProgram::shared_ptr_type render_point_line_polygon_program,
 					GPlatesOpenGL::GLVisualLayers &gl_visual_layers,
-					boost::optional<MapProjection::non_null_ptr_to_const_type> map_projection,
-					boost::optional<GPlatesOpenGL::GLProgram::shared_ptr_type> render_point_line_polygon_program);
+					const GPlatesOpenGL::GLViewProjection &view_projection,
+					boost::optional<MapProjection::non_null_ptr_to_const_type> map_projection);
 
 			/**
 			 * Sets state in shader program for axially symmetric meshes.
-			 *
-			 * Returns false if shader program not supported by runtime system.
 			 */
-			bool
+			void
 			set_axially_symmetric_mesh_program_state(
-					GPlatesOpenGL::GLRenderer &renderer,
+					GPlatesOpenGL::GL &gl,
+					GPlatesOpenGL::GLProgram::shared_ptr_type render_axially_symmetric_mesh_program,
 					GPlatesOpenGL::GLVisualLayers &gl_visual_layers,
-					boost::optional<GPlatesOpenGL::GLProgram::shared_ptr_type> render_axially_symmetric_mesh_program);
+					const GPlatesOpenGL::GLViewProjection &view_projection);
 		};
 
 
@@ -512,37 +504,27 @@ namespace GPlatesGui
 				boost::optional<MapProjection::non_null_ptr_to_const_type> map_projection = boost::none);
 
 		/**
-		 * Initialise objects requiring @a GLRenderer.
+		 * Initialise objects requiring @a GL.
 		 */
 		void
 		initialise(
-				GPlatesOpenGL::GLRenderer &renderer);
+				GPlatesOpenGL::GL &gl);
 
 		/**
 		 * Must be called before streaming or queuing any primitives.
 		 */
 		void
 		begin_painting(
-				GPlatesOpenGL::GLRenderer &renderer);
+				GPlatesOpenGL::GL &gl);
 
 		/**
 		 * Renders any streamed or queued primitives.
 		 */
 		cache_handle_type
 		end_painting(
-				GPlatesOpenGL::GLRenderer &renderer,
+				GPlatesOpenGL::GL &gl,
+				const GPlatesOpenGL::GLViewProjection &view_projection,
 				float scale);
-
-		/**
-		 * Returns the render.
-		 *
-		 * NOTE: Can *only* be called between @a begin_painting and @a end_painting.
-		 */
-		GPlatesOpenGL::GLRenderer &
-		get_renderer()
-		{
-			return d_renderer.get();
-		}
 
 
 		PointLinePolygonDrawables drawables_off_the_sphere;
@@ -557,34 +539,33 @@ namespace GPlatesGui
 
 		cache_handle_type
 		paint_scalar_fields(
-				GPlatesOpenGL::GLRenderer &renderer);
+				GPlatesOpenGL::GL &gl,
+				const GPlatesOpenGL::GLViewProjection &view_projection);
 
 		cache_handle_type
 		paint_rasters(
-				GPlatesOpenGL::GLRenderer &renderer);
+				GPlatesOpenGL::GL &gl,
+				const GPlatesOpenGL::GLViewProjection &view_projection);
 
 		void
 		paint_text_drawables_2D(
-				GPlatesOpenGL::GLRenderer &renderer,
+				GPlatesOpenGL::GL &gl,
 				float scale);
 
 		void
 		paint_text_drawables_3D(
-				GPlatesOpenGL::GLRenderer &renderer,
+				GPlatesOpenGL::GL &gl,
 				float scale);
 
-
-		//! References the renderer (is only valid between @a begin_painting and @a end_painting).
-		boost::optional<GPlatesOpenGL::GLRenderer &> d_renderer;
 
 		//! For obtaining the OpenGL light and rendering rasters and scalar fields.
 		GPlatesOpenGL::GLVisualLayers::non_null_ptr_type d_gl_visual_layers;
 
 		//! Used to stream vertex elements (indices) to.
-		GPlatesOpenGL::GLVertexElementBuffer::shared_ptr_type d_vertex_element_buffer;
+		GPlatesOpenGL::GLBuffer::shared_ptr_type d_vertex_element_buffer;
 
 		//! Used to stream vertices to.
-		GPlatesOpenGL::GLVertexBuffer::shared_ptr_type d_vertex_buffer;
+		GPlatesOpenGL::GLBuffer::shared_ptr_type d_vertex_buffer;
 
 		/**
 		 * Used for vertices of type @a coloured_vertex_type (streamed to @a d_vertex_buffer).
@@ -599,23 +580,19 @@ namespace GPlatesGui
 		GPlatesOpenGL::GLVertexArray::shared_ptr_type d_axially_symmetric_mesh_vertex_array;
 
 		/**
-		 * Used for rendering to a 2D map view (is none for 3D globe view).
-		 */
-		boost::optional<MapProjection::non_null_ptr_to_const_type> d_map_projection;
-
-		/**
 		 * Shader program to render points/lines/polygons.
-		 *
-		 * Is boost::none if not supported by the runtime system.
 		 */
-		boost::optional<GPlatesOpenGL::GLProgram::shared_ptr_type> d_render_point_line_polygon_program;
+		GPlatesOpenGL::GLProgram::shared_ptr_type d_render_point_line_polygon_program;
 
 		/**
 		 * Shader program for rendering axially symmetric meshes.
-		 *
-		 * Is boost::none if not supported by the runtime system.
 		 */
-		boost::optional<GPlatesOpenGL::GLProgram::shared_ptr_type> d_render_axially_symmetric_mesh_program;
+		GPlatesOpenGL::GLProgram::shared_ptr_type d_render_axially_symmetric_mesh_program;
+
+		/**
+		 * Used for rendering to a 2D map view (is none for 3D globe view).
+		 */
+		boost::optional<MapProjection::non_null_ptr_to_const_type> d_map_projection;
 	};
 }
 

src/qt-resources/opengl/layer_painter/render_axially_symmetric_mesh_vertex_shader.glsl
@@ -27,41 +27,46 @@
 // Vertex shader source code for rendering axially symmetric meshes.
 //
 
+uniform mat4 view_projection;
+
 uniform bool lighting_enabled;
 
-attribute vec3 world_space_position_attribute;
-attribute vec4 colour_attribute;
-attribute vec3 world_space_x_axis_attribute;
-attribute vec3 world_space_y_axis_attribute;
-attribute vec3 world_space_z_axis_attribute;
-attribute vec2 model_space_radial_position_attribute;
-attribute vec2 radial_and_axial_normal_weights_attribute;
+layout(location = 0) in vec3 world_space_position;
+layout(location = 1) in vec4 colour;
+layout(location = 2) in vec3 world_space_x_axis;
+layout(location = 3) in vec3 world_space_y_axis;
+layout(location = 4) in vec3 world_space_z_axis;
+layout(location = 5) in vec2 model_space_radial_position;
+layout(location = 6) in vec2 radial_and_axial_normal_weights;
 
-varying vec3 world_space_sphere_normal;
-varying vec3 world_space_x_axis;
-varying vec3 world_space_y_axis;
-varying vec3 world_space_z_axis;
-// The model-space coordinates are interpolated across the geometry.
-varying vec2 model_space_radial_position;
-varying vec2 radial_and_axial_normal_weights;
+out VertexData
+{
+	vec3 world_space_sphere_normal;
+	vec4 colour;
+	vec3 world_space_x_axis;
+	vec3 world_space_y_axis;
+	vec3 world_space_z_axis;
+	// The model-space coordinates are interpolated across the geometry.
+	vec2 model_space_radial_position;
+	vec2 radial_and_axial_normal_weights;
+} vs_out;
 
 void main (void)
 {
-	gl_Position = gl_ModelViewProjectionMatrix * vec4(world_space_position_attribute, 1);
+	gl_Position = view_projection * vec4(world_space_position, 1);
 
 	// Output the vertex colour.
 	// We render both sides (front and back) of triangles (ie, there's no back-face culling).
-	gl_FrontColor = colour_attribute;
-	gl_BackColor = colour_attribute;
+    vs_out.colour = colour;
 
 	if (lighting_enabled)
 	{
 		// Pass to fragment shader.
-		world_space_sphere_normal = normalize(world_space_position_attribute);
-		world_space_x_axis = world_space_x_axis_attribute;
-		world_space_y_axis = world_space_y_axis_attribute;
-		world_space_z_axis = world_space_z_axis_attribute;
-		model_space_radial_position = model_space_radial_position_attribute;
-		radial_and_axial_normal_weights = radial_and_axial_normal_weights_attribute;
+		vs_out.world_space_sphere_normal = normalize(world_space_position);
+		vs_out.world_space_x_axis = world_space_x_axis;
+		vs_out.world_space_y_axis = world_space_y_axis;
+		vs_out.world_space_z_axis = world_space_z_axis;
+		vs_out.model_space_radial_position = model_space_radial_position;
+		vs_out.radial_and_axial_normal_weights = radial_and_axial_normal_weights;
 	}
 }

src/qt-resources/opengl/layer_painter/render_point_line_polygon_fragment_shader.glsl
@@ -28,7 +28,6 @@
 //
 
 uniform bool map_view_enabled;
-
 uniform bool lighting_enabled;
 
 // In the map view the lighting is constant across the map because
@@ -38,13 +37,19 @@ uniform float map_view_ambient_and_diffuse_lighting;
 uniform float globe_view_light_ambient_contribution;
 uniform vec3 globe_view_world_space_light_direction;
 
-// The world-space coordinates are interpolated across the geometry.
-varying vec3 globe_view_world_space_position;
+in VertexData
+{
+	// The world-space coordinates are interpolated across the geometry.
+	vec3 globe_view_world_space_position;
+	vec4 colour;
+} fs_in;
+
+layout(location = 0) out vec4 colour;
 
 void main (void)
 {
 	// The interpolated fragment colour.
-	vec4 colour = gl_Color;
+	colour = fs_in.colour;
 
 	if (lighting_enabled)
 	{
@@ -56,7 +61,7 @@ void main (void)
 		{
 			// Apply the Lambert diffuse lighting using the world-space position as the globe surface normal.
 			// Note that neither the light direction nor the surface normal need be normalised.
-			float lambert = lambert_diffuse_lighting(globe_view_world_space_light_direction, globe_view_world_space_position);
+			float lambert = lambert_diffuse_lighting(globe_view_world_space_light_direction, fs_in.globe_view_world_space_position);
 
 			// Blend between ambient and diffuse lighting.
 			float ambient_and_diffuse_lighting = mix_ambient_with_diffuse_lighting(lambert, globe_view_light_ambient_contribution);
@@ -64,7 +69,4 @@ void main (void)
 			colour.rgb *= ambient_and_diffuse_lighting;
 		}
 	}
-
-	// The final fragment colour.
-	gl_FragColor = colour;
 }

src/qt-resources/opengl/layer_painter/render_point_line_polygon_vertex_shader.glsl
@@ -27,30 +27,37 @@
 // Vertex shader source code to render points, lines and polygons with lighting.
 //
 
+uniform mat4 view_projection;
+			
 // The 3D globe view needs to calculate lighting across the geometry but the map view does not
 // because the surface normal is constant across the map (ie, it's flat unlike the 3D globe).
 uniform bool map_view_enabled;
-
 uniform bool lighting_enabled;
 
-// The world-space coordinates are interpolated across the geometry.
-varying vec3 globe_view_world_space_position;
+layout(location = 0) in vec4 position;
+layout(location = 1) in vec4 colour;
+
+out VertexData
+{
+	// The world-space coordinates are interpolated across the geometry.
+	vec3 globe_view_world_space_position;
+	vec4 colour;
+} vs_out;
 
 void main (void)
 {
-	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+	gl_Position = view_projection * position;
 
 	// Output the vertex colour.
 	// We render both sides (front and back) of triangles (ie, there's no back-face culling).
-	gl_FrontColor = gl_Color;
-	gl_BackColor = gl_Color;
+    vs_out.colour = colour;
 
     if (lighting_enabled)
     {
         if (!map_view_enabled)
         {
             // This assumes the geometry does not need a model transform (eg, reconstruction rotation).
-            globe_view_world_space_position = gl_Vertex.xyz / gl_Vertex.w;
+            vs_out.globe_view_world_space_position = position.xyz / position.w;
         }
     }
 }

commit 1114c3e501d2f94410b3e1be946212fedc8ca2ea

Fixed shader error recently introduced in 75e0d2c.

src/qt-resources/opengl/layer_painter/render_axially_symmetric_mesh_fragment_shader.glsl
@@ -52,11 +52,11 @@ void main (void)
 		// Calculate the model-space normal of the axially symmetric mesh at the current fragment location.
 		// The mesh, in model-space, is axially symmetric about its z-axis so we blend the radial (x,y) normal with the z-axis.
 		vec3 model_space_mesh_normal = vec3(
-				radial_and_axial_normal_weights.x * normalize(model_space_radial_position),
-				radial_and_axial_normal_weights.y);
+				fs_in.radial_and_axial_normal_weights.x * normalize(fs_in.model_space_radial_position),
+				fs_in.radial_and_axial_normal_weights.y);
 		
 		// Convert the model-space normal to world-space (the same space the light direction is in).
-		vec3 world_space_mesh_normal = mat3(world_space_x_axis, world_space_y_axis, world_space_z_axis) * model_space_mesh_normal;
+		vec3 world_space_mesh_normal = mat3(fs_in.world_space_x_axis, fs_in.world_space_y_axis, fs_in.world_space_z_axis) * model_space_mesh_normal;
 		
 		// Apply the Lambert diffuse lighting using the world-space normal.
 		// Note that neither the light direction nor the surface normal need be normalised.
@@ -70,7 +70,7 @@ void main (void)
 		// We use a non-zero lower bound so that the mesh retains some diffuse lighting in shadow to help visualise its shape better.
 		// NOTE: Using float instead of integer parameters to 'clamp' otherwise driver compiler
 		// crashes on some systems complaining cannot find (integer overload of) function in 'stdlib'.
-		lambert *= clamp(8 * dot(world_space_light_direction, world_space_sphere_normal), 0.3, 1.0);
+		lambert *= clamp(8 * dot(world_space_light_direction, fs_in.world_space_sphere_normal), 0.3, 1.0);
 
 		// Blend between ambient and diffuse lighting.
 		float ambient_and_diffuse_lighting = mix_ambient_with_diffuse_lighting(lambert, light_ambient_contribution);