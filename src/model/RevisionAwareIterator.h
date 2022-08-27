GPlates/GPlates

commit 83250d899201d99e4dabe4a29c718648ae413793

Qt6 requires C++17 which deprecated std::iterator.
- We are now required to explicitly provide the typedefs that std::iterator injected.

src/model/RevisionAwareIterator.h
@@ -28,7 +28,7 @@
 #ifndef GPLATES_MODEL_REVISIONAWAREITERATOR_H
 #define GPLATES_MODEL_REVISIONAWAREITERATOR_H
 
-#include <iterator>  /* iterator, bidirectional_iterator_tag */
+#include <iterator>  /* bidirectional_iterator_tag */
 #include <boost/operators.hpp>
 
 #include "FeatureCollectionHandle.h"
@@ -90,19 +90,6 @@ namespace GPlatesModel
 	 */
 	template<class HandleType>
 	class RevisionAwareIterator :
-			public std::iterator<
-				std::bidirectional_iterator_tag,
-				typename RevisionAwareIteratorInternals::Traits<HandleType>::value_type,
-				ptrdiff_t,
-				// The 'pointer' inner type is set to void, because the dereference
-				// operator returns a temporary, and it is not desirable to take a pointer
-				// to a temporary.
-				void,
-				// The 'reference' inner type is not a reference, because the dereference
-				// operator returns a temporary, and it is not desirable to take a reference
-				// to a temporary.
-				typename RevisionAwareIteratorInternals::Traits<HandleType>::value_type
-			>,
 			public boost::equivalent<RevisionAwareIterator<HandleType> >,
 			public boost::equality_comparable<RevisionAwareIterator<HandleType> >
 	{
@@ -138,6 +125,23 @@ namespace GPlatesModel
 		 * The type used to index the elements of the container.
 		 */
 		typedef container_size_type index_type;
+
+
+		//
+		// Iterator typedefs.
+		//
+		using iterator_category = std::bidirectional_iterator_tag;
+		using value_type = typename RevisionAwareIteratorInternals::Traits<HandleType>::value_type;
+		using difference_type = std::ptrdiff_t;
+		// The 'pointer' inner type is set to void, because the dereference
+		// operator returns a temporary, and it is not desirable to take a pointer
+		// to a temporary.
+		using pointer = void;
+		// The 'reference' inner type is not a reference, because the dereference
+		// operator returns a temporary, and it is not desirable to take a reference
+		// to a temporary.
+		using reference = typename RevisionAwareIteratorInternals::Traits<HandleType>::value_type;
+
 
 		/**
 		 * Default constructor.

src/file-io/ExportTemplateFilenameSequence.h
@@ -219,11 +219,19 @@ namespace GPlatesFileIO
 	 * Dereferencing iterator returns a 'const QString'.
 	 */
 	class ExportTemplateFilenameSequenceIterator :
-			public std::iterator<std::forward_iterator_tag, const QString>,
 			public boost::equality_comparable<ExportTemplateFilenameSequenceIterator>,
 			public boost::incrementable<ExportTemplateFilenameSequenceIterator>
 	{
 	public:
+
+		// Iterator typedefs.
+		using iterator_category = std::forward_iterator_tag;
+		using value_type = const QString;
+		using difference_type = std::ptrdiff_t;
+		using pointer = const QString *;
+		using reference = const QString &;
+
+
 		ExportTemplateFilenameSequenceIterator() :
 			d_sequence_impl(NULL),
 			d_sequence_index(0)

src/gui/ColourSchemeContainer.h
@@ -58,10 +58,17 @@ namespace GPlatesGui
 			NUM_CATEGORIES // Must be the last one.
 		};
 
-		class Iterator :
-				public std::iterator<std::input_iterator_tag, Type>
+		class Iterator
 		{
 		public:
+
+			// Iterator typedefs.
+			using iterator_category = std::input_iterator_tag;
+			using value_type = Type;
+			using difference_type = std::ptrdiff_t;
+			using pointer = Type *;
+			using reference = Type &;
+
 
 			Iterator(
 					Type curr) :

src/maths/CubeQuadTreePartition.h
@@ -258,7 +258,6 @@ namespace GPlatesMaths
 		 */
 		template <class ElementQualifiedType>
 		class ElementIterator :
-				public std::iterator<std::forward_iterator_tag, ElementQualifiedType>,
 				public boost::forward_iteratable<ElementIterator<ElementQualifiedType>, ElementQualifiedType *>
 		{
 		public:
@@ -268,6 +267,13 @@ namespace GPlatesMaths
 								typename ElementList::const_iterator,
 								typename ElementList::iterator>::type
 										element_list_iterator_type;
+
+			// Iterator typedefs.
+			using iterator_category = std::forward_iterator_tag;
+			using value_type = ElementQualifiedType;
+			using difference_type = std::ptrdiff_t;
+			using pointer = ElementQualifiedType *;
+			using reference = ElementQualifiedType &;
 
 			explicit
 			ElementIterator(

src/model/FeatureHandleWeakRefBackInserter.h
@@ -45,8 +45,7 @@ namespace GPlatesModel {
 	 * Consult Josuttis p.289 for an example insert iterator.
 	 */
 	template<typename C>
-	class FeatureHandleWeakRefBackInserter:
-			public std::iterator<std::output_iterator_tag, void, void, void, void>
+	class FeatureHandleWeakRefBackInserter
 	{
 	public:
 		/**
@@ -58,6 +57,14 @@ namespace GPlatesModel {
 		 * The type of this class.
 		 */
 		typedef FeatureHandleWeakRefBackInserter this_type;
+
+		// Iterator typedefs.
+		using iterator_category = std::output_iterator_tag;
+		using value_type = void;
+		using difference_type = void;
+		using pointer = void;
+		using reference = void;
+
 
 		/**
 		 * Construct an instance of this class which will insert into @a target_container.

src/utils/IntrusiveSinglyLinkedList.h
@@ -122,10 +122,18 @@ namespace GPlatesUtils
 		 */
 		template <class ElementNodeQualifiedType>
 		class Iterator :
-				public std::iterator<std::forward_iterator_tag, ElementNodeQualifiedType>,
 				public boost::forward_iteratable<Iterator<ElementNodeQualifiedType>, ElementNodeQualifiedType *>
 		{
 		public:
+
+			// Iterator typedefs.
+			using iterator_category = std::forward_iterator_tag;
+			using value_type = ElementNodeQualifiedType;
+			using difference_type = std::ptrdiff_t;
+			using pointer = ElementNodeQualifiedType *;
+			using reference = ElementNodeQualifiedType &;
+
+
 			explicit
 			Iterator(
 					ElementNodeQualifiedType *node = NULL) :

src/utils/Profile.cc
@@ -296,12 +296,18 @@ namespace
 		 * Links are either the child links or the parent links.
 		 */
 		class ProfileLinkIterator :
-				public std::iterator<
-						std::iterator_traits<profile_link_map_const_iterator>::iterator_category,
-						ProfileLink>,
 				public boost::equality_comparable<ProfileLinkIterator>
 		{
 		public:
+
+			// Iterator typedefs.
+			using iterator_category = std::iterator_traits<profile_link_map_const_iterator>::iterator_category;
+			using value_type = ProfileLink;
+			using difference_type = std::ptrdiff_t;
+			using pointer = ProfileLink *;
+			using reference = ProfileLink &;
+
+
 			ProfileLinkIterator(
 					const profile_link_map_type &profile_link_map,
 					const profile_link_map_const_iterator &profile_link_map_const_iter) :

src/utils/SmartNodeLinkedList.h
@@ -28,7 +28,7 @@
 #ifndef GPLATES_UTILS_SMARTNODELINKEDLIST_H
 #define GPLATES_UTILS_SMARTNODELINKEDLIST_H
 
-#include <iterator>  // std::iterator, std::bidirectional_iterator_tag
+#include <iterator>  // std::bidirectional_iterator_tag
 #include <boost/noncopyable.hpp>
 #include <boost/operators.hpp>
 #include <boost/type_traits/add_const.hpp>
@@ -203,7 +203,6 @@ namespace GPlatesUtils
 		 */
 		template <class ElementNodeQualifiedType>
 		class NodeIterator:
-				public std::iterator<std::bidirectional_iterator_tag, ElementNodeQualifiedType>,
 				public boost::bidirectional_iteratable<NodeIterator<ElementNodeQualifiedType>, ElementNodeQualifiedType *>
 		{
 		public:
@@ -215,6 +214,14 @@ namespace GPlatesUtils
 			
 			//! Typedef for a const or non-const @a Node.
 			typedef typename GPlatesUtils::CopyConst<ElementNodeQualifiedType, Node>::type node_qualified_type;
+
+			// Iterator typedefs.
+			using iterator_category = std::bidirectional_iterator_tag;
+			using value_type = element_node_qualified_type;
+			using difference_type = std::ptrdiff_t;
+			using pointer = element_node_qualified_type *;
+			using reference = element_node_qualified_type &;
+
 
 			explicit
 			NodeIterator(

commit a105ed2592dea8292a1cfede993de4b8e0a7bcf2

Fixed compile error introduced in 83250d8.

src/model/RevisionAwareIterator.h
@@ -112,11 +112,6 @@ namespace GPlatesModel
 		typedef typename HandleTraits<handle_type>::revision_type revision_type;
 
 		/**
-		 * The type returned by this iterator on dereference, with appropriate const-ness.
-		 */
-		typedef typename RevisionAwareIteratorInternals::Traits<handle_type>::value_type value_type;
-
-		/**
 		 * The type of a weak-ref to the Handle we're iterating over, with appropriate const-ness.
 		 */
 		typedef typename RevisionAwareIteratorInternals::Traits<handle_type>::handle_weak_ref_type handle_weak_ref_type;
@@ -131,6 +126,7 @@ namespace GPlatesModel
 		// Iterator typedefs.
 		//
 		using iterator_category = std::bidirectional_iterator_tag;
+		// Type returned by this iterator on dereference, with appropriate const-ness.
 		using value_type = typename RevisionAwareIteratorInternals::Traits<HandleType>::value_type;
 		using difference_type = std::ptrdiff_t;
 		// The 'pointer' inner type is set to void, because the dereference