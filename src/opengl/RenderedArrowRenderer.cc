GPlates/GPlates

commit ef1cce5dc843fa49745f53b0fdd90e028eb80fe4

Added renderer to draw rendered arrows.

src/opengl/RenderedArrowRenderer.h
@@ -1,0 +1,250 @@
+/**
+ * Copyright (C) 2022 The University of Sydney, Australia
+ *
+ * This file is part of GPlates.
+ *
+ * GPlates is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, version 2, as published by
+ * the Free Software Foundation.
+ *
+ * GPlates is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef GPLATES_OPENGL_RENDERED_ARROW_RENDERER_H
+#define GPLATES_OPENGL_RENDERED_ARROW_RENDERER_H
+
+#include <cstdint>
+#include <vector>
+
+#include "Vulkan.h"
+#include "VulkanBuffer.h"
+
+#include "gui/Colour.h"
+
+#include "maths/UnitVector3D.h"
+
+
+namespace GPlatesGui
+{
+	class SceneLightingParameters;
+}
+
+namespace GPlatesOpenGL
+{
+	class GLViewProjection;
+
+	/**
+	 * Draw rendered arrows.
+	 */
+	class RenderedArrowRenderer
+	{
+	public:
+
+		explicit
+		RenderedArrowRenderer(
+				const GPlatesGui::SceneLightingParameters &scene_lighting_parameters);
+
+
+		/**
+		 * The Vulkan device was just created.
+		 */
+		void
+		initialise_vulkan_resources(
+				Vulkan &vulkan,
+				vk::RenderPass default_render_pass,
+				vk::CommandBuffer initialisation_command_buffer,
+				vk::Fence initialisation_submit_fence);
+
+		/**
+		 * The Vulkan device is about to be destroyed.
+		 */
+		void
+		release_vulkan_resources(
+				Vulkan &vulkan);
+
+
+		/**
+		 * Added a rendered arrow (to be drawn when @a render is called).
+		 *
+		 * @param inverse_viewport_zoom_factor is used for rendering the view-dependent rendered arrows.
+		 */
+		void
+		add(
+				Vulkan &vulkan,
+				const GPlatesMaths::Vector3D &arrow_start,
+				const GPlatesMaths::Vector3D &arrow_vector,
+				float arrow_body_width,
+				float arrowhead_length,
+				const GPlatesGui::Colour &arrow_colour,
+				const double &inverse_viewport_zoom_factor);
+
+		/**
+		 * Draw the rendered arrows added so far (by @a add).
+		 */
+		void
+		render(
+				Vulkan &vulkan,
+				vk::CommandBuffer command_buffer,
+				const GLViewProjection &view_projection,
+				bool is_globe_active);
+
+	private:
+
+		//
+		// Push constants.
+		//
+		// layout (push_constant) uniform PushConstants
+		// {
+		//     mat4 view_projection;
+		//     vec3 world_space_light_direction;
+		//     bool lighting_enabled;
+		//     float light_ambient_contribution;
+		// };
+		//
+		// NOTE: This fits within the minimum required size limit of 128 bytes for push constants.
+		//       And push constants use the std430 layout.
+		//
+		struct PushConstants
+		{
+			float view_projection[16];
+			float world_space_light_direction[3];
+			std::uint32_t/*bool*/ lighting_enabled;
+			float light_ambient_contribution;
+		};
+
+
+		/**
+		 * Per-vertex data for an arrow.
+		 *
+		 * The mesh normal (used when calculating lighting in vertex/fragment shaders) is determined
+		 * by weighting the radial normal and the axial normal. We do this instead of the usual
+		 * storing of per-vertex normals because for a cone (used in arrow heads) it is difficult
+		 * to get the correct lighting at the cone apex (even when using multiple apex vertices
+		 * with same position but with different normals). For more details see
+		 * http://stackoverflow.com/questions/15283508/low-polygon-cone-smooth-shading-at-the-tip
+		 */
+		struct MeshVertex
+		{
+			// Radial (x, y) position is either (0, 0) or on the unit circle.
+			float model_space_normalised_radial_position[2];  // (x, y) position in model space
+
+			// Weights that control the model-space surface normal.
+			float model_space_radial_normal_weight;  // the radial (x, y) component of the model-space surface normal
+			float model_space_axial_normal_weight;   // the axial (z) component of the model-space surface normal
+
+			// Weights that identify where this vertex is on the arrow mesh.
+			// These are used to change the model-space radial position (on unit circle) to an arrow instance's
+			// model-space (x, y, z) position depending on that particular instance's body/head width/length.
+			float arrow_body_width_weight;  // 1.0 if vertex is on arrow body (otherwise 0.0)
+			float arrowhead_width_weight;   // 1.0 if vertex is on circular part of arrowhead cone (otherwise 0.0)
+			float arrow_body_length_weight; // 1.0 if vertex is at end of arrow body or anywhere on arrowhead (otherwise 0.0)
+			float arrowhead_length_weight;  // 1.0 if vertex is at pointy apex of arrowhead (otherwise 0.0)
+		};
+
+		/**
+		 * Per-instance data for an arrow.
+		 */
+		struct MeshInstance
+		{
+			float world_space_start_position[3];  // (x, y, z) position in world space of base of arrow
+
+			// World-space frame of reference of arrow instance.
+			// This is used to transform the model-space position and surface normal to world space.
+			float world_space_x_axis[3];
+			float world_space_y_axis[3];
+			float world_space_z_axis[3];  // direction arrow is pointing
+
+			// This arrow's body/head width/length.
+			float arrow_body_width;
+			float arrowhead_width;
+			float arrow_body_length;
+			float arrowhead_length;
+
+			float colour[4];  // arrow colour
+		};
+
+
+		void
+		create_graphics_pipeline(
+				Vulkan &vulkan,
+				vk::RenderPass default_render_pass);
+
+		void
+		create_arrow_mesh(
+				std::vector<MeshVertex> &vertices,
+				std::vector<std::uint32_t> &vertex_indices);
+
+		void
+		load_arrow_mesh(
+				Vulkan &vulkan,
+				vk::CommandBuffer initialisation_command_buffer,
+				vk::Fence initialisation_submit_fence,
+				const std::vector<MeshVertex> &vertices,
+				const std::vector<std::uint32_t> &vertex_indices);
+
+
+		/**
+		 * Number of arrow instances per dynamic vertex buffer (each instance consumes sizeof(MeshInstance) bytes).
+		 */
+		static const unsigned int NUM_ARROWS_PER_INSTANCE_BUFFER = 50000;  // ~4MB (at 80 bytes per instance)
+
+		/**
+		 * Arrow mesh tessellation (how many vertices in a circular cross-section of arrow body or head).
+		 */
+		static const unsigned int NUM_VERTICES_IN_ARROW_CIRCULAR_CROSS_SECTION = 16;
+
+		/**
+		 * Ratio of an arrowhead width to length.
+		 */
+		static constexpr float ARROWHEAD_WIDTH_TO_LENGTH_RATIO = 0.5;
+
+		/**
+		 * We want to keep the projected arrowhead size constant regardless of the
+		 * the length of the arrow body, except...
+		 *
+		 * ...if the ratio of arrowhead size to arrow body length is large enough then
+		 * we need to start scaling the arrowhead size by the arrow body length so
+		 * that the arrowhead disappears as the arrow body disappears.
+		 */
+		static constexpr float MAX_RATIO_ARROWHEAD_TO_ARROW_BODY_LENGTH = 0.5;
+
+
+		/**
+		 * Lighting parameters such as whether lighting enabled for rendered arrows,
+		 * the light direction and ambient contribution.
+		 */
+		const GPlatesGui::SceneLightingParameters &d_scene_lighting_parameters;
+
+		// Graphics pipeline and layout.
+		vk::PipelineLayout d_pipeline_layout;
+		vk::Pipeline d_graphics_pipeline;
+
+		//! Vertex buffer containing per-vertex data (static buffer).
+		VulkanBuffer d_vertex_buffer;
+		//! Index buffer (static buffer).
+		VulkanBuffer d_index_buffer;
+		//! Vertex buffer containing per-instance data (dynamic buffers).
+		VulkanBuffer d_instance_buffers[Vulkan::NUM_ASYNC_FRAMES];
+		//! Persistently mapped pointers to the per-instance buffers.
+		void *d_instance_buffer_mapped_pointers[Vulkan::NUM_ASYNC_FRAMES] = {};
+
+		//! Number of vertex indices for the single arrow mesh.
+		std::uint32_t d_num_vertex_indices = 0;
+
+		/**
+		 * Number of arrows to render.
+		 *
+		 * This is incremented by @a add and reset to zero upon returning from @a render.
+		 */
+		std::uint32_t d_num_arrows_to_render = 0;
+	};
+}
+
+#endif // GPLATES_OPENGL_RENDERED_ARROW_RENDERER_H

src/gui/SceneRenderer.cc
@@ -84,6 +84,9 @@ GPlatesGui::SceneRenderer::initialise_vulkan_resources(
 	// Initialise the background stars.
 	d_stars.initialise_vulkan_resources(vulkan, default_render_pass, initialisation_command_buffer, initialisation_submit_fence);
 
+	// Initialise rendered geometry renderer.
+	d_rendered_geometry_renderer.initialise_vulkan_resources(vulkan, default_render_pass, initialisation_command_buffer, initialisation_submit_fence);
+
 #if 0
 	// Create the shader program that sorts and blends the list of fragments (per pixel) in depth order.
 	create_sort_and_blend_scene_fragments_shader_program(gl);
@@ -119,6 +122,9 @@ GPlatesGui::SceneRenderer::release_vulkan_resources(
 	// Destroy the shader program that sorts and blends the list of fragments (per pixel) in depth order..
 	d_sort_and_blend_scene_fragments_shader_program.reset();
 #endif
+
+	// Release the rendered geometry renderer.
+	d_rendered_geometry_renderer.release_vulkan_resources(vulkan);
 
 	// Release the background stars.
 	d_stars.release_vulkan_resources(vulkan);
@@ -372,6 +378,7 @@ GPlatesGui::SceneRenderer::render_scene(
 
 	const cache_handle_type frame_cache_handle = d_rendered_geometry_renderer.render(
 			vulkan,
+			default_render_pass_command_buffer,
 			view_projection,
 			scene_view.get_viewport_zoom().zoom_factor(),
 			// Render the globe or map (and its contents) depending on whether the globe or map is currently active...

src/opengl/RenderedGeometryRenderer.cc
@@ -17,10 +17,25 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+#include <cmath>
+#include <cstddef>  // offsetof
+#include <cstring>
+#include <vector>
+
 #include "RenderedGeometryRenderer.h"
+
+#include "GLMatrix.h"
+#include "GLViewProjection.h"
+#include "VulkanException.h"
+#include "VulkanUtils.h"
+
+#include "global/GPlatesAssert.h"
+
+#include "maths/MathsUtils.h"
 
 #include "presentation/ViewState.h"
 
+#include "view-operations/RenderedArrow.h"
 #include "view-operations/RenderedGeometryCollection.h"
 #include "view-operations/RenderedGeometryLayer.h"
 
@@ -75,7 +90,8 @@ namespace GPlatesOpenGL
 GPlatesOpenGL::RenderedGeometryRenderer::RenderedGeometryRenderer(
 		GPlatesPresentation::ViewState &view_state) :
 	d_rendered_geometry_collection(view_state.get_rendered_geometry_collection()),
-	d_gl_visual_layers(GLVisualLayers::create(view_state.get_application_state()))
+	d_gl_visual_layers(GLVisualLayers::create(view_state.get_application_state())),
+	d_rendered_arrow_renderer(view_state.get_scene_lighting_parameters())
 {
 }
 
@@ -84,8 +100,11 @@ void
 GPlatesOpenGL::RenderedGeometryRenderer::initialise_vulkan_resources(
 		Vulkan &vulkan,
 		vk::RenderPass default_render_pass,
-		vk::CommandBuffer initialisation_command_buffer)
+		vk::CommandBuffer initialisation_command_buffer,
+		vk::Fence initialisation_submit_fence)
 {
+	d_rendered_arrow_renderer.initialise_vulkan_resources(
+			vulkan, default_render_pass, initialisation_command_buffer, initialisation_submit_fence);
 }
 
 
@@ -93,12 +112,14 @@ void
 GPlatesOpenGL::RenderedGeometryRenderer::release_vulkan_resources(
 		Vulkan &vulkan)
 {
+	d_rendered_arrow_renderer.release_vulkan_resources(vulkan);
 }
 
 
 GPlatesOpenGL::RenderedGeometryRenderer::cache_handle_type
 GPlatesOpenGL::RenderedGeometryRenderer::render(
 		Vulkan &vulkan,
+		vk::CommandBuffer command_buffer,
 		const GLViewProjection &view_projection,
 		const double &viewport_zoom_factor,
 		bool is_globe_active,
@@ -112,16 +133,34 @@ GPlatesOpenGL::RenderedGeometryRenderer::render(
 			is_globe_active,
 			improve_performance_reduce_quality_of_sub_surfaces_hint);
 
-	// Render the rendered geometry layers.
+	// Visit the rendered geometry layers.
 	d_rendered_geometry_collection.accept_visitor(*this);
 
+	// Render any arrows (each arrow is a 3D mesh).
+	d_rendered_arrow_renderer.render(vulkan, command_buffer, view_projection, is_globe_active);
+
 	// Get the cache handle for all the rendered geometry layers.
-	const cache_handle_type cache_handle = d_visitation_params->d_cache_handle;
+	const cache_handle_type cache_handle = d_visitation_params->cache_handle;
 
 	// These parameters are only used for the duration of this 'render()' method.
 	d_visitation_params = boost::none;
 
 	return cache_handle;
+}
+
+
+void
+GPlatesOpenGL::RenderedGeometryRenderer::visit_rendered_arrow(
+		const GPlatesViewOperations::RenderedArrow &rendered_arrow)
+{
+	d_rendered_arrow_renderer.add(
+			*d_visitation_params->vulkan,
+			GPlatesMaths::Vector3D(rendered_arrow.get_start_position().position_vector()),
+			rendered_arrow.get_vector(),
+			rendered_arrow.get_arrow_body_width(),
+			rendered_arrow.get_arrowhead_size(),
+			rendered_arrow.get_colour(),
+			d_visitation_params->inverse_viewport_zoom_factor);
 }
 
 

src/opengl/RenderedGeometryRenderer.h
@@ -20,12 +20,15 @@
 #ifndef GPLATES_OPENGL_RENDERED_GEOMETRY_RENDERER_H
 #define GPLATES_OPENGL_RENDERED_GEOMETRY_RENDERER_H
 
+#include <cstdint>
 #include <boost/noncopyable.hpp>
 #include <boost/optional.hpp>
 #include <boost/shared_ptr.hpp>
 
 #include "GLVisualLayers.h"
+#include "RenderedArrowRenderer.h"
 #include "Vulkan.h"
+#include "VulkanBuffer.h"
 
 #include "gui/Colour.h"
 
@@ -67,14 +70,13 @@ namespace GPlatesOpenGL
 
 		/**
 		 * The Vulkan device was just created.
-		 *
-		 * The initialisation command buffer will be submitted to the graphics+compute queue upon return.
 		 */
 		void
 		initialise_vulkan_resources(
 				Vulkan &vulkan,
 				vk::RenderPass default_render_pass,
-				vk::CommandBuffer initialisation_command_buffer);
+				vk::CommandBuffer initialisation_command_buffer,
+				vk::Fence initialisation_submit_fence);
 
 		/**
 		 * The Vulkan device is about to be destroyed.
@@ -93,6 +95,7 @@ namespace GPlatesOpenGL
 		cache_handle_type
 		render(
 				Vulkan &vulkan,
+				vk::CommandBuffer command_buffer,
 				const GLViewProjection &view_projection,
 				const double &viewport_zoom_factor,
 				bool is_globe_active,
@@ -109,6 +112,11 @@ namespace GPlatesOpenGL
 
 	private:
 
+		void
+		visit_rendered_arrow(
+				const GPlatesViewOperations::RenderedArrow &rendered_arrow) override;
+
+
 		/**
 		 * Returns true if any rendered geometry layer has sub-surface geometries.
 		 */
@@ -122,30 +130,30 @@ namespace GPlatesOpenGL
 		struct VisitationParams
 		{
 			VisitationParams(
-					GPlatesOpenGL::Vulkan &vulkan,
-					const GPlatesOpenGL::GLViewProjection &view_projection,
-					const double &viewport_zoom_factor,
-					bool is_globe_active,
+					GPlatesOpenGL::Vulkan &vulkan_,
+					const GPlatesOpenGL::GLViewProjection &view_projection_,
+					const double &viewport_zoom_factor_,
+					bool is_globe_active_,
 					// Used for sub-surfaces...
-					bool improve_performance_reduce_quality_of_sub_surfaces_hint = false) :
-				d_vulkan(&vulkan),
-				d_view_projection(view_projection),
-				d_inverse_viewport_zoom_factor(1.0 / viewport_zoom_factor),
-				d_is_globe_active(is_globe_active),
-				d_improve_performance_reduce_quality_of_sub_surfaces_hint(improve_performance_reduce_quality_of_sub_surfaces_hint),
-				d_cache_handle(new std::vector<cache_handle_type>())
+					bool improve_performance_reduce_quality_of_sub_surfaces_hint_ = false) :
+				vulkan(&vulkan_),
+				view_projection(view_projection_),
+				inverse_viewport_zoom_factor(1.0 / viewport_zoom_factor_),
+				is_globe_active(is_globe_active_),
+				improve_performance_reduce_quality_of_sub_surfaces_hint(improve_performance_reduce_quality_of_sub_surfaces_hint_),
+				cache_handle(new std::vector<cache_handle_type>())
 			{  }
 
-			GPlatesOpenGL::Vulkan *d_vulkan;
-			GPlatesOpenGL::GLViewProjection d_view_projection;
-			double d_inverse_viewport_zoom_factor;
-			bool d_is_globe_active;
+			GPlatesOpenGL::Vulkan *vulkan;
+			GPlatesOpenGL::GLViewProjection view_projection;
+			double inverse_viewport_zoom_factor;
+			bool is_globe_active;
 
 			// Used for sub-surfaces...
-			bool d_improve_performance_reduce_quality_of_sub_surfaces_hint;
+			bool improve_performance_reduce_quality_of_sub_surfaces_hint;
 
 			// Cache of rendered geometry layers.
-			boost::shared_ptr<std::vector<cache_handle_type> > d_cache_handle;
+			boost::shared_ptr<std::vector<cache_handle_type> > cache_handle;
 		};
 
 
@@ -158,6 +166,12 @@ namespace GPlatesOpenGL
 		 * Keeps track of OpenGL-related objects that persist from one render to the next.
 		 */
 		GPlatesOpenGL::GLVisualLayers::non_null_ptr_type d_gl_visual_layers;
+
+		/**
+		 * Renders rendered geometries of type @a RenderedArrow.
+		 */
+		RenderedArrowRenderer d_rendered_arrow_renderer;
+
 
 		/**
 		 * Parameters that are only available when a @a RenderedGeometryCollection is visiting us.

src/opengl/Stars.cc
@@ -48,11 +48,7 @@ const GPlatesGui::Colour GPlatesOpenGL::Stars::DEFAULT_COLOUR(0.75f, 0.75f, 0.75
 
 GPlatesOpenGL::Stars::Stars(
 		const GPlatesGui::Colour &colour) :
-	d_colour(colour),
-	d_num_small_star_vertices(0),
-	d_num_small_star_vertex_indices(0),
-	d_num_large_star_vertices(0),
-	d_num_large_star_vertex_indices(0)
+	d_colour(colour)
 {
 }
 
@@ -268,29 +264,17 @@ GPlatesOpenGL::Stars::create_graphics_pipeline(
 	//
 	// Colour blend state.
 	//
-	//
-	// For alpha-blending we want:
-	//
-	//   RGB = A_src * RGB_src + (1-A_src) * RGB_dst
-	//     A =     1 *   A_src + (1-A_src) *   A_dst
-	//
-	// ...so we need to use separate (src,dst) blend factors for the RGB and alpha channels...
-	//
-	//   RGB uses (A_src, 1 - A_src)
-	//     A uses (    1, 1 - A_src)
-	//
-	// ...this enables the destination to be a texture that is subsequently blended into the final scene.
-	// In this case the destination alpha must be correct in order to properly blend the texture into the final scene.
-	// However if we're rendering directly into the scene (ie, no render-to-texture) then destination alpha is not
-	// actually used (since only RGB in the final scene is visible) and therefore could use same blend factors as RGB.
-	//
 	vk::PipelineColorBlendAttachmentState blend_attachment_state;
 	blend_attachment_state
 			.setBlendEnable(true)
+			// RGB = A_src * RGB_src + (1-A_src) * RGB_dst ...
 			.setSrcColorBlendFactor(vk::BlendFactor::eSrcAlpha)
 			.setDstColorBlendFactor(vk::BlendFactor::eOneMinusSrcAlpha)
+			.setColorBlendOp(vk::BlendOp::eAdd)
+			//   A =     1 *   A_src + (1-A_src) *   A_dst ...
 			.setSrcAlphaBlendFactor(vk::BlendFactor::eOne)
 			.setDstAlphaBlendFactor(vk::BlendFactor::eOneMinusSrcAlpha)
+			.setAlphaBlendOp(vk::BlendOp::eAdd)
 			.setColorWriteMask(
 					vk::ColorComponentFlagBits::eR |
 					vk::ColorComponentFlagBits::eG |
@@ -421,7 +405,12 @@ GPlatesOpenGL::Stars::stream_stars(
 		// Randomising the distance to the stars gives a nicer 3D effect.
 		double radius = RADIUS + rand();
 
-		vertex_type vertex(x * radius, y * radius, z * radius);
+		const vertex_type vertex =
+		{
+			static_cast<float>(x * radius),
+			static_cast<float>(y * radius),
+			static_cast<float>(z * radius)
+		};
 		ok = ok && stream_points.add_vertex(vertex);
 
 		++points_generated;

src/opengl/Stars.h
@@ -27,7 +27,6 @@
 #include <boost/optional.hpp>
 
 #include "GLStreamPrimitives.h"
-#include "GLVertexUtils.h"
 #include "Vulkan.h"
 #include "VulkanBuffer.h"
 
@@ -104,7 +103,11 @@ namespace GPlatesOpenGL
 		// Ideally we'd have these points at infinity, but a large distance works well.
 		static const constexpr double RADIUS = 7.0f;
 
-		typedef GLVertexUtils::Vertex vertex_type;
+		struct Vertex
+		{
+			float x, y, z;
+		};
+		typedef Vertex vertex_type;
 		typedef std::uint32_t vertex_index_type;
 		typedef GLDynamicStreamPrimitives<vertex_type, vertex_index_type> stream_primitives_type;
 
@@ -164,10 +167,10 @@ namespace GPlatesOpenGL
 		VulkanBuffer d_vertex_buffer;
 		VulkanBuffer d_index_buffer;
 
-		unsigned int d_num_small_star_vertices;
-		unsigned int d_num_small_star_vertex_indices;
-		unsigned int d_num_large_star_vertices;
-		unsigned int d_num_large_star_vertex_indices;
+		unsigned int d_num_small_star_vertices = 0;
+		unsigned int d_num_small_star_vertex_indices = 0;
+		unsigned int d_num_large_star_vertices = 0;
+		unsigned int d_num_large_star_vertex_indices = 0;
 	};
 }
 

commit 26a47559b7bd872e1c82b40bedc9ddb9296565e8

Fix using arrow body length equal to total arrow length (bug recently introduced in ef1cce5).

src/opengl/RenderedArrowRenderer.cc
@@ -214,7 +214,7 @@ GPlatesOpenGL::RenderedArrowRenderer::render(
 	// {
 	//     vec4 frustum_planes[6];
 	//     float inverse_viewport_zoom_factor;
-	//     float max_ratio_arrowhead_to_arrow_body_length;
+	//     float max_ratio_arrowhead_length_to_arrow_length;
 	//     float arrowhead_width_to_length_ratio;
 	//     uint num_input_arrow_instances;
 	// };
@@ -234,7 +234,7 @@ GPlatesOpenGL::RenderedArrowRenderer::render(
 
 	// Extra push constants.
 	compute_push_constants.inverse_viewport_zoom_factor = inverse_viewport_zoom_factor;
-	compute_push_constants.max_ratio_arrowhead_to_arrow_body_length = MAX_RATIO_ARROWHEAD_TO_ARROW_BODY_LENGTH;
+	compute_push_constants.max_ratio_arrowhead_length_to_arrow_length = MAX_RATIO_ARROWHEAD_LENGTH_TO_ARROW_LENGTH;
 	compute_push_constants.arrowhead_width_to_length_ratio = ARROWHEAD_WIDTH_TO_LENGTH_RATIO;
 
 	// Set all push constants except the number of instances.

src/opengl/RenderedArrowRenderer.h
@@ -105,7 +105,7 @@ namespace GPlatesOpenGL
 		// {
 		//     vec4 frustum_planes[6];
 		//     float inverse_viewport_zoom_factor;
-		//     float max_ratio_arrowhead_to_arrow_body_length;
+		//     float max_ratio_arrowhead_length_to_arrow_length;
 		//     float arrowhead_width_to_length_ratio;
 		//     uint num_input_arrow_instances;
 		// };
@@ -117,7 +117,7 @@ namespace GPlatesOpenGL
 		{
 			float frustum_planes[6][4];
 			float inverse_viewport_zoom_factor;
-			float max_ratio_arrowhead_to_arrow_body_length;
+			float max_ratio_arrowhead_length_to_arrow_length;
 			float arrowhead_width_to_length_ratio;
 			std::uint32_t num_input_arrow_instances;
 		};
@@ -295,14 +295,14 @@ namespace GPlatesOpenGL
 		static constexpr float ARROWHEAD_WIDTH_TO_LENGTH_RATIO = 0.5;
 
 		/**
-		 * We want to keep the projected arrowhead size constant regardless of the
-		 * the length of the arrow body, except...
+		 * We want to keep the projected arrowhead length constant regardless of the
+		 * the length of the arrow, except...
 		 *
-		 * ...if the ratio of arrowhead size to arrow body length is large enough then
-		 * we need to start scaling the arrowhead size by the arrow body length so
-		 * that the arrowhead disappears as the arrow body disappears.
+		 * ...if the ratio of arrowhead length to arrow length is large enough then
+		 * we need to start scaling the arrowhead length by the arrow length so
+		 * that the arrowhead disappears as the arrow disappears.
 		 */
-		static constexpr float MAX_RATIO_ARROWHEAD_TO_ARROW_BODY_LENGTH = 0.5;
+		static constexpr float MAX_RATIO_ARROWHEAD_LENGTH_TO_ARROW_LENGTH = 0.5;
 
 
 		/**

src/qt-resources/vulkan/arrows.comp
@@ -25,7 +25,7 @@ layout (push_constant) uniform PushConstants
 {
     vec4 frustum_planes[6];
     float inverse_viewport_zoom_factor;
-    float max_ratio_arrowhead_to_arrow_body_length;
+    float max_ratio_arrowhead_length_to_arrow_length;
     float arrowhead_width_to_length_ratio;
     uint num_input_arrow_instances;
 };
@@ -154,47 +154,42 @@ void main()
     // Input arrow instance.
     const InputArrowInstance input_arrow_instance = input_arrow_instances.instances[input_arrow_instance_index];
     const vec3 arrow_start = input_arrow_instance.arrow_start;
-    const vec3 arrow_vector = input_arrow_instance.arrow_vector;
+    vec3 arrow_vector = input_arrow_instance.arrow_vector;
     float arrow_body_width = input_arrow_instance.arrow_body_width;
     float arrowhead_length = input_arrow_instance.arrowhead_length;
     vec4 arrow_colour = input_arrow_instance.colour;
 
-    // Calculate position from start point along vector direction to end point.
-    // The length of the arrow in world space is inversely proportional to the zoom (or magnification).
-    const vec3 arrow_end = arrow_start + inverse_viewport_zoom_factor * arrow_vector;
+    // Scale the arrow vector, the arrow body width and arrowhead length according to the zoom.
+    arrow_vector *= inverse_viewport_zoom_factor;
+    arrow_body_width *= inverse_viewport_zoom_factor;
+    arrowhead_length *= inverse_viewport_zoom_factor;
 
-    const vec3 arrow_body_vector = arrow_end - arrow_start;
-    const float arrow_body_length = length(arrow_body_vector);
-
+    const float arrow_length = length(arrow_vector);
     // Avoid divide-by-zero - and if arrow length is near zero it won't be visible.
-    if (arrow_body_length == 0)
+    if (arrow_length == 0)
     {
         return;
     }
-    const vec3 arrow_unit_vector = (1.0 / arrow_body_length) * arrow_body_vector;
+    const vec3 arrow_unit_vector = (1.0 / arrow_length) * arrow_vector;
 
     // See if arrow instance is visible (in view frustum).
-    float arrow_bounding_sphere_radius = 0.5 * arrow_body_length;
+    float arrow_bounding_sphere_radius = 0.5 * arrow_length;
     vec3 arrow_bounding_sphere_centre = arrow_start + arrow_bounding_sphere_radius * arrow_unit_vector;
     if (!sphere_intersects_frustum(arrow_bounding_sphere_centre, arrow_bounding_sphere_radius))
     {
         return;
     }
 
-    // Also scale the arrow body width and arrowhead length according to the zoom.
-    arrow_body_width *= inverse_viewport_zoom_factor;
-    arrowhead_length *= inverse_viewport_zoom_factor;
-
-    // We want to keep the projected arrowhead size constant regardless of the
-    // the length of the arrow body, except...
+    // We want to keep the projected arrowhead length constant regardless of the
+    // the length of the arrow, except...
     //
-    // ...if the ratio of arrowhead size to arrow body length is large enough then
-    // we need to start scaling the arrowhead size by the arrow body length so
-    // that the arrowhead disappears as the arrow body disappears.
-    const float max_arrowhead_size = max_ratio_arrowhead_to_arrow_body_length * arrow_body_length;
-    if (arrowhead_length > max_arrowhead_size)
+    // ...if the ratio of arrowhead length to arrow length is large enough then
+    // we need to start scaling the arrowhead length by the arrow length so
+    // that the arrowhead disappears as the arrow disappears.
+    const float max_arrowhead_length = max_ratio_arrowhead_length_to_arrow_length * arrow_length;
+    if (arrowhead_length > max_arrowhead_length)
     {
-        const float arrow_scale = max_arrowhead_size / arrowhead_length;
+        const float arrow_scale = max_arrowhead_length / arrowhead_length;
 
         arrowhead_length *= arrow_scale;
 
@@ -202,6 +197,9 @@ void main()
         arrow_body_width *= arrow_scale;
     }
     const float arrowhead_width = arrowhead_width_to_length_ratio * arrowhead_length;
+
+    // Arrow body length is the total arrow length minus the arrowhead length.
+    const float arrow_body_length = arrow_length - arrowhead_length;
 
     // Find an orthonormal basis using 'arrow_unit_vector'.
     const vec3 arrow_z_axis = arrow_unit_vector;