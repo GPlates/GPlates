c9552e6b404c51a6d98ed0d80d7bd4298c70efd2
John Cannon <john.cannon@sydney.edu.au>
2023-04-27 00:56:10 +1000

    Allow clients to override transcribing when pickling.

--- src/api/PythonPickle.h
+++ src/api/PythonPickle.h
@@ -36,6 +36,59 @@ namespace GPlatesApi
 {
 	namespace PythonPickle
 	{
+		/**
+		 * The default method of transcribing (loading/saving) an object to be used when picking/unpickling an object.
+		 *
+		 * This just delegates transcribing directly to the object itself (via its holder pointer 'ObjectHolderType'),
+		 * which means that the object type must support transcribing (see "scribe/Transcribe.h").
+		 *
+		 * This class can be specialised for a specific 'ObjectHolderType'. This might be desired if you don't want to
+		 * implement transcribing directly on a specific object type (such as when it's not clear the best
+		 * way to do that and so you'd rather just implement something that only applies to pickling and not
+		 * to other use cases that might rely on direct transcribing such as saving/loading project files).
+		 *
+		 * Note: If using this default implementation then the object type must support transcribing (see "scribe/Transcribe.h").
+		 *       This means 'ObjectHolderType' must also support transcribing, but this is supported for commonly used smart pointers
+		 *       like 'boost::shared_ptr<ObjectType>' and 'GPlatesUtils::non_null_intrusive_ptr<ObjectType>', so only
+		 *       the object type ('ObjectType') needs to implement transcribing for this default implementation to work.
+		 */
+		template <typename ObjectHolderType>
+		class Transcribe
+		{
+		public:
+
+			static
+			void
+			pickle(
+					GPlatesScribe::Scribe &scribe,
+					const ObjectHolderType &object)
+			{
+				// This saves the object holder pointer which in turns saves the object
+				// (because the holder pointer should be an owning pointer).
+				scribe.save(TRANSCRIBE_SOURCE, object, "object");
+			}
+
+			static
+			ObjectHolderType
+			unpickle(
+					GPlatesScribe::Scribe &scribe)
+			{
+				// This loads the object holder pointer which in turns loads the object
+				// (because the holder pointer should be an owning pointer).
+				GPlatesScribe::LoadRef<ObjectHolderType> object = scribe.load<ObjectHolderType>(TRANSCRIBE_SOURCE, "object");
+				// If transcribing (loading) the object failed then it is due to backwards/forwards
+				// compatibility differences between the object that was pickled into the byte stream and
+				// the object we are attempting to unpickle. This shouldn't happen unless the version of
+				// pygplates that pickled was different than the unpickling version (this version).
+				GPlatesGlobal::Assert<GPlatesScribe::Exceptions::UnsupportedVersion>(
+						object.is_valid(),
+						GPLATES_ASSERTION_SOURCE);
+
+				return object.get();
+			}
+		};
+
+
 		namespace Impl
 		{
 			/**
@@ -77,16 +130,16 @@ namespace GPlatesApi
 					const Bytes &object_bytes);
 
 
-			template <typename ObjectType>
+			template <typename ObjectHolderType>
 			Bytes
 			pickle(
-					const ObjectType &object)
+					const ObjectHolderType &object)
 			{
 				// The scribe used to save the object to a transcription.
 				GPlatesScribe::Scribe scribe;
 
 				// Transcribe the object.
-				scribe.save(TRANSCRIBE_SOURCE, object, "object");
+				Transcribe<ObjectHolderType>::pickle(scribe, object);
 
 				// Get the transcription.
 				GPlatesScribe::Transcription::non_null_ptr_to_const_type object_transcription =
@@ -98,8 +151,8 @@ namespace GPlatesApi
 				return object_bytes;
 			}
 
-			template <typename ObjectType>
-			GPlatesScribe::LoadRef<ObjectType>
+			template <typename ObjectHolderType>
+			ObjectHolderType
 			unpickle(
 					const Bytes &object_bytes)
 			{
@@ -111,74 +164,57 @@ namespace GPlatesApi
 				GPlatesScribe::Scribe scribe(object_transcription);
 
 				// Transcribe the object.
-				GPlatesScribe::LoadRef<ObjectType> object = scribe.load<ObjectType>(TRANSCRIBE_SOURCE, "object");
-				// If transcribing (loading) the object failed then it is due to backwards/forwards
-				// compatibility differences between the object that was pickled into the byte stream and
-				// the object we are attempting to unpickle. This shouldn't happen unless the version of
-				// pygplates that pickled was different than the unpickling version (this version).
-				GPlatesGlobal::Assert<GPlatesScribe::Exceptions::UnsupportedVersion>(
-						object.is_valid(),
-						GPLATES_ASSERTION_SOURCE);
-
-				return object;
+				return Transcribe<ObjectHolderType>::unpickle(scribe);
 			}
-		}
 
 
-		/**
-		 * Pickle suite for template type 'ObjectType'.
-		 *
-		 * This can be used as:
-		 *
-		 *     .def_pickle(GPlatesApi::PythonPickle::PickleSuite<ObjectType>())
-		 *
-		 * Note: We only implement 'getinitargs()'.
-		 *       We don't use 'getstate()', 'setstate()' or 'getstate_manages_dict()'.
-		 *       This is because 'getinitargs()' (a part of boost::python::pickle_suite) will pickle
-		 *       an object (of type 'ObjectType') into a byte stream (a Python 'bytes' object), and
-		 *       then @a unpickle will reverse that to convert the byte stream back into an object.
-		 *
-		 * Note: 'ObjectType' must support transcribing (see "scribe/Transcribe.h").
-		 */
-		template <typename ObjectType>
-		class PickleSuite :
-				public boost::python::pickle_suite
-		{
-		public:
-
-			static
-			boost::python::tuple
-			getinitargs(
-					const ObjectType &object)
+			/**
+			 * Pickle suite for template type 'ObjectHolderType'.
+			 *
+			 * This is used as:
+			 *
+			 *     .def_pickle(GPlatesApi::PythonPickle::Impl::PickleSuite<ObjectHolderType>())
+			 *
+			 * Note: We only implement 'getinitargs()'.
+			 *       We don't use 'getstate()', 'setstate()' or 'getstate_manages_dict()'.
+			 *       This is because 'getinitargs()' (a part of boost::python::pickle_suite) will pickle
+			 *       an object (of type 'ObjectHolderType') into a byte stream (a Python 'bytes' object), and
+			 *       then @a unpickle will reverse that to convert the byte stream back into an object.
+			 */
+			template <typename ObjectHolderType>
+			class PickleSuite :
+					public boost::python::pickle_suite
 			{
-				// Pickle object into a Bytes object.
-				Impl::Bytes object_bytes = Impl::pickle(object);
+			public:
 
-				// Convert Bytes object from C++ to Python and return it in the tuple.
-				return boost::python::make_tuple(object_bytes);
+				static
+				boost::python::tuple
+				getinitargs(
+						const ObjectHolderType &object)
+				{
+					// Pickle object into a Bytes object.
+					Bytes object_bytes = pickle(object);
+
+					// Convert Bytes object from C++ to Python and return it in the tuple.
+					return boost::python::make_tuple(object_bytes);
+				}
+			};
+
+			/**
+			 * Initialise an object by unpickling a byte stream back into an object.
+			 *
+			 * This is used to create a single-argument constructor using:
+			 *
+			 *     .def("__init__", boost::python::make_constructor(&GPlatesApi::PythonPickle::Impl::init<ObjectHolderType>))
+			 */
+			template <typename ObjectHolderType>
+			ObjectHolderType
+			init(
+					const Bytes &object_bytes)
+			{
+				// Unpickle a Bytes object into an object.
+				return unpickle<ObjectHolderType>(object_bytes);
 			}
-		};
-
-
-		/**
-		 * Initialise an object by unpickling a byte stream back into an object (of type 'ObjectType').
-		 *
-		 * This can be used to create a single-argument constructor using:
-		 *
-		 *     .def("__init__", boost::python::make_constructor(&GPlatesApi::PythonPickle::init<ObjectType>))
-		 *
-		 * Note: 'ObjectType' must support transcribing (see "scribe/Transcribe.h").
-		 */
-		template <typename ObjectType>
-		ObjectType
-		init(
-				const Impl::Bytes &object_bytes)
-		{
-			// Unpickle a Bytes object into an object.
-			GPlatesScribe::LoadRef<ObjectType> object = Impl::unpickle<ObjectType>(object_bytes);
-
-			// Return a copy of the loaded object.
-			return object.get();
 		}
 
 
@@ -211,8 +247,8 @@ namespace GPlatesApi
 					PythonClassType &python_class) const
 			{
 				python_class
-						.def("__init__", boost::python::make_constructor(&init<ObjectHolderType>))
-						.def_pickle(PickleSuite<ObjectHolderType>())
+						.def("__init__", boost::python::make_constructor(&Impl::init<ObjectHolderType>))
+						.def_pickle(Impl::PickleSuite<ObjectHolderType>())
 				;
 			}
 		};
