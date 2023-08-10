97b3f68e857920599fedddc72e908445d7213056
John Cannon <john.cannon@sydney.edu.au>
2023-08-10 17:46:50 +1000

    Renamed scene_fragments.glsl to utils/scene_tile.glsl.

--- src/qt-resources/vulkan/utils/scene_tile.glsl
+++ src/qt-resources/vulkan/utils/scene_tile.glsl
@@ -17,8 +17,8 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#ifndef SCENE_FRAGMENTS_GLSL
-#define SCENE_FRAGMENTS_GLSL
+#ifndef SCENE_TILE_GLSL
+#define SCENE_TILE_GLSL
 
 //
 // Objects rendered into the scene can add fragments to per-pixel fragment lists sorted by depth,
@@ -31,12 +31,17 @@
 // one invocation spinning on a lock and thus preventing the invocation that has the lock from releasing it
 // (eg, because the invocations happen to be executed in lock-step).
 //
+// Note: Scene rendering is limited to a fixed-size tile rather than being the size of the render target (eg, swapchain image) in order to
+//       limit graphics memory usage. Vulkan guarantees support of at least 128MB for the maximum storage buffer size,
+//       but we can fairly easily exceed this. For example, full-screen on a 4K monitor with an average of 8 fragments overlapping
+//       each pixel (at 16 bytes per fragment) consumes about 1GB of graphics memory.
+//
 
 //
-// NOTE: Callers should define SCENE_FRAGMENTS_SET and SCENE_FRAGMENTS_BINDING as integer constant expressions BEFORE including this header.
+// NOTE: Callers should define SCENE_TILE_DESCRIPTOR_SET and SCENE_TILE_DESCRIPTOR_BINDING as integer constant expressions BEFORE including this header.
 //
 
-struct SceneFragment
+struct SceneTileFragment
 {
     uint next_fragment_pointer;  // pointer to next fragment in a linked list (or zero)
     uint colour;  // colour packed with packUnorm4x8()
@@ -47,35 +52,35 @@ struct SceneFragment
 // Using 'coherent' storage images/buffers so that writes from invocations are visible to each other.
 // For example, multiple L1 caches near the GPU cores are not used, instead the single L2 cache is used as the point of coherence.
 //
-// A 2D storage image containing head pointers (uint indices) for each pixel.
-layout (set = SCENE_FRAGMENTS_SET, binding = SCENE_FRAGMENTS_BINDING, r32ui) uniform coherent uimage2D scene_fragments_list_head_pointer_image;
+// A 2D storage image containing head pointers (uint indices) for each pixel in a scene tile.
+layout (set = SCENE_TILE_DESCRIPTOR_SET, binding = SCENE_TILE_DESCRIPTOR_BINDING, r32ui) uniform coherent uimage2D scene_tile_list_head_pointer_image;
 //
-// A storage buffer containing all the scene fragments.
-layout (set = SCENE_FRAGMENTS_SET, binding = SCENE_FRAGMENTS_BINDING + 1) coherent buffer SceneFragmentsStorage
+// A storage buffer containing all the scene fragments in a scene tile.
+layout (set = SCENE_TILE_DESCRIPTOR_SET, binding = SCENE_TILE_DESCRIPTOR_BINDING + 1) coherent buffer SceneTileStorage
 {
-    SceneFragment fragments[];
-} scene_fragments_storage;
+    SceneTileFragment fragments[];
+} scene_tile_storage;
 //
 // A storage buffer containing the global allocation pointer (uint index).
-layout (set = SCENE_FRAGMENTS_SET, binding = SCENE_FRAGMENTS_BINDING + 2) coherent buffer SceneFragmentsAllocator
+layout (set = SCENE_TILE_DESCRIPTOR_SET, binding = SCENE_TILE_DESCRIPTOR_BINDING + 2) coherent buffer SceneTileAllocator
 {
     uint alloc_fragment_pointer;  // starts at 1 (so that 0 can be used as null pointer)
-} scene_fragments_allocator;
+} scene_tile_allocator;
 
 
 /*
  * Allocate and construct (with null next pointer) a scene fragment in the storage buffer.
  */
 uint
-allocate_scene_fragment(
+scene_tile_allocate_fragment(
         vec4 fragment_colour,
         float fragment_z)
 {
     // Allocate a new index into the fragment storage buffer.
-    uint fragment_pointer = atomicAdd(scene_fragments_allocator.alloc_fragment_pointer, 1);
+    uint fragment_pointer = atomicAdd(scene_tile_allocator.alloc_fragment_pointer, 1);
 
     // Construct the new fragment (in the fragment storage buffer).
-    scene_fragments_storage.fragments[fragment_pointer] = SceneFragment(
+    scene_tile_storage.fragments[fragment_pointer] = SceneTileFragment(
             0,  // next_fragment_pointer
             packUnorm4x8(fragment_colour),
             fragment_z);
@@ -90,14 +95,14 @@ allocate_scene_fragment(
  * Returns false if a fragment at 'z' would be behind an opaque fragment (and hence not visible).
  */
 bool
-search_scene_fragment(
+scene_tile_search_fragment(
         out uint previous_fragment_pointer,
         out uint next_fragment_pointer,
         float fragment_z)
 {
     // Start at the head of the fragment list.
     uint head_fragment_pointer = imageLoad(
-            scene_fragments_list_head_pointer_image,
+            scene_tile_list_head_pointer_image,
             ivec2(gl_FragCoord).xy).x;
 
     previous_fragment_pointer = 0;
@@ -107,21 +112,21 @@ search_scene_fragment(
     {
         // If the fragment 'z' value is less than the next fragment then the caller will need to
         // insert a new fragment just before the next fragment.
-        if (fragment_z < scene_fragments_storage.fragments[next_fragment_pointer].z)
+        if (fragment_z < scene_tile_storage.fragments[next_fragment_pointer].z)
         {
             return true;
         }
 
         // Our fragment will be behind the next fragment.
         // So if the next fragment is opaque then our fragment will not be visible.
-        float next_fragment_alpha = unpackUnorm4x8(scene_fragments_storage.fragments[next_fragment_pointer].colour).a;
+        float next_fragment_alpha = unpackUnorm4x8(scene_tile_storage.fragments[next_fragment_pointer].colour).a;
         if (next_fragment_alpha == 1.0)
         {
             return  false;
         }
 
         previous_fragment_pointer = next_fragment_pointer;
-        next_fragment_pointer = scene_fragments_storage.fragments[previous_fragment_pointer].next_fragment_pointer;
+        next_fragment_pointer = scene_tile_storage.fragments[previous_fragment_pointer].next_fragment_pointer;
     }
 
     // Caller will need to insert a new fragment at the tail of the list (or head if list is empty).
@@ -136,12 +141,12 @@ search_scene_fragment(
  * Note: 'head_fragment_pointer' can be zero (meaning the fragment list is currently empty).
  */
 bool
-insert_scene_fragment_at_head(
+scene_tile_insert_fragment_at_head(
         uint fragment_pointer,
         uint head_fragment_pointer)
 {
     // New fragment's next pointer points to head fragment.
-    scene_fragments_storage.fragments[fragment_pointer].next_fragment_pointer = head_fragment_pointer;
+    scene_tile_storage.fragments[fragment_pointer].next_fragment_pointer = head_fragment_pointer;
 
     // Make sure writes to our new fragment have completed before we let other invocations know of its existence
     // (by atomically inserting it into the fragment list).
@@ -150,7 +155,7 @@ insert_scene_fragment_at_head(
     // Attempt to store new *head* pointer (to new fragment) into head-pointer image, and return true if successful.
     // This will fail if another invocation has just updated the head pointer in the image.
     return imageAtomicCompSwap(
-            scene_fragments_list_head_pointer_image,
+            scene_tile_list_head_pointer_image,
             ivec2(gl_FragCoord).xy,
             head_fragment_pointer,
             fragment_pointer) == head_fragment_pointer;
@@ -164,13 +169,13 @@ insert_scene_fragment_at_head(
  * Note: 'next_fragment_pointer' can be zero (meaning the fragment is being added to the tail of the fragment list).
  */
 bool
-insert_scene_fragment(
+scene_tile_insert_fragment(
         uint fragment_pointer,
         uint previous_fragment_pointer,
         uint next_fragment_pointer)
 {
     // New fragment's next pointer points to next fragment.
-    scene_fragments_storage.fragments[fragment_pointer].next_fragment_pointer = next_fragment_pointer;
+    scene_tile_storage.fragments[fragment_pointer].next_fragment_pointer = next_fragment_pointer;
 
     // Make sure writes to our new fragment have completed before we let other invocations know of its existence
     // (by atomically inserting it into the fragment list).
@@ -179,7 +184,7 @@ insert_scene_fragment(
     // Attempt to store new pointer (to new fragment) in *previous* fragment's next pointer, and return true if successful.
     // This will fail if another invocation has just updated the next pointer in the previous fragment.
     return atomicCompSwap(
-            scene_fragments_storage.fragments[previous_fragment_pointer].next_fragment_pointer,
+            scene_tile_storage.fragments[previous_fragment_pointer].next_fragment_pointer,
             next_fragment_pointer,
             fragment_pointer) == next_fragment_pointer;
 }
@@ -191,7 +196,7 @@ insert_scene_fragment(
  * or if fragment is fully transparent, or if fragment is a helper invocation.
  */
 bool
-add_fragment_to_scene(
+scene_tile_add_fragment(
         vec4 fragment_colour,
         float fragment_z)
 {
@@ -216,7 +221,7 @@ add_fragment_to_scene(
         // Search in order of increasing fragment 'z' to find the previous and next fragments of our fragment.
         uint previous_fragment_pointer;
         uint next_fragment_pointer;
-        if (!search_scene_fragment(previous_fragment_pointer, next_fragment_pointer, fragment_z))
+        if (!scene_tile_search_fragment(previous_fragment_pointer, next_fragment_pointer, fragment_z))
         {
             // If we've already allocated/constructed a fragment then it'll remain allocated but unused.
             //
@@ -229,7 +234,7 @@ add_fragment_to_scene(
         // If we've not allocated/constructed our fragment yet then do so now.
         if (fragment_pointer == 0)
         {
-            fragment_pointer = allocate_scene_fragment(fragment_colour, fragment_z);
+            fragment_pointer = scene_tile_allocate_fragment(fragment_colour, fragment_z);
         }
 
         // Insert our fragment into the fragment list at the current fragment coordinate (pixel).
@@ -237,7 +242,7 @@ add_fragment_to_scene(
         {
             // No previous fragment means we need to insert at the head of the list.
             // Note that the next fragment can also be null (meaning the list is currently empty).
-            if (insert_scene_fragment_at_head(fragment_pointer, next_fragment_pointer))
+            if (scene_tile_insert_fragment_at_head(fragment_pointer, next_fragment_pointer))
             {
                 return true;
             }
@@ -246,7 +251,7 @@ add_fragment_to_scene(
         {
             // There's a previous fragment, so we can insert after it.
             // Note that the next fragment can be null (meaning we're inserting a new tail).
-            if (insert_scene_fragment(fragment_pointer, previous_fragment_pointer, next_fragment_pointer))
+            if (scene_tile_insert_fragment(fragment_pointer, previous_fragment_pointer, next_fragment_pointer))
             {
                 return true;
             }
@@ -260,4 +265,4 @@ add_fragment_to_scene(
     return false;
 }
 
-#endif // SCENE_FRAGMENTS_GLSL
+#endif // SCENE_TILE_GLSL
