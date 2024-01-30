GPlates/GPlates

commit 844dab5779a9883a6eb10d2ff93e3a88ea95231b

NetRotationUtils now has only two classes that simplify net rotation.
These are NetRotationAccumulator and NetRotationCalculator.

src/app-logic/NetRotationUtils.cc
@@ -23,206 +23,297 @@
 
 #include "maths/CalculateVelocity.h"
 #include "maths/FiniteRotation.h"
+#include "maths/MathsUtils.h"
 
 #include "NetRotationUtils.h"
 #include "ReconstructionTree.h"
 #include "RotationUtils.h"
 
-namespace
-{
+#include "global/GPlatesAssert.h"
+#include "global/PreconditionViolationError.h"
 
 
-
-	void
-	display_net_rotation_output_(
-			const GPlatesAppLogic::NetRotationUtils::net_rotation_map_type &results,
-			const double &time,
-			bool also_by_plate = true)
-	{
-		qDebug() << "Time: " << time;
-		if (also_by_plate)
-		{
-			qDebug() << "\tBegin print out of net rotations per plate id";
-			qDebug() << "\tNet rotation per plate id: ";
-		}
-
-		try
-		{
-
-			GPlatesAppLogic::NetRotationUtils::net_rotation_map_type::const_iterator it = results.begin();
-
-			GPlatesMaths::Vector3D total_weighted;
-			GPlatesMaths::Vector3D total_unweighted;
-			double total_weight = 0.;
-			for (; it != results.end() ; ++it)
-			{
-				GPlatesAppLogic::NetRotationUtils::NetRotationResult result = it->second;
-
-				if (GPlatesMaths::are_almost_exactly_equal(result.d_weighting_factor,0.)) return;
-				GPlatesMaths::Vector3D omega(result.d_rotation_component.x()/result.d_weighting_factor,
-											 result.d_rotation_component.y()/result.d_weighting_factor,
-											 result.d_rotation_component.z()/result.d_weighting_factor);
-
-				total_weighted = total_weighted + omega;
-				total_unweighted = total_unweighted + result.d_rotation_component;
-				total_weight += result.d_weighting_factor;
-
-
-				double mag = omega.magnitude().dval();
-
-				if (also_by_plate)
-				{
-					qDebug() << "\t\tPlate id: " << it->first;
-					qDebug() << "\t\tOmega pre weight (xyz): " << result.d_rotation_component.x().dval() << result.d_rotation_component.y().dval() << result.d_rotation_component.z().dval();
-					qDebug() << "\t\tOmega post weight (xyz): " << omega.x().dval() << omega.y().dval() << omega.z().dval();
-					if (!GPlatesMaths::are_almost_exactly_equal(mag,0))
-					{
-						std::pair<GPlatesMaths::LatLonPoint, double> pole =
-								GPlatesAppLogic::NetRotationUtils::convert_net_rotation_xyz_to_pole(omega);
-						qDebug() << "\t\tOmega (pole): " << pole.first.latitude() << pole.first.longitude() << pole.second;
-					}
-
-					qDebug() << "\t\tWeighting factor " << result.d_weighting_factor;
-					qDebug();
-				}
-			}
-
-			if (GPlatesMaths::are_almost_exactly_equal(total_weight,0.))
-			{
-				qDebug() << "Zero total weights";
-				return;
-			}
-			GPlatesMaths::Vector3D total(total_unweighted.x()/total_weight,
-										 total_unweighted.y()/total_weight,
-										 total_unweighted.z()/total_weight);
-
-			double mag = total.magnitude().dval();
-			qDebug() << "\t\tTotal Omega (xyz): " << total.x().dval() << total.y().dval() << total.z().dval();
-			if (!GPlatesMaths::are_almost_exactly_equal(mag,0))
-			{
-				std::pair<GPlatesMaths::LatLonPoint, double> pole =
-						GPlatesAppLogic::NetRotationUtils::convert_net_rotation_xyz_to_pole(total);
-				qDebug() << "\t\t Total Omega (pole): " << pole.first.latitude() << pole.first.longitude() << pole.second;
-				qDebug() << "FLAG " << "\tTime: " << time << "\t Total Omega (pole): " << pole.first.latitude() << pole.first.longitude() << pole.second;
-			}
-		}
-		catch(std::exception &e)
-		{
-			qDebug() << "Caught exception: " << e.what();
-		}
-	}
-}
-
-void
-GPlatesAppLogic::NetRotationUtils::sum_net_rotations(
-		const NetRotationUtils::net_rotation_map_type::value_type &net_rotation,
-		NetRotationUtils::net_rotation_map_type &net_rotations)
-{
-	GPlatesModel::integer_plate_id_type id = net_rotation.first;
-	NetRotationUtils::NetRotationResult result = net_rotation.second;
-
-	NetRotationUtils::net_rotation_map_type::iterator it =
-			net_rotations.find(id);
-
-	// If we don't have a map entry for our plate-id, make a new one.
-	if (it == net_rotations.end())
-	{
-		net_rotations.insert(NetRotationUtils::net_rotation_map_type::value_type(id,result));
-	}
-	else
-	{
-		// Add the current rotation data to the running total for the plate-id
-		NetRotationUtils::NetRotationResult old_result = it->second;
-
-		old_result.d_rotation_component = old_result.d_rotation_component + result.d_rotation_component;
-		old_result.d_weighting_factor += result.d_weighting_factor;
-		old_result.d_plate_area_component += result.d_plate_area_component;
-		it->second = old_result;
-	}
-}
-
-void
-GPlatesAppLogic::NetRotationUtils::display_net_rotation_output(
-		const GPlatesAppLogic::NetRotationUtils::net_rotation_map_type &results,
-		const double &time,
-		bool also_by_plate)
-{
-	display_net_rotation_output_(results,time,also_by_plate);
-}
-
-std::pair<GPlatesMaths::LatLonPoint, double>
-GPlatesAppLogic::NetRotationUtils::convert_net_rotation_xyz_to_pole(
-		const GPlatesMaths::Vector3D v)
-{
-	using namespace GPlatesMaths;
-	double mag = v.magnitude().dval();
-
-	UnitVector3D uv = v.get_normalisation();
-
-	LatLonPoint llp = make_lat_lon_point(PointOnSphere(uv));
-
-	return std::make_pair(llp,mag);
-}
-
-
-GPlatesMaths::Vector3D
-GPlatesAppLogic::NetRotationUtils::convert_net_rotation_pole_to_xyz(
-		const GPlatesMaths::LatLonPoint &llp, const double &angle)
-{
-	// Angle should already be in form degrees per million years
-	using namespace GPlatesMaths;
-	PointOnSphere p = make_point_on_sphere(llp);
-	UnitVector3D uv = p.position_vector();
-	Vector3D v = angle*Vector3D(uv);
-	return v;
-}
-
-GPlatesAppLogic::NetRotationUtils::NetRotationResult
-GPlatesAppLogic::NetRotationUtils::calc_net_rotation_contribution(
+GPlatesAppLogic::NetRotationUtils::NetRotationAccumulator
+GPlatesAppLogic::NetRotationUtils::NetRotationAccumulator::create(
 		const GPlatesMaths::PointOnSphere &point,
 		const GPlatesMaths::FiniteRotation &stage_pole,
-		double time_interval)
+		const double &time_interval,
+		const double &sample_square_length_in_radians)
 {
-	using namespace GPlatesMaths;
+	if (GPlatesMaths::are_almost_exactly_equal(time_interval, 0))
+	{
+		return NetRotationAccumulator();
+	}
 
 	if (represents_identity_rotation(stage_pole.unit_quat()))
 	{
-		return NetRotationResult(Vector3D(),0.,0.,0.);
+		return NetRotationAccumulator();
 	}
 
-	if (are_almost_exactly_equal(time_interval,0.))
+	const GPlatesMaths::Vector3D stage_pole_vector = convert_finite_rotation_to_rotation_vector(stage_pole);
+
+	const GPlatesMaths::Vector3D v = cross(stage_pole_vector, point.position_vector());
+
+	const GPlatesMaths::Vector3D omega = cross(point.position_vector(), v);
+
+	const double x = point.position_vector().x().dval();
+	const double y = point.position_vector().y().dval();
+	const double cos_latitude_squared = x * x + y * y;
+	// Cosine is positive for the full latitude range [-pi/2, pi/2].
+	const double cos_latitude = std::sqrt(cos_latitude_squared);
+
+	// Area of the sample square surrounding the sample point (in steradians, or square radians).
+	const double area_steradians = cos_latitude * sample_square_length_in_radians * sample_square_length_in_radians;
+
+	// Rotation component is weighted by the sample area.
+	const GPlatesMaths::Vector3D rotation_component = omega * area_steradians;
+
+	// Rotation component is weight by the sample area.
+	const double weighting_factor = cos_latitude_squared * area_steradians;
+
+	return NetRotationAccumulator(omega, weighting_factor, area_steradians);
+}
+
+void
+GPlatesAppLogic::NetRotationUtils::NetRotationAccumulator::add(
+		const NetRotationAccumulator &net_rotation)
+{
+	d_rotation_component = d_rotation_component + net_rotation.d_rotation_component;
+	d_weighting_factor += net_rotation.d_weighting_factor;
+	d_area_steradians += net_rotation.d_area_steradians;
+}
+
+GPlatesMaths::FiniteRotation
+GPlatesAppLogic::NetRotationUtils::NetRotationAccumulator::get_net_rotation() const
+{
+	if (GPlatesMaths::are_almost_exactly_equal(d_weighting_factor, 0))
 	{
-		return NetRotationResult(Vector3D(),0.,0.,0.);
+		return GPlatesMaths::FiniteRotation::create_identity_rotation();
 	}
 
-	const UnitQuaternion3D uq = stage_pole.unit_quat();
+	const GPlatesMaths::Vector3D weighted_rotation_component = (1.0 / d_weighting_factor) * d_rotation_component;
 
-	const boost::optional<GPlatesMaths::UnitVector3D> &axis_hint = stage_pole.axis_hint();
-	UnitQuaternion3D::RotationParams params = uq.get_rotation_params(axis_hint);
+	// Extract finite rotation from rotation rate vector.
+	return convert_rotation_vector_to_finite_rotation(weighted_rotation_component);
+}
 
-	// Angle from quaternion is radians
-	// Convert angle to degrees per Ma.
-	double angle = convert_rad_to_deg(params.angle).dval()/time_interval;
+boost::optional<std::pair<GPlatesMaths::LatLonPoint, double>>
+GPlatesAppLogic::NetRotationUtils::NetRotationAccumulator::get_net_rotation_lat_lon_pole_and_angle() const
+{
+	// Get the net rotation as a finite rotation.
+	const GPlatesMaths::FiniteRotation finite_rotation = get_net_rotation();
 
-	LatLonPoint llp = make_lat_lon_point(PointOnSphere(params.axis));
+	const GPlatesMaths::UnitQuaternion3D &uq = finite_rotation.unit_quat();
+	if (represents_identity_rotation(uq))
+	{
+		return boost::none;
+	}
 
-	Vector3D stage_pole_xyz = convert_net_rotation_pole_to_xyz(llp,angle);
+	// Convert finite rotation to a lat-lon pole and angle (in degrees).
+	const GPlatesMaths::UnitQuaternion3D::RotationParams params = uq.get_rotation_params(finite_rotation.axis_hint());
+	const GPlatesMaths::LatLonPoint llp = make_lat_lon_point(GPlatesMaths::PointOnSphere(params.axis));
+	const double angle = convert_rad_to_deg(params.angle).dval();
 
-	Vector3D v = cross(stage_pole_xyz,point.position_vector());
+	return std::make_pair(llp, angle);
+}
 
-	Vector3D omega = cross(point.position_vector(),v);
+GPlatesMaths::FiniteRotation
+GPlatesAppLogic::NetRotationUtils::NetRotationAccumulator::convert_rotation_vector_to_finite_rotation(
+		const GPlatesMaths::Vector3D &rotation_vec)
+{
+	if (rotation_vec.is_zero_magnitude())
+	{
+		return GPlatesMaths::FiniteRotation::create_identity_rotation();
+	}
 
-	double cos_latitude = std::cos(std::asin(point.position_vector().z().dval()));
-	omega = omega*cos_latitude;
-	double x = point.position_vector().x().dval();
-	double y = point.position_vector().y().dval();
-	double weighting_factor = (x*x + y*y)*cos_latitude;
+	const double rotation_angle = rotation_vec.magnitude().dval();
 
-#if 0
-	double test = std::pow((x*x + y*y),1.5);
-	qDebug() << "x,y,cos_lat: " << x << y << cos_latitude;
-	qDebug() << "weighting factor: " << weighting_factor;
-	qDebug() << "test: " << test;
-#endif
-	return NetRotationResult(omega,weighting_factor,cos_latitude,0.);
+	const GPlatesMaths::PointOnSphere rotation_pole(
+			GPlatesMaths::UnitVector3D((1.0 / rotation_angle) * rotation_vec));
+
+	return GPlatesMaths::FiniteRotation::create(rotation_pole, rotation_angle);
+}
+
+GPlatesMaths::Vector3D
+GPlatesAppLogic::NetRotationUtils::NetRotationAccumulator::convert_finite_rotation_to_rotation_vector(
+		const GPlatesMaths::FiniteRotation &finite_rotation)
+{
+	const GPlatesMaths::UnitQuaternion3D &uq = finite_rotation.unit_quat();
+
+	const GPlatesMaths::UnitQuaternion3D::RotationParams params = uq.get_rotation_params(finite_rotation.axis_hint());
+
+	return params.angle * GPlatesMaths::Vector3D(params.axis);
+}
+
+
+GPlatesAppLogic::NetRotationUtils::NetRotationCalculator::NetRotationCalculator(
+		const resolved_topological_boundary_seq_type &resolved_topological_boundaries,
+		const resolved_topological_network_seq_type &resolved_topological_networks,
+		const double &time,
+		const double &velocity_delta_time,
+		VelocityDeltaTime::Type velocity_delta_time_type,
+		GPlatesModel::integer_plate_id_type anchor_plate_id,
+		unsigned int num_samples_along_meridian) :
+	d_resolved_topological_boundaries(resolved_topological_boundaries),
+	d_resolved_topological_networks(resolved_topological_networks),
+	d_time(time),
+	d_velocity_delta_time(velocity_delta_time),
+	d_velocity_delta_time_type(velocity_delta_time_type),
+	d_velocity_time_period(VelocityDeltaTime::get_time_range(velocity_delta_time_type, time, velocity_delta_time)),
+	d_anchor_plate_id(anchor_plate_id)
+{
+	GPlatesGlobal::Assert<GPlatesGlobal::PreconditionViolationError>(
+			num_samples_along_meridian > 0,
+			GPLATES_ASSERTION_SOURCE);
+	const unsigned int num_samples_along_parallel = 2 * num_samples_along_meridian;
+
+	const double delta_in_degrees = 180.0 / num_samples_along_meridian;
+	const double delta_in_radians = GPlatesMaths::convert_deg_to_rad(delta_in_degrees);
+
+	// Loop over lat-lon grid and calculate the rotation contribution at each point.
+	for (unsigned int latitude_index = 0; latitude_index <= num_samples_along_meridian; ++latitude_index)
+	{
+		const double latitude = (latitude_index == num_samples_along_meridian)
+				? 90.0
+				: -90.0 + latitude_index * delta_in_degrees;
+
+		for (unsigned int longitude_index = 0; longitude_index <= num_samples_along_parallel; ++longitude_index)
+		{
+			const double longitude = (longitude_index == num_samples_along_parallel)
+					? 180.0
+					: -180.0 + longitude_index * delta_in_degrees;
+
+			const GPlatesMaths::PointOnSphere position = GPlatesMaths::make_point_on_sphere(GPlatesMaths::LatLonPoint(latitude, longitude));
+
+			// Add net rotation contribution from a deforming network first, otherwise from a rigid plate.
+			if (!add_net_rotation_contribution_from_resolved_networks(position, delta_in_radians))
+			{
+				add_net_rotation_contribution_from_resolved_boundaries(position, delta_in_radians);
+			}
+		}
+	}
+}
+
+bool
+GPlatesAppLogic::NetRotationUtils::NetRotationCalculator::add_net_rotation_contribution_from_resolved_networks(
+		const GPlatesMaths::PointOnSphere &position,
+		const double &sample_square_length_in_radians)
+{
+	// Check which deforming network (if any) the position lies in.
+	for (auto network_ptr : d_resolved_topological_networks)
+	{
+		// See if point is in network boundary and if so, return the stage rotation.
+		boost::optional< std::pair<
+				GPlatesMaths::FiniteRotation,
+				ResolvedTriangulation::Network::PointLocation> > point_stage_rotation =
+						network_ptr->get_triangulation_network().calculate_stage_rotation(
+								position,
+								d_velocity_delta_time,
+								d_velocity_delta_time_type);
+		if (point_stage_rotation)
+		{
+			const NetRotationAccumulator net_rotation_contribution =
+					NetRotationAccumulator::create(
+							position,
+							point_stage_rotation->first,
+							d_velocity_delta_time,
+							sample_square_length_in_radians);
+
+			add_net_rotation_contribution(network_ptr, net_rotation_contribution);
+
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool
+GPlatesAppLogic::NetRotationUtils::NetRotationCalculator::add_net_rotation_contribution_from_resolved_boundaries(
+		const GPlatesMaths::PointOnSphere &position,
+		const double &sample_square_length_in_radians)
+{
+	// Check which rigid (non-deforming) plate (if any) the position lies in.
+	for (auto boundary_ptr : d_resolved_topological_boundaries)
+	{
+		const GPlatesMaths::PolygonOnSphere::non_null_ptr_to_const_type boundary = boundary_ptr->resolved_topology_boundary();
+
+		// Get the stage rotation from the plate ID.
+		// If resolved boundary has no plate ID then the position does not contribute net rotation.
+		const boost::optional<GPlatesMaths::FiniteRotation> boundary_stage_pole = get_resolved_boundary_stage_pole(boundary_ptr);
+		if (boundary_stage_pole)
+		{
+			if (boundary->is_point_in_polygon(position,GPlatesMaths::PolygonOnSphere::HIGH_SPEED_HIGH_SETUP_HIGH_MEMORY_USAGE))
+			{
+				const NetRotationAccumulator net_rotation_contribution =
+						NetRotationAccumulator::create(
+							position,
+							boundary_stage_pole.get(),
+							d_velocity_delta_time,
+							sample_square_length_in_radians);
+
+				add_net_rotation_contribution(boundary_ptr, net_rotation_contribution);
+
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+void
+GPlatesAppLogic::NetRotationUtils::NetRotationCalculator::add_net_rotation_contribution(
+		ResolvedTopologicalNetwork::non_null_ptr_to_const_type resolved_topological_network,
+		const NetRotationAccumulator &net_rotation_contribution)
+{
+	d_topological_network_net_rotation_map[resolved_topological_network].add(net_rotation_contribution);
+	d_plate_id_net_rotation_map[resolved_topological_network->plate_id()].add(net_rotation_contribution);
+	d_total_net_rotation.add(net_rotation_contribution);
+}
+
+void
+GPlatesAppLogic::NetRotationUtils::NetRotationCalculator::add_net_rotation_contribution(
+		ResolvedTopologicalBoundary::non_null_ptr_to_const_type resolved_topological_boundary,
+		const NetRotationAccumulator &net_rotation_contribution)
+{
+	d_topological_boundary_net_rotation_map[resolved_topological_boundary].add(net_rotation_contribution);
+	d_plate_id_net_rotation_map[resolved_topological_boundary->plate_id()].add(net_rotation_contribution);
+	d_total_net_rotation.add(net_rotation_contribution);
+}
+
+boost::optional<GPlatesMaths::FiniteRotation>
+GPlatesAppLogic::NetRotationUtils::NetRotationCalculator::get_resolved_boundary_stage_pole(
+		ResolvedTopologicalBoundary::non_null_ptr_to_const_type resolved_topological_boundary) const
+{
+	const boost::optional<GPlatesModel::integer_plate_id_type> boundary_plate_id = resolved_topological_boundary->plate_id();
+	if (!boundary_plate_id)
+	{
+		return boost::none;
+	}
+
+	// See if plate ID is already in the map. If not then calculate and insert into map.
+	stage_pole_map_type::const_iterator it = d_resolved_boundary_stage_pole_map.find(boundary_plate_id.get());
+	if (it != d_resolved_boundary_stage_pole_map.end())
+	{
+		return it->second;
+	}
+
+	// Get the stage pole for the plate ID.
+	const ReconstructionTree::non_null_ptr_to_const_type tree_older =
+			resolved_topological_boundary->get_reconstruction_tree_creator().get_reconstruction_tree(
+					d_velocity_time_period.first/*older*/);
+
+	const ReconstructionTree::non_null_ptr_to_const_type tree_younger =
+			resolved_topological_boundary->get_reconstruction_tree_creator().get_reconstruction_tree(
+					d_velocity_time_period.second/*younger*/);
+
+	const GPlatesMaths::FiniteRotation stage_pole = RotationUtils::get_stage_pole(
+			*tree_older, *tree_younger,
+			boundary_plate_id.get(), d_anchor_plate_id);
+
+	// Insert the stage pole into the plate ID map.
+	auto insert_result = d_resolved_boundary_stage_pole_map.insert(stage_pole_map_type::value_type(boundary_plate_id.get(), stage_pole));
+
+	return insert_result.first->second;
 }

commit c7fc6ec1974c4399683710e7a8d71771cf71e68e

Fix a couple of bugs introduced in 844dab5.
- Need to convert angle to angle/Myr.
- Store area-weighted rotation component.

src/app-logic/NetRotationUtils.cc
@@ -50,7 +50,7 @@ GPlatesAppLogic::NetRotationUtils::NetRotationAccumulator::create(
 		return NetRotationAccumulator();
 	}
 
-	const GPlatesMaths::Vector3D stage_pole_vector = convert_finite_rotation_to_rotation_vector(stage_pole);
+	const GPlatesMaths::Vector3D stage_pole_vector = convert_finite_rotation_to_rotation_vector(stage_pole, time_interval);
 
 	const GPlatesMaths::Vector3D v = cross(stage_pole_vector, point.position_vector());
 
@@ -71,7 +71,7 @@ GPlatesAppLogic::NetRotationUtils::NetRotationAccumulator::create(
 	// Rotation component is weight by the sample area.
 	const double weighting_factor = cos_latitude_squared * area_steradians;
 
-	return NetRotationAccumulator(omega, weighting_factor, area_steradians);
+	return NetRotationAccumulator(rotation_component, weighting_factor, area_steradians);
 }
 
 void
@@ -136,13 +136,15 @@ GPlatesAppLogic::NetRotationUtils::NetRotationAccumulator::convert_rotation_vect
 
 GPlatesMaths::Vector3D
 GPlatesAppLogic::NetRotationUtils::NetRotationAccumulator::convert_finite_rotation_to_rotation_vector(
-		const GPlatesMaths::FiniteRotation &finite_rotation)
+		const GPlatesMaths::FiniteRotation &finite_rotation,
+		const double &time_interval)
 {
 	const GPlatesMaths::UnitQuaternion3D &uq = finite_rotation.unit_quat();
 
 	const GPlatesMaths::UnitQuaternion3D::RotationParams params = uq.get_rotation_params(finite_rotation.axis_hint());
 
-	return params.angle * GPlatesMaths::Vector3D(params.axis);
+	// Convert angle from radians to radians/Myr, and scale the axis with it.
+	return (params.angle / time_interval) * GPlatesMaths::Vector3D(params.axis);
 }
 
 

src/app-logic/NetRotationUtils.h
@@ -125,7 +125,8 @@ namespace GPlatesAppLogic
 			static
 			GPlatesMaths::Vector3D
 			convert_finite_rotation_to_rotation_vector(
-					const GPlatesMaths::FiniteRotation &finite_rotation);
+					const GPlatesMaths::FiniteRotation &finite_rotation,
+					const double &time_interval);
 
 
 			NetRotationAccumulator(