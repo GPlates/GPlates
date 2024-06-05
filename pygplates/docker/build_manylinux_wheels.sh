82b78d3cc70cc888b609c89ffa9e37e353f54eec
John Cannon <john.cannon@sydney.edu.au>
2024-06-05 17:59:23 +1000

    Link to libGL instead of libOpenGL in manylinux wheel.
    We do this by setting the CMake variable OpenGL_GL_PREFERENCE to LEGACY
    (instead of the default GLVND). Then we don't have to exclude any
    libraries from auditwheel because libGL is whitelisted.

--- pygplates/docker/build_manylinux_wheels.sh
+++ pygplates/docker/build_manylinux_wheels.sh
@@ -7,36 +7,57 @@ cd /io
 cp -r cmake doc-cpp doc-python-api src CMakeLists.txt COPYING pyproject.toml README.md /pygplates
 mkdir /pygplates/pygplates
 cp -r pygplates/CMakeLists.txt pygplates/test /pygplates/pygplates
+cd /pygplates
 
 # Build wheels (in the local file system) for each Python version.
-cd /pygplates
 for cp_version in 38 39 310 311 312
 do
-    /opt/python/cp${cp_version}-cp${cp_version}/bin/python -m pip wheel --wheel-dir dist -v .
+    # The Python executable depends on the Python version.
+    python_exe=/opt/python/cp${cp_version}-cp${cp_version}/bin/python
+
+    #
+    # Build a wheel for the current Python version (and store the wheel in the 'dist/' sub-directory).
+    #
+    # Note: We set the CMake variable OpenGL_GL_PREFERENCE to LEGACY (instead of the default GLVND).
+    #       This causes pyGPlates to prefer to use the 'libGL' LEGACY dependency (instead of the default
+    #       'libOpenGL' GLVND dependency). The 'libGL' library is whitelisted by auditwheel (meaning it will
+    #       not be copied into the wheel repaired by auditwheel). This is presumably because it is available
+    #       by default on all Linux distributions. Whereas 'libOpenGL' is NOT whitelisted (presumably because
+    #       it is NOT available by default on all Linux distros) and hence would need to be copied into the wheel
+    #       (if it was used). However copying into the wheel is problematic if 'libOpenGL' itself needs to come
+    #       from the end machine (eg, if it's NOT hardware-independent - see https://github.com/pypa/auditwheel/issues/241).
+    #       Alternatively, if 'libOpenGL' actually is hardware-independent and we copy it into the wheel then the
+    #       end machine might still need to have the 'libglvnd' package installed (which is not the case for all
+    #       Linux distros by default - see https://github.com/linuxdeploy/linuxdeploy/issues/152#issuecomment-830975582 ).
+    #       So we prefer to link to 'libGL' instead (which should be available by default on all Linux distros).
+    #
+    # Note: Besides pyGPlates, Qt is the other library that uses 'libGL'. And they made an effort to not
+    #       use 'libOpenGL' for the same reasons (ie, it's not installed by default on all Linux distros).
+    #       See https://bugreports.qt.io/browse/QTBUG-89754.
+    #
+    # Note: Previously we linked to 'libOpenGL' (because we didn't set OpenGL_GL_PREFERENCE to LEGACY) and
+    #       so it was copied into the wheel (because it's not whitelisted by auditwheel). It, in turn, links to
+    #       'libGLdispatch' and so that was also copied into the wheel. That caused a segmentation fault during
+    #       'import pygplates' because there were two copies of 'libGLdispatch' being referenced. One was copied
+    #       into the wheel (due to being a dependency of 'libOpenGL' that was referenced by pyGPlates). The other
+    #       was referenced by 'libGL' (via Qt) and hence was not copied into the wheel (since 'libGL' is whitelisted).
+    #       The segmentation fault was most likely because, according to https://github.com/NVIDIA/libglvnd:
+    #         "since all OpenGL functions are dispatched through the same table in libGLdispatch,"
+    #         "it doesn't matter which library is used to find the entrypoint".
+    #       ...where by "it doesn't matter which library is used to find the entrypoint" they mean 'libOpenGL' and
+    #       'libGL' (not 'libGLdispatch'). So having Qt reference '/usr/lib64/libGLdispatch.so.0' (via 'libGL') and
+    #       pyGPlates reference the 'libGLdispatch' copied into the wheel (via 'libOpenGL') would result in *two*
+    #       dispatch tables (instead of one central table). And this is likely what caused the segmentation fault.
+    #
+    $python_exe -m pip wheel \
+        --wheel-dir dist \
+        -v \
+        --config-settings cmake.define.OpenGL_GL_PREFERENCE=LEGACY \
+        .
 done
 
 # Repair the built wheels so that they're manylinux compatible.
-#
-# Note: We need to exclude specific OpenGL-related libraries from being copied into the repaired wheel.
-#       We exclude 'libOpenGL' since it needs to come from the end machine, not the wheel
-#       (see https://github.com/pypa/auditwheel/issues/241). Similarly for 'libGLX' I assume.
-#       If we don't exclude 'libGLdispatch' then we get a segmentation fault during 'import pygplates'.
-#       This is most likely because, according to https://github.com/NVIDIA/libglvnd:
-#         "Note that since all OpenGL functions are dispatched through the same table in libGLdispatch,"
-#         "it doesn't matter which library is used to find the entrypoint".
-#       ...so having some libraries reference '/usr/lib64/libGLdispatch.so.0' and others reference the
-#       'libGLdispatch' copied into the wheel would result in two dispatch tables which could cause problems.
-#       Also note that it appears 'libGL' is automatically whitelisted by auditwheel (not copied into the wheel),
-#       so we don't need to exclude it here.
-# Note: One issue with excluding the above-mentioned OpenGL-related libraries is if they're not installed by default
-#       on a particular Linux distribution. However these libraries should get installed with the 'libglvnd' package,
-#       so as long as that's installed then it should run fine on the end machine.
-#
-auditwheel repair \
-    --exclude libGLdispatch.so.0 \
-    --exclude libGLX.so.0 \
-    --exclude libOpenGL.so.0 \
-    dist/pygplates*.whl
+auditwheel repair dist/pygplates*.whl
 
 # Copy the manylinux wheels to the host file system.
 if [ ! -d /io/wheelhouse ]
