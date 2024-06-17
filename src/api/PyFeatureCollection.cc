f35d3a8a3549bd6fc09772a444a8fa529b9cf945
John Cannon <john.cannon@sydney.edu.au>
2024-06-18 01:03:14 +1000

    Fix pygplates.FeatureCollection.get() incorrect named argument.
    Fixes #2

--- src/api/PyFeatureCollection.cc
+++ src/api/PyFeatureCollection.cc
@@ -1027,7 +1027,7 @@ export_feature_collection()
 		.def("get",
 				&GPlatesApi::feature_collection_handle_get_feature,
 				(bp::arg("feature_query"),
-						bp::arg("feature_query") = GPlatesApi::FeatureReturn::EXACTLY_ONE),
+						bp::arg("feature_return") = GPlatesApi::FeatureReturn::EXACTLY_ONE),
 				"get(feature_query, [feature_return=FeatureReturn.exactly_one])\n"
 				"  Returns one or more features matching a feature type, feature id or predicate.\n"
 				"\n"
