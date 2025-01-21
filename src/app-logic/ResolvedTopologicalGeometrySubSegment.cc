GPlates/GPlates

commit a0450ca7c24976baf3e2c64f5f8eb5e2403a1ed5

Find source feature for each vertex in resolved topology or sub-segment.
Similar to finding resolved vertex source infos but returns a feature
instead of calculating velocity.

src/app-logic/ResolvedTopologicalGeometrySubSegment.cc
@@ -212,6 +212,99 @@ GPlatesAppLogic::ResolvedTopologicalGeometrySubSegment::get_reversed_sub_segment
 }
 
 
+void
+GPlatesAppLogic::ResolvedTopologicalGeometrySubSegment::get_sub_segment_point_source_features(
+		std::vector<GPlatesModel::FeatureHandle::weak_ref> &point_source_features,
+		bool include_rubber_band_points) const
+{
+	if (!d_point_source_features)
+	{
+		d_point_source_features = std::vector<GPlatesModel::FeatureHandle::weak_ref>();
+
+		// Get the point source features (including at the optional rubber band points).
+		ResolvedTopologicalSubSegmentImpl::get_sub_segment_vertex_source_features(
+				d_point_source_features.get(),
+				d_sub_segment,
+				d_segment_reconstruction_geometry,
+				true/*include_rubber_band_points*/);
+	}
+
+	// Copy to caller's sequence.
+	//
+	// If the caller does not want rubber band points then avoid copying them (if they exist).
+	std::vector<GPlatesModel::FeatureHandle::weak_ref>::const_iterator src_point_source_infos_begin = d_point_source_features->begin();
+	std::vector<GPlatesModel::FeatureHandle::weak_ref>::const_iterator src_point_source_infos_end = d_point_source_features->end();
+	if (!include_rubber_band_points)
+	{
+		if (d_sub_segment.get_start_rubber_band())
+		{
+			++src_point_source_infos_begin;
+		}
+		if (d_sub_segment.get_end_rubber_band())
+		{
+			--src_point_source_infos_end;
+		}
+	}
+
+	std::copy(
+			src_point_source_infos_begin,
+			src_point_source_infos_end,
+			std::back_inserter(point_source_features));
+}
+
+
+void
+GPlatesAppLogic::ResolvedTopologicalGeometrySubSegment::get_reversed_sub_segment_point_source_features(
+		std::vector<GPlatesModel::FeatureHandle::weak_ref> &point_source_features,
+		bool use_reverse,
+		bool include_rubber_band_points) const
+{
+	if (!d_point_source_features)
+	{
+		d_point_source_features = std::vector<GPlatesModel::FeatureHandle::weak_ref>();
+
+		// Get the point source features (including at the optional rubber band points).
+		ResolvedTopologicalSubSegmentImpl::get_sub_segment_vertex_source_features(
+				d_point_source_features.get(),
+				d_sub_segment,
+				d_segment_reconstruction_geometry,
+				true/*include_rubber_band_points*/);
+	}
+
+	// Copy to caller's sequence.
+	//
+	// If the caller does not want rubber band points then avoid copying them (if they exist).
+	std::vector<GPlatesModel::FeatureHandle::weak_ref>::const_iterator src_point_source_infos_begin = d_point_source_features->begin();
+	std::vector<GPlatesModel::FeatureHandle::weak_ref>::const_iterator src_point_source_infos_end = d_point_source_features->end();
+	if (!include_rubber_band_points)
+	{
+		if (d_sub_segment.get_start_rubber_band())
+		{
+			++src_point_source_infos_begin;
+		}
+		if (d_sub_segment.get_end_rubber_band())
+		{
+			--src_point_source_infos_end;
+		}
+	}
+
+	if (use_reverse)
+	{
+		std::reverse_copy(
+				src_point_source_infos_begin,
+				src_point_source_infos_end,
+				std::back_inserter(point_source_features));
+	}
+	else
+	{
+		std::copy(
+				src_point_source_infos_begin,
+				src_point_source_infos_end,
+				std::back_inserter(point_source_features));
+	}
+}
+
+
 const boost::optional<GPlatesAppLogic::sub_segment_seq_type> &
 GPlatesAppLogic::ResolvedTopologicalGeometrySubSegment::get_sub_sub_segments() const
 {

src/app-logic/ResolvedTopologicalGeometrySubSegment.h
@@ -212,6 +212,34 @@ namespace GPlatesAppLogic
 					earth_radius_in_kms);
 		}
 
+		/**
+		 * Returns the per-point source infos at the points returned by @a get_sub_segment_geometry_points.
+		 *
+		 * Note: Each source info maps to a point in @a get_sub_segment_geometry_points.
+		 *
+		 * Note: The number of source infos is guaranteed to match points in @a get_sub_segment_geometry_points.
+		 */
+		void
+		get_sub_segment_geometry_point_source_infos(
+				resolved_vertex_source_info_seq_type &point_source_infos) const
+		{
+			get_sub_segment_point_source_infos(point_source_infos, true/*include_rubber_band_points*/);
+		}
+
+		/**
+		 * Returns the per-point source features at the points returned by @a get_sub_segment_geometry_points.
+		 *
+		 * Note: Each source feature maps to a point in @a get_sub_segment_geometry_points.
+		 *
+		 * Note: The number of source features is guaranteed to match points in @a get_sub_segment_geometry_points.
+		 */
+		void
+		get_sub_segment_geometry_point_source_features(
+				std::vector<GPlatesModel::FeatureHandle::weak_ref> &point_source_features) const
+		{
+			get_sub_segment_point_source_features(point_source_features, true/*include_rubber_band_points*/);
+		}
+
 
 		/**
 		 * Return the number of points in the sub-segment geometry.
@@ -323,6 +351,39 @@ namespace GPlatesAppLogic
 
 
 		/**
+		 * Returns the (unreversed) per-point source features.
+		 *
+		 * Each point in @a get_sub_segment_points references a source feature.
+		 * This method returns the same number of point source features as points returned by @a get_sub_segment_points.
+		 *
+		 * Does not clear @a point_source_features - just appends point source features.
+		 *
+		 * @throws PreconditionViolationError if the section reconstruction geometry passed into @a create
+		 * is neither a @a ReconstructedFeatureGeometry nor a @a ResolvedTopologicalLine.
+		 */
+		void
+		get_sub_segment_point_source_features(
+				std::vector<GPlatesModel::FeatureHandle::weak_ref> &point_source_features,
+				bool include_rubber_band_points = true) const;
+
+		/**
+		 * Same as @a get_sub_segment_point_source_features but reverses them if necessary such that
+		 * they are in the same order as @a get_reversed_sub_segment_points.
+		 *
+		 * The @a use_reverse flag should be associated with the desired sharing resolved topology.
+		 * For example, it can be obtained from the relevant @a ResolvedTopologyInfo.
+		 *
+		 * These are @a get_sub_segment_point_source_features if @a use_reverse is false,
+		 * otherwise they are a reversed version of @a get_sub_segment_point_source_features.
+		 */
+		void
+		get_reversed_sub_segment_point_source_features(
+				std::vector<GPlatesModel::FeatureHandle::weak_ref> &point_source_features,
+				bool use_reverse,
+				bool include_rubber_band_points = true) const;
+
+
+		/**
 		 * Return any sub-segments of the resolved topological section that this sub-segment came from.
 		 *
 		 * If topological section is a ResolvedTopologicalLine then returns sub-segments, otherwise returns none.
@@ -374,6 +435,13 @@ namespace GPlatesAppLogic
 		 * As an optimisation, this is only created when first requested.
 		 */
 		mutable boost::optional<resolved_vertex_source_info_seq_type> d_point_source_infos;
+
+		/**
+		 * Each point in the subsegment geometry can potentially reference a different source feature.
+		 *
+		 * As an optimisation, this is only created when first requested.
+		 */
+		mutable boost::optional<std::vector<GPlatesModel::FeatureHandle::weak_ref>> d_point_source_features;
 
 		/**
 		 * Sub-segments of our ResolvedTopologicalLine topological section (if one) than contribute to this sub-segment.

src/app-logic/ResolvedTopologicalSharedSubSegment.cc
@@ -215,6 +215,99 @@ GPlatesAppLogic::ResolvedTopologicalSharedSubSegment::get_reversed_shared_sub_se
 }
 
 
+void
+GPlatesAppLogic::ResolvedTopologicalSharedSubSegment::get_shared_sub_segment_point_source_features(
+		std::vector<GPlatesModel::FeatureHandle::weak_ref> &point_source_features,
+		bool include_rubber_band_points) const
+{
+	if (!d_point_source_features)
+	{
+		d_point_source_features = std::vector<GPlatesModel::FeatureHandle::weak_ref>();
+
+		// Get the point source features (including at the optional rubber band points).
+		ResolvedTopologicalSubSegmentImpl::get_sub_segment_vertex_source_features(
+				d_point_source_features.get(),
+				d_shared_sub_segment,
+				d_shared_segment_reconstruction_geometry,
+				true/*include_rubber_band_points*/);
+	}
+
+	// Copy to caller's sequence.
+	//
+	// If the caller does not want rubber band points then avoid copying them (if they exist).
+	std::vector<GPlatesModel::FeatureHandle::weak_ref>::const_iterator src_point_source_infos_begin = d_point_source_features->begin();
+	std::vector<GPlatesModel::FeatureHandle::weak_ref>::const_iterator src_point_source_infos_end = d_point_source_features->end();
+	if (!include_rubber_band_points)
+	{
+		if (d_shared_sub_segment.get_start_rubber_band())
+		{
+			++src_point_source_infos_begin;
+		}
+		if (d_shared_sub_segment.get_end_rubber_band())
+		{
+			--src_point_source_infos_end;
+		}
+	}
+
+	std::copy(
+			src_point_source_infos_begin,
+			src_point_source_infos_end,
+			std::back_inserter(point_source_features));
+}
+
+
+void
+GPlatesAppLogic::ResolvedTopologicalSharedSubSegment::get_reversed_shared_sub_segment_point_source_features(
+		std::vector<GPlatesModel::FeatureHandle::weak_ref> &point_source_features,
+		bool use_reverse,
+		bool include_rubber_band_points) const
+{
+	if (!d_point_source_features)
+	{
+		d_point_source_features = std::vector<GPlatesModel::FeatureHandle::weak_ref>();
+
+		// Get the point source features (including at the optional rubber band points).
+		ResolvedTopologicalSubSegmentImpl::get_sub_segment_vertex_source_features(
+				d_point_source_features.get(),
+				d_shared_sub_segment,
+				d_shared_segment_reconstruction_geometry,
+				true/*include_rubber_band_points*/);
+	}
+
+	// Copy to caller's sequence.
+	//
+	// If the caller does not want rubber band points then avoid copying them (if they exist).
+	std::vector<GPlatesModel::FeatureHandle::weak_ref>::const_iterator src_point_source_infos_begin = d_point_source_features->begin();
+	std::vector<GPlatesModel::FeatureHandle::weak_ref>::const_iterator src_point_source_infos_end = d_point_source_features->end();
+	if (!include_rubber_band_points)
+	{
+		if (d_shared_sub_segment.get_start_rubber_band())
+		{
+			++src_point_source_infos_begin;
+		}
+		if (d_shared_sub_segment.get_end_rubber_band())
+		{
+			--src_point_source_infos_end;
+		}
+	}
+
+	if (use_reverse)
+	{
+		std::reverse_copy(
+				src_point_source_infos_begin,
+				src_point_source_infos_end,
+				std::back_inserter(point_source_features));
+	}
+	else
+	{
+		std::copy(
+				src_point_source_infos_begin,
+				src_point_source_infos_end,
+				std::back_inserter(point_source_features));
+	}
+}
+
+
 const boost::optional<GPlatesAppLogic::sub_segment_seq_type> &
 GPlatesAppLogic::ResolvedTopologicalSharedSubSegment::get_sub_sub_segments() const
 {

src/app-logic/ResolvedTopologicalBoundary.cc
@@ -57,7 +57,7 @@ GPlatesAppLogic::ResolvedTopologicalBoundary::resolved_topology_geometry_point_v
 	const resolved_topology_boundary_ptr_type resolved_topology_boundary_ = resolved_topology_boundary();
 
 	// Get the resolved source infos (one per point in the resolved boundary).
-	const resolved_vertex_source_info_seq_type &resolved_source_infos = get_vertex_source_infos();
+	const resolved_vertex_source_info_seq_type &resolved_source_infos = get_resolved_topology_geometry_point_source_infos();
 
 	// Number of resolved source infos should match number of points in the resolved boundary.
 	GPlatesGlobal::Assert<GPlatesGlobal::AssertionFailureException>(
@@ -86,7 +86,7 @@ GPlatesAppLogic::ResolvedTopologicalBoundary::resolved_topology_geometry_point_v
 
 
 const GPlatesAppLogic::resolved_vertex_source_info_seq_type &
-GPlatesAppLogic::ResolvedTopologicalBoundary::get_vertex_source_infos() const
+GPlatesAppLogic::ResolvedTopologicalBoundary::get_resolved_topology_geometry_point_source_infos() const
 {
 	// Cache all vertex source infos on first call.
 	if (!d_vertex_source_infos)
@@ -98,6 +98,19 @@ GPlatesAppLogic::ResolvedTopologicalBoundary::get_vertex_source_infos() const
 }
 
 
+const std::vector<GPlatesModel::FeatureHandle::weak_ref> &
+GPlatesAppLogic::ResolvedTopologicalBoundary::get_resolved_topology_geometry_point_source_features() const
+{
+	// Cache all vertex source features on first call.
+	if (!d_vertex_source_features)
+	{
+		calc_vertex_source_features();
+	}
+
+	return d_vertex_source_features.get();
+}
+
+
 void
 GPlatesAppLogic::ResolvedTopologicalBoundary::calc_vertex_source_infos() const
 {
@@ -105,14 +118,28 @@ GPlatesAppLogic::ResolvedTopologicalBoundary::calc_vertex_source_infos() const
 	resolved_vertex_source_info_seq_type &vertex_source_infos = d_vertex_source_infos.get();
 
 	// Copy source infos from points in each subsegment.
-	sub_segment_seq_type::const_iterator sub_segments_iter = d_sub_segment_seq.begin();
-	sub_segment_seq_type::const_iterator sub_segments_end = d_sub_segment_seq.end();
-	for ( ; sub_segments_iter != sub_segments_end; ++sub_segments_iter)
+	for (const auto &sub_segment : d_sub_segment_seq)
 	{
-		const ResolvedTopologicalGeometrySubSegment::non_null_ptr_type &sub_segment = *sub_segments_iter;
 		// Subsegment should be reversed if that's how it contributed to this resolved topological boundary...
 		sub_segment->get_reversed_sub_segment_point_source_infos(
 				vertex_source_infos,
+				INCLUDE_SUB_SEGMENT_RUBBER_BAND_POINTS_IN_RESOLVED_BOUNDARY/*include_rubber_band_points*/);
+	}
+}
+
+
+void
+GPlatesAppLogic::ResolvedTopologicalBoundary::calc_vertex_source_features() const
+{
+	d_vertex_source_features = std::vector<GPlatesModel::FeatureHandle::weak_ref>();
+	std::vector<GPlatesModel::FeatureHandle::weak_ref> &vertex_source_features = d_vertex_source_features.get();
+
+	// Copy source features from points in each subsegment.
+	for (const auto &sub_segment : d_sub_segment_seq)
+	{
+		// Subsegment should be reversed if that's how it contributed to this resolved topological boundary...
+		sub_segment->get_reversed_sub_segment_point_source_features(
+				vertex_source_features,
 				INCLUDE_SUB_SEGMENT_RUBBER_BAND_POINTS_IN_RESOLVED_BOUNDARY/*include_rubber_band_points*/);
 	}
 }

src/app-logic/ResolvedTopologicalBoundary.h
@@ -42,6 +42,7 @@
 #include "maths/PolygonOnSphere.h"
 #include "maths/Vector3D.h"
 
+#include "model/FeatureHandle.h"
 #include "model/WeakObserverVisitor.h"
 
 #include "utils/Earth.h"
@@ -136,6 +137,15 @@ namespace GPlatesAppLogic
 		}
 
 		/**
+		 * Returns the resolved topology polygon as a @a PolygonOnSphere.
+		 */
+		resolved_topology_boundary_ptr_type
+		resolved_topology_boundary() const
+		{
+			return d_resolved_topology_boundary_ptr;
+		}
+
+		/**
 		 * Access the resolved topology polygon as a @a GeometryOnSphere.
 		 */
 		virtual
@@ -169,24 +179,27 @@ namespace GPlatesAppLogic
 				VelocityUnits::Value velocity_units = VelocityUnits::CMS_PER_YR,
 				const double &earth_radius_in_kms = GPlatesUtils::Earth::EQUATORIAL_RADIUS_KMS) const;
 
-		/**
-		 * Returns the resolved topology polygon as a @a PolygonOnSphere.
-		 */
-		resolved_topology_boundary_ptr_type
-		resolved_topology_boundary() const
-		{
-			return d_resolved_topology_boundary_ptr;
-		}
-
 
 		/**
-		 * Returns the per-vertex source reconstructed feature geometries.
+		 * Returns the source infos at points in @a resolved_topology_geometry_points.
 		 *
-		 * Each vertex returned by @a resolved_topology_boundary references a source reconstructed feature geometry.
-		 * This method returns the same number of vertex sources as vertices returned by @a resolved_topology_boundary.
+		 * Note: Each source info maps to a point in @a resolved_topology_geometry_points.
+		 *
+		 * Note: The number of source infos is guaranteed to match points in @a resolved_topology_geometry_points.
 		 */
 		const resolved_vertex_source_info_seq_type &
-		get_vertex_source_infos() const;
+		get_resolved_topology_geometry_point_source_infos() const;
+
+
+		/**
+		 * Returns the source features at points in @a resolved_topology_geometry_points.
+		 *
+		 * Note: Each source feature maps to a point in @a resolved_topology_geometry_points.
+		 *
+		 * Note: The number of source features is guaranteed to match points in @a resolved_topology_geometry_points.
+		 */
+		const std::vector<GPlatesModel::FeatureHandle::weak_ref> &
+		get_resolved_topology_geometry_point_source_features() const;
 
 
 		/**
@@ -262,12 +275,18 @@ namespace GPlatesAppLogic
 
 
 		/**
-		 * Each point in the resolved topological line can potentially reference a different
-		 * source reconstructed feature geometry.
+		 * Each point in the resolved topological line can potentially reference a different source info.
 		 *
 		 * As an optimisation, this is only created when first requested.
 		 */
 		mutable boost::optional<resolved_vertex_source_info_seq_type> d_vertex_source_infos;
+
+		/**
+		 * Each point in the resolved topological line can potentially reference a different source feature.
+		 *
+		 * As an optimisation, this is only created when first requested.
+		 */
+		mutable boost::optional<std::vector<GPlatesModel::FeatureHandle::weak_ref>> d_vertex_source_features;
 
 
 		/**
@@ -303,6 +322,9 @@ namespace GPlatesAppLogic
 
 		void
 		calc_vertex_source_infos() const;
+
+		void
+		calc_vertex_source_features() const;
 	};
 }
 

src/app-logic/ResolvedTopologicalLine.cc
@@ -54,7 +54,7 @@ GPlatesAppLogic::ResolvedTopologicalLine::resolved_topology_geometry_point_veloc
 	const resolved_topology_line_ptr_type resolved_topology_line_ = resolved_topology_line();
 
 	// Get the resolved source infos (one per point in the resolved line).
-	const resolved_vertex_source_info_seq_type &resolved_source_infos = get_vertex_source_infos();
+	const resolved_vertex_source_info_seq_type &resolved_source_infos = get_resolved_topology_geometry_point_source_infos();
 
 	// Number of resolved source infos should match number of points in the resolved line.
 	GPlatesGlobal::Assert<GPlatesGlobal::AssertionFailureException>(
@@ -83,7 +83,7 @@ GPlatesAppLogic::ResolvedTopologicalLine::resolved_topology_geometry_point_veloc
 
 
 const GPlatesAppLogic::resolved_vertex_source_info_seq_type &
-GPlatesAppLogic::ResolvedTopologicalLine::get_vertex_source_infos() const
+GPlatesAppLogic::ResolvedTopologicalLine::get_resolved_topology_geometry_point_source_infos() const
 {
 	// Cache all vertex source infos on first call.
 	if (!d_vertex_source_infos)
@@ -95,6 +95,19 @@ GPlatesAppLogic::ResolvedTopologicalLine::get_vertex_source_infos() const
 }
 
 
+const std::vector<GPlatesModel::FeatureHandle::weak_ref> &
+GPlatesAppLogic::ResolvedTopologicalLine::get_resolved_topology_geometry_point_source_features() const
+{
+	// Cache all vertex source features on first call.
+	if (!d_vertex_source_features)
+	{
+		calc_vertex_source_features();
+	}
+
+	return d_vertex_source_features.get();
+}
+
+
 void
 GPlatesAppLogic::ResolvedTopologicalLine::calc_vertex_source_infos() const
 {
@@ -102,14 +115,28 @@ GPlatesAppLogic::ResolvedTopologicalLine::calc_vertex_source_infos() const
 	resolved_vertex_source_info_seq_type &vertex_source_infos = d_vertex_source_infos.get();
 
 	// Copy source infos from points in each subsegment.
-	sub_segment_seq_type::const_iterator sub_segments_iter = d_sub_segment_seq.begin();
-	sub_segment_seq_type::const_iterator sub_segments_end = d_sub_segment_seq.end();
-	for ( ; sub_segments_iter != sub_segments_end; ++sub_segments_iter)
+	for (const auto &sub_segment : d_sub_segment_seq)
 	{
-		const ResolvedTopologicalGeometrySubSegment::non_null_ptr_type &sub_segment = *sub_segments_iter;
 		// Subsegment should be reversed if that's how it contributed to this resolved topological line...
 		sub_segment->get_reversed_sub_segment_point_source_infos(
 				vertex_source_infos,
+				INCLUDE_SUB_SEGMENT_RUBBER_BAND_POINTS_IN_RESOLVED_LINE/*include_rubber_band_points*/);
+	}
+}
+
+
+void
+GPlatesAppLogic::ResolvedTopologicalLine::calc_vertex_source_features() const
+{
+	d_vertex_source_features = std::vector<GPlatesModel::FeatureHandle::weak_ref>();
+	std::vector<GPlatesModel::FeatureHandle::weak_ref> &vertex_source_features = d_vertex_source_features.get();
+
+	// Copy source features from points in each subsegment.
+	for (const auto &sub_segment : d_sub_segment_seq)
+	{
+		// Subsegment should be reversed if that's how it contributed to this resolved topological line...
+		sub_segment->get_reversed_sub_segment_point_source_features(
+				vertex_source_features,
 				INCLUDE_SUB_SEGMENT_RUBBER_BAND_POINTS_IN_RESOLVED_LINE/*include_rubber_band_points*/);
 	}
 }

commit 6a776146a9facd70e155aae28382dcdfc595b162

Fix bug with 'use_reverse' recently introduced in a0450ca.

src/app-logic/ResolvedTopologicalGeometrySubSegment.cc
@@ -232,23 +232,23 @@ GPlatesAppLogic::ResolvedTopologicalGeometrySubSegment::get_sub_segment_point_so
 	// Copy to caller's sequence.
 	//
 	// If the caller does not want rubber band points then avoid copying them (if they exist).
-	std::vector<GPlatesModel::FeatureHandle::weak_ref>::const_iterator src_point_source_infos_begin = d_point_source_features->begin();
-	std::vector<GPlatesModel::FeatureHandle::weak_ref>::const_iterator src_point_source_infos_end = d_point_source_features->end();
+	std::vector<GPlatesModel::FeatureHandle::weak_ref>::const_iterator src_point_source_features_begin = d_point_source_features->begin();
+	std::vector<GPlatesModel::FeatureHandle::weak_ref>::const_iterator src_point_source_features_end = d_point_source_features->end();
 	if (!include_rubber_band_points)
 	{
 		if (d_sub_segment.get_start_rubber_band())
 		{
-			++src_point_source_infos_begin;
+			++src_point_source_features_begin;
 		}
 		if (d_sub_segment.get_end_rubber_band())
 		{
-			--src_point_source_infos_end;
+			--src_point_source_features_end;
 		}
 	}
 
 	std::copy(
-			src_point_source_infos_begin,
-			src_point_source_infos_end,
+			src_point_source_features_begin,
+			src_point_source_features_end,
 			std::back_inserter(point_source_features));
 }
 
@@ -256,7 +256,6 @@ GPlatesAppLogic::ResolvedTopologicalGeometrySubSegment::get_sub_segment_point_so
 void
 GPlatesAppLogic::ResolvedTopologicalGeometrySubSegment::get_reversed_sub_segment_point_source_features(
 		std::vector<GPlatesModel::FeatureHandle::weak_ref> &point_source_features,
-		bool use_reverse,
 		bool include_rubber_band_points) const
 {
 	if (!d_point_source_features)
@@ -274,32 +273,32 @@ GPlatesAppLogic::ResolvedTopologicalGeometrySubSegment::get_reversed_sub_segment
 	// Copy to caller's sequence.
 	//
 	// If the caller does not want rubber band points then avoid copying them (if they exist).
-	std::vector<GPlatesModel::FeatureHandle::weak_ref>::const_iterator src_point_source_infos_begin = d_point_source_features->begin();
-	std::vector<GPlatesModel::FeatureHandle::weak_ref>::const_iterator src_point_source_infos_end = d_point_source_features->end();
+	std::vector<GPlatesModel::FeatureHandle::weak_ref>::const_iterator src_point_source_features_begin = d_point_source_features->begin();
+	std::vector<GPlatesModel::FeatureHandle::weak_ref>::const_iterator src_point_source_features_end = d_point_source_features->end();
 	if (!include_rubber_band_points)
 	{
 		if (d_sub_segment.get_start_rubber_band())
 		{
-			++src_point_source_infos_begin;
+			++src_point_source_features_begin;
 		}
 		if (d_sub_segment.get_end_rubber_band())
 		{
-			--src_point_source_infos_end;
+			--src_point_source_features_end;
 		}
 	}
 
-	if (use_reverse)
+	if (d_use_reverse)
 	{
 		std::reverse_copy(
-				src_point_source_infos_begin,
-				src_point_source_infos_end,
+				src_point_source_features_begin,
+				src_point_source_features_end,
 				std::back_inserter(point_source_features));
 	}
 	else
 	{
 		std::copy(
-				src_point_source_infos_begin,
-				src_point_source_infos_end,
+				src_point_source_features_begin,
+				src_point_source_features_end,
 				std::back_inserter(point_source_features));
 	}
 }

src/app-logic/ResolvedTopologicalGeometrySubSegment.h
@@ -379,7 +379,6 @@ namespace GPlatesAppLogic
 		void
 		get_reversed_sub_segment_point_source_features(
 				std::vector<GPlatesModel::FeatureHandle::weak_ref> &point_source_features,
-				bool use_reverse,
 				bool include_rubber_band_points = true) const;
 
 

src/app-logic/ResolvedTopologicalSharedSubSegment.cc
@@ -90,11 +90,11 @@ GPlatesAppLogic::ResolvedTopologicalSharedSubSegment::get_reversed_shared_sub_se
 {
 	// Get the points in the shared sub-segment.
 	std::vector<GPlatesMaths::PointOnSphere> geometry_points;
-	get_reversed_shared_sub_segment_points(geometry_points, include_rubber_band_points);
+	get_reversed_shared_sub_segment_points(geometry_points, use_reverse, include_rubber_band_points);
 
 	// Get the resolved source infos (one per point in the shared sub-segment).
 	resolved_vertex_source_info_seq_type geometry_point_source_infos;
-	get_reversed_shared_sub_segment_point_source_infos(geometry_point_source_infos, include_rubber_band_points);
+	get_reversed_shared_sub_segment_point_source_infos(geometry_point_source_infos, use_reverse, include_rubber_band_points);
 
 	// Number of resolved source infos should match number of points in the shared sub-segment.
 	GPlatesGlobal::Assert<GPlatesGlobal::AssertionFailureException>(
@@ -235,23 +235,23 @@ GPlatesAppLogic::ResolvedTopologicalSharedSubSegment::get_shared_sub_segment_poi
 	// Copy to caller's sequence.
 	//
 	// If the caller does not want rubber band points then avoid copying them (if they exist).
-	std::vector<GPlatesModel::FeatureHandle::weak_ref>::const_iterator src_point_source_infos_begin = d_point_source_features->begin();
-	std::vector<GPlatesModel::FeatureHandle::weak_ref>::const_iterator src_point_source_infos_end = d_point_source_features->end();
+	std::vector<GPlatesModel::FeatureHandle::weak_ref>::const_iterator src_point_source_features_begin = d_point_source_features->begin();
+	std::vector<GPlatesModel::FeatureHandle::weak_ref>::const_iterator src_point_source_features_end = d_point_source_features->end();
 	if (!include_rubber_band_points)
 	{
 		if (d_shared_sub_segment.get_start_rubber_band())
 		{
-			++src_point_source_infos_begin;
+			++src_point_source_features_begin;
 		}
 		if (d_shared_sub_segment.get_end_rubber_band())
 		{
-			--src_point_source_infos_end;
+			--src_point_source_features_end;
 		}
 	}
 
 	std::copy(
-			src_point_source_infos_begin,
-			src_point_source_infos_end,
+			src_point_source_features_begin,
+			src_point_source_features_end,
 			std::back_inserter(point_source_features));
 }
 
@@ -277,32 +277,32 @@ GPlatesAppLogic::ResolvedTopologicalSharedSubSegment::get_reversed_shared_sub_se
 	// Copy to caller's sequence.
 	//
 	// If the caller does not want rubber band points then avoid copying them (if they exist).
-	std::vector<GPlatesModel::FeatureHandle::weak_ref>::const_iterator src_point_source_infos_begin = d_point_source_features->begin();
-	std::vector<GPlatesModel::FeatureHandle::weak_ref>::const_iterator src_point_source_infos_end = d_point_source_features->end();
+	std::vector<GPlatesModel::FeatureHandle::weak_ref>::const_iterator src_point_source_features_begin = d_point_source_features->begin();
+	std::vector<GPlatesModel::FeatureHandle::weak_ref>::const_iterator src_point_source_features_end = d_point_source_features->end();
 	if (!include_rubber_band_points)
 	{
 		if (d_shared_sub_segment.get_start_rubber_band())
 		{
-			++src_point_source_infos_begin;
+			++src_point_source_features_begin;
 		}
 		if (d_shared_sub_segment.get_end_rubber_band())
 		{
-			--src_point_source_infos_end;
+			--src_point_source_features_end;
 		}
 	}
 
 	if (use_reverse)
 	{
 		std::reverse_copy(
-				src_point_source_infos_begin,
-				src_point_source_infos_end,
+				src_point_source_features_begin,
+				src_point_source_features_end,
 				std::back_inserter(point_source_features));
 	}
 	else
 	{
 		std::copy(
-				src_point_source_infos_begin,
-				src_point_source_infos_end,
+				src_point_source_features_begin,
+				src_point_source_features_end,
 				std::back_inserter(point_source_features));
 	}
 }